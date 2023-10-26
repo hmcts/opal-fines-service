f4a7a9789816e42ca33def354404ae341881795f
Dan Lysiak <50049163+danlysiak@users.noreply.github.com>
2023-10-26 23:00:27 +0100

    Initial db creation and secret mappings

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -14,3 +14,8 @@ variable "common_tags" {
   type = map(string)
 }
 
+variable "aks_subscription_id" {}
+
+variable "jenkins_AAD_objectId" {
+  description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
+}
