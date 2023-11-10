a579d0ee62519f2a8cec0eae09cd5ceecd77c943
DeclanClarkeCGI <142809814+DeclanClarkeCGI@users.noreply.github.com>
2023-11-10 14:33:39 +0000

    Initial changes for getDefendantAccount and putDefendantAccount

--- src/integrationTest/java/uk/gov/hmcts/opal/controllers/GetWelcomeTest.java
+++ src/integrationTest/java/uk/gov/hmcts/opal/controllers/GetWelcomeTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -12,6 +13,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
+@ContextConfiguration(classes = RootController.class)
 class GetWelcomeTest {
 
     @Autowired
