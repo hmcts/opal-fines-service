086d2e27bcf25b77fc1581d34d5bc005e9b6640e
DeclanClarkeCGI <142809814+DeclanClarkeCGI@users.noreply.github.com>
2023-11-13 18:05:15 +0000

    Changes to increase coverage

--- src/integrationTest/java/uk/gov/hmcts/opal/controllers/DefendantAccountControllerIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/opal/controllers/DefendantAccountControllerIntegrationTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -13,8 +14,11 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 import uk.gov.hmcts.opal.dto.AccountEnquiryDto;
 import uk.gov.hmcts.opal.entity.DefendantAccountEntity;
+import uk.gov.hmcts.opal.repository.DefendantAccountRepository;
 
 import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest
@@ -28,17 +32,22 @@ public class DefendantAccountControllerIntegrationTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @MockBean
+    private DefendantAccountRepository defendantAccountRepository; // Assuming this is your JpaRepository
+
     DefendantAccountEntity requestEntity = new DefendantAccountEntity();
 
     @BeforeEach
     public void setUp() {
-
         requestEntity.setBusinessUnitId(Short.valueOf("1"));
         requestEntity.setAccountNumber("1212");
     }
 
     @Test
     public void testPutDefendantAccount_Success() throws Exception {
+        // Arrange
+        when(defendantAccountRepository.save(any(DefendantAccountEntity.class)))
+            .thenReturn(requestEntity);
 
         // Act and Assert
         mockMvc.perform(MockMvcRequestBuilders
@@ -51,15 +60,16 @@ public class DefendantAccountControllerIntegrationTest {
 
     @Test
     public void testGetDefendantAccount_Success() throws Exception {
-
-        mockMvc.perform(MockMvcRequestBuilders
-                            .put("/api/defendant-account")
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .content(objectMapper.writeValueAsString(requestEntity)));
+        // Arrange
+        when(defendantAccountRepository.findByBusinessUnitIdAndAccountNumber(
+            requestEntity.getBusinessUnitId(), requestEntity.getAccountNumber()))
+            .thenReturn(requestEntity);
 
         // Arrange
-        AccountEnquiryDto request = AccountEnquiryDto.builder().businessUnitId(Short.valueOf("1"))
-            .accountNumber("1212").build();
+        AccountEnquiryDto request = AccountEnquiryDto.builder()
+            .businessUnitId(Short.valueOf("1"))
+            .accountNumber("1212")
+            .build();
 
         // Act and Assert
         mockMvc.perform(MockMvcRequestBuilders
@@ -75,6 +85,10 @@ public class DefendantAccountControllerIntegrationTest {
         // Arrange
         AccountEnquiryDto request = AccountEnquiryDto.builder().build();
 
+        when(defendantAccountRepository.findByBusinessUnitIdAndAccountNumber(
+            null, null))
+            .thenReturn(null);
+
         // Act and Assert
         mockMvc.perform(MockMvcRequestBuilders
                             .get("/api/defendant-account")
@@ -83,4 +97,5 @@ public class DefendantAccountControllerIntegrationTest {
             .andExpect(MockMvcResultMatchers.status().isNoContent());
     }
 }
+
 
