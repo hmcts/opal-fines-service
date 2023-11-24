9f381e555e349653a98403186c9662048301e75a
Sabah Irfan <sabah.irfan@gmail.com>
2023-11-24 14:41:43 +0000

    PO-96: Add Launch darkly Client for backend

---
+++ src/main/java/uk/gov/hmcts/opal/launchdarkly/FeatureToggleService.java
@@ -0,0 +1,18 @@
+package uk.gov.hmcts.opal.launchdarkly;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class FeatureToggleService {
+
+    private final FeatureToggleApi featureToggleApi;
+
+    public boolean isFeatureEnabled(String feature) {
+        return this.featureToggleApi.isFeatureEnabled(feature);
+    }
+
+}
