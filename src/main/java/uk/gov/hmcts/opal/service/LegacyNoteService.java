5f2445ffeb92a5864131d9c7a566eb19b415803b
DeclanClarkeCGI <142809814+DeclanClarkeCGI@users.noreply.github.com>
2023-12-05 10:34:18 +0000

    Add support for legacy root for account notes (hello world)

--- src/main/java/uk/gov/hmcts/opal/service/LegacyNoteService.java
+++ src/main/java/uk/gov/hmcts/opal/service/LegacyNoteService.java
@@ -2,11 +2,14 @@ package uk.gov.hmcts.opal.service;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 import uk.gov.hmcts.opal.dto.NoteDto;
 
 @Service
@@ -15,26 +18,33 @@ import uk.gov.hmcts.opal.dto.NoteDto;
 @Slf4j
 public class LegacyNoteService implements NoteServiceInterface {
 
+    @Value("${legacy-gateway-url}")
+    String gatewayUrl;
+
     private final RestTemplate restTemplate;
-    private final ObjectMapper objectMapper;
 
     @Override
     public NoteDto saveNote(NoteDto noteDto) {
-        String gatewayUrl = "https://postman-echo.com/post";
         log.info("Sending note to {}", gatewayUrl);
 
-        ResponseEntity<String> responseEntity = restTemplate.postForEntity(gatewayUrl, noteDto, String.class);
+        // Create a UriComponentsBuilder and add parameters
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString("")
+            .queryParam("actionType", "postAccountNotes");
+
+        ResponseEntity<String> responseEntity = restTemplate.postForEntity(
+            builder.toUriString(), noteDto, String.class);
 
         if (responseEntity.getStatusCode().is2xxSuccessful() && responseEntity.getBody() != null) {
             String rawJson = responseEntity.getBody();
             log.info("Raw JSON response: {}", rawJson);
 
             try {
+                ObjectMapper objectMapper = new ObjectMapper();
+                objectMapper.registerModule(new JavaTimeModule());
                 JsonNode root = objectMapper.readTree(rawJson);
-                JsonNode jsonNode = root.path("json");
-                if (!jsonNode.isMissingNode()) {
-                    return objectMapper.treeToValue(jsonNode, NoteDto.class);
-                }
+
+                return objectMapper.treeToValue(root, NoteDto.class);
+
             } catch (Exception e) {
                 log.error("Error deserializing response: {}", e.getMessage(), e);
             }
---
+++ src/test/java/uk/gov/hmcts/opal/service/LegacyNoteServiceTest.java
@@ -0,0 +1,144 @@
+package uk.gov.hmcts.opal.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import uk.gov.hmcts.opal.dto.NoteDto;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class LegacyNoteServiceTest {
+
+    @Mock
+    private RestTemplate restTemplate;
+
+    @Mock
+    private Logger log;
+
+    @InjectMocks
+    private LegacyNoteService legacyNoteService;
+
+
+    @Test
+    @SuppressWarnings("unchecked")
+    void saveNote_SuccessfulResponse() throws Exception {
+        // Arrange
+        final NoteDto inputNoteDto = new NoteDto();
+
+        NoteDto expectedNoteDto = NoteDto.builder()
+            .noteId(1L)
+            .noteType("AC")
+            .associatedRecordType("defendants_accounts")
+            .associatedRecordId("123456")
+            .noteText("This is a sample note text.")
+            .postedDate(null)
+            .postedBy("user123")
+            .build();
+
+        String jsonBody = "{\n"
+            + "    \"noteId\": 1,\n"
+            + "    \"noteType\": \"AC\",\n"
+            + "    \"associatedRecordType\": \"defendants_accounts\",\n"
+            + "    \"associatedRecordId\": \"123456\",\n"
+            + "    \"noteText\": \"This is a sample note text.\",\n"
+            + "    \"postedBy\": \"user123\"\n"
+            + "}";
+
+        ResponseEntity<String> successfulResponseEntity = new ResponseEntity<>(jsonBody, HttpStatus.OK);
+        when(restTemplate.postForEntity(any(String.class), any(NoteDto.class), any(Class.class)))
+            .thenReturn(successfulResponseEntity);
+
+        // Act
+        NoteDto resultNoteDto = legacyNoteService.saveNote(inputNoteDto);
+
+        // Assert
+        assertEquals(expectedNoteDto, resultNoteDto);
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    void saveNote_FailureBodyResponse() throws Exception {
+        // Arrange
+        final NoteDto inputNoteDto = new NoteDto();
+
+
+        ResponseEntity<String> unsuccessfulResponseEntity = new ResponseEntity<>(
+            null, HttpStatus.OK);
+        when(restTemplate.postForEntity(any(String.class), any(NoteDto.class), any(Class.class)))
+            .thenReturn(unsuccessfulResponseEntity);
+
+        // Act
+        NoteDto resultNoteDto = legacyNoteService.saveNote(inputNoteDto);
+
+        // Assert
+
+        assertNull(resultNoteDto);
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    void saveNote_FailureCodeResponse() throws Exception {
+        // Arrange
+        final NoteDto inputNoteDto = new NoteDto();
+
+
+        String jsonBody = "{\n"
+            + "    \"noteType\": \"AC\",\n"
+            + "    \"associatedRecordType\": \"defendants_accounts\",\n"
+            + "    \"associatedRecordId\": \"123456\",\n"
+            + "    \"noteText\": \"This is a sample note text.\",\n"
+            + "    \"postedBy\": \"user123\"\n"
+            + "}";
+
+        ResponseEntity<String> unsuccessfulResponseEntity = new ResponseEntity<>(
+            jsonBody, HttpStatus.INTERNAL_SERVER_ERROR);
+        when(restTemplate.postForEntity(any(String.class), any(NoteDto.class), any(Class.class)))
+            .thenReturn(unsuccessfulResponseEntity);
+
+        // Act
+        NoteDto resultNoteDto = legacyNoteService.saveNote(inputNoteDto);
+
+        // Assert
+
+        assertNull(resultNoteDto);
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    void saveNote_ErrorResponse() throws Exception {
+        // Arrange
+        final NoteDto inputNoteDto = new NoteDto();
+
+
+        String jsonBody = "{\n"
+            + "    \"noteTypeFOOBAR\": \"AC\",\n"
+            + "    \"associatedRecordType\": \"defendants_accounts\",\n"
+            + "    \"associatedRecordId\": \"123456\",\n"
+            + "    \"noteText\": \"This is a sample note text.\",\n"
+            + "    \"postedBy\": \"user123\"\n"
+            + "}";
+
+        ResponseEntity<String> unsuccessfulResponseEntity = new ResponseEntity<>(
+            jsonBody, HttpStatus.OK);
+        when(restTemplate.postForEntity(any(String.class), any(NoteDto.class), any(Class.class)))
+            .thenReturn(unsuccessfulResponseEntity);
+
+        // Act
+        NoteDto resultNoteDto = legacyNoteService.saveNote(inputNoteDto);
+
+        // Assert
+
+        assertNull(resultNoteDto);
+    }
+
+}
