bf6becd83d550acf58e94aa172d281657188632c
Cade Faulkner <147704481+CadeFaulkner@users.noreply.github.com>
2023-12-13 11:53:52 +0000

    Po 57 serenity automation for api testing (#85)
    * implementation of serenity for backend int tests
    
    * Apply smoke/functional tasks for Serenity
    
    * Upgrade jackson
    
    * Checkstyle fixes
    
    * Artifact test reports
    
    * cleaning up unnecessary config and files
    
    ---------
    
    Co-authored-by: Dan Lysiak <danlysiak@hotmail.com>

---
+++ src/functionalTest/java/uk/gov/hmcts/opal/steps/NotesApiStepDef.java
@@ -0,0 +1,46 @@
+package uk.gov.hmcts.opal.steps;
+
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.When;
+import net.serenitybdd.rest.SerenityRest;
+import org.hamcrest.Matchers;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Map;
+
+import static net.serenitybdd.rest.SerenityRest.then;
+
+public class NotesApiStepDef extends BaseStepDef {
+
+    @When("I post the following data to the notes API")
+    public void postToNotesApi(DataTable notesFields) throws JSONException {
+        Map<String, String> dataToPost = notesFields.asMap(String.class, String.class);
+
+        JSONObject body = new JSONObject();
+        body.put("associatedRecordId", dataToPost.get("recordId"));
+        body.put("associatedRecordType", dataToPost.get("recordType"));
+        body.put("noteText", dataToPost.get("noteText"));
+        body.put("noteType", dataToPost.get("noteType"));
+        body.put("postedBy", dataToPost.get("postedBy"));
+        body.put("postedDate", dataToPost.get("postedDate"));
+        if (dataToPost.get("noteId") != null) {
+            body.put("noteId", dataToPost.get("noteId"));
+        }
+        SerenityRest.given()
+            .accept("*/*")
+            .contentType("application/json")
+            .body(body.toString())
+            .when()
+            .post(getTestUrl() + "/api/notes");
+        then().assertThat()
+            .statusCode(201)
+            .body("associatedRecordId", Matchers.equalTo(dataToPost.get("recordId")))
+            .body("associatedRecordType", Matchers.equalTo(dataToPost.get("recordType")))
+            .body("noteText", Matchers.equalTo(dataToPost.get("noteText")))
+            .body("noteType", Matchers.equalTo(dataToPost.get("noteType")))
+            .body("postedBy", Matchers.equalTo(dataToPost.get("postedBy")));
+        //.body("postedDate", equalTo(dataToPost.get("postedDate")));
+    }
+
+}
