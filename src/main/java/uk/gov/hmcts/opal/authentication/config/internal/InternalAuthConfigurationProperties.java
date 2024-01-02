ec8fb2d2a7e47569a83f01ee2a9ef2436712ea25
Sabah Irfan <sabah.irfan@gmail.com>
2024-01-02 17:18:10 +0000

    code cleanup

--- src/main/java/uk/gov/hmcts/opal/authentication/config/internal/InternalAuthConfigurationProperties.java
+++ src/main/java/uk/gov/hmcts/opal/authentication/config/internal/InternalAuthConfigurationProperties.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.opal.authentication.config.internal;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.opal.authentication.config.AuthConfigurationProperties;
@@ -10,6 +11,7 @@ import uk.gov.hmcts.opal.authentication.config.AuthConfigurationProperties;
 @ConfigurationProperties("spring.security.oauth2.client.registration.internal-azure-ad")
 @Getter
 @Setter
+@ToString
 public class InternalAuthConfigurationProperties implements AuthConfigurationProperties {
 
     private String clientId;
