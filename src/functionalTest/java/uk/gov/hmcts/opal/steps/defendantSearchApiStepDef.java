4d4439fbceeb6769b90a636535060eb4c75d35bb
CadeFaulkner <cade.faulkner@hmcts.net>
2024-01-12 16:11:18 +0000

    auto tests for PO-119

---
+++ src/functionalTest/java/uk/gov/hmcts/opal/steps/defendantSearchApiStepDef.java
@@ -0,0 +1,96 @@
+package uk.gov.hmcts.opal.steps;
+
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.response.Response;
+import net.serenitybdd.rest.SerenityRest;
+import org.hamcrest.Matchers;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Map;
+
+import static net.serenitybdd.rest.SerenityRest.then;
+
+public class defendantSearchApiStepDef extends BaseStepDef {
+
+    private ThreadLocal<Response> apiResponse = new ThreadLocal<>();
+    @When("I make a call to the defendant search API using the parameters")
+    public void postToDefendantSearchAPI(DataTable searchCriteria) throws JSONException {
+        Map<String, String> dataToPost = searchCriteria.asMap(String.class, String.class);
+
+        JSONObject requestBody = new JSONObject();
+        JSONObject dateOfBirth = new JSONObject();
+
+        requestBody.put("forename", dataToPost.get("forename") != null ? dataToPost.get("forename") : "");
+        requestBody.put("surname", dataToPost.get("surname") != null ? dataToPost.get("surname") : "");
+        requestBody.put("initials", dataToPost.get("initials") != null ? dataToPost.get("initials") : "");
+
+        dateOfBirth.put("dayOfMonth", dataToPost.get("dayOfMonth") != null ? dataToPost.get("dayOfMonth") : "");
+        dateOfBirth.put("monthOfYear", dataToPost.get("monthOfYear") != null ? dataToPost.get("monthOfYear") : "");
+        dateOfBirth.put("year", dataToPost.get("year") != null ? dataToPost.get("year") : "");
+        requestBody.put("dateOfBirth", dateOfBirth);
+
+        requestBody.put("addressLineOne", dataToPost.get("addressLineOne") != null ? dataToPost.get("addressLineOne") : "");
+
+            SerenityRest.given()
+            .accept("*/*")
+            .contentType("application/json")
+            .body(requestBody.toString())
+            .when()
+            .post(getTestUrl() + "/api/defendant-account/search");
+
+    }
+
+    @Then("there is one result returned matching")
+    public void thereIsOneResultReturnedMatching(DataTable expectedData) {
+        Map<String, String> expectedResult = expectedData.asMap(String.class, String.class);
+
+        then().assertThat()
+            .statusCode(200)
+            .body("totalCount", Matchers.equalTo(1))
+            .body("searchResults.name[0]", Matchers.equalTo(expectedResult.get("name")))
+            .body("searchResults.dateOfBirth[0]", Matchers.equalTo(expectedResult.get("dateOfBirth")))
+            .body("searchResults.addressLine1[0]", Matchers.equalTo(expectedResult.get("addressLine1")));
+    }
+
+    @Then("there are no results returned")
+    public void thereAreNoResultsReturned() {
+        then().assertThat()
+            .statusCode(200)
+            .body("totalCount", Matchers.equalTo(0));
+    }
+
+    @Then("the returned results match")
+    public void theReturnedResultsMatch(DataTable expectedData) {
+        Map<String, String> expectedResult = expectedData.asMap(String.class, String.class);
+
+        int totalCount = then().extract().jsonPath().getInt("totalCount");
+        System.out.println("total count is : " + totalCount);
+
+        int index = 0;
+        then().assertThat()
+            .statusCode(200);
+
+        while (index < totalCount) {
+            if (expectedResult.get("name") != null) {
+                then().assertThat()
+                    .body("searchResults.name[" + index + "]", Matchers.containsString(expectedResult.get("name")));
+            }
+            if (expectedResult.get("dateOfBirth") != null) {
+                then().assertThat()
+                .body(
+                    "searchResults.dateOfBirth[" + index + "]",
+                    Matchers.containsString(expectedResult.get("dateOfBirth")));
+        }
+            if (expectedResult.get("addressLine1") != null) {
+                then().assertThat()
+            .body(
+                "searchResults.addressLine1[" + index + "]",
+                Matchers.containsString(expectedResult.get("addressLine1")));
+    }
+        index++;
+    }
+    }
+}
