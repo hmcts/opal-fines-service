50138539ff5504b8456df1170b6a7b1361a321a4
CadeFaulkner <cade.faulkner@hmcts.net>
2024-01-25 12:44:21 +0000

    checkStyle

--- src/functionalTest/java/uk/gov/hmcts/opal/steps/ModeSwitchStepDef.java
+++ src/functionalTest/java/uk/gov/hmcts/opal/steps/ModeSwitchStepDef.java
@@ -2,37 +2,22 @@ package uk.gov.hmcts.opal.steps;
 
 import io.cucumber.java.AfterAll;
 import io.cucumber.java.BeforeAll;
-import io.cucumber.java.en.Given;
 import net.serenitybdd.rest.SerenityRest;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.Objects;
 
-import static net.serenitybdd.rest.SerenityRest.then;
-
 public class ModeSwitchStepDef extends BaseStepDef {
     @BeforeAll
     public static void beforeAll() throws JSONException {
         String testMode = System.getProperty("test.mode");
 
-         if(Objects.equals(testMode, "opal")){
-             JSONObject requestBody = new JSONObject();
-             requestBody.put("mode", "opal");
-
-             System.out.println("Test mode equals "+ testMode +" setting to: opal");
-             SerenityRest.given()
-                 .accept("*/*")
-                 .contentType("application/json")
-                 .body(requestBody.toString())
-                 .when()
-                 .put(getTestUrl() + "/api/testing-support/app-mode");
-         }
-        if(Objects.equals(testMode, "legacy")){
+        if (Objects.equals(testMode, "opal")) {
             JSONObject requestBody = new JSONObject();
-            requestBody.put("mode", "legacy");
+            requestBody.put("mode", "opal");
 
-            System.out.println("Test mode equals "+ testMode +" setting to: legacy");
+            System.out.println("Test mode equals " + testMode + " setting to: opal");
             SerenityRest.given()
                 .accept("*/*")
                 .contentType("application/json")
@@ -40,7 +25,19 @@ public class ModeSwitchStepDef extends BaseStepDef {
                 .when()
                 .put(getTestUrl() + "/api/testing-support/app-mode");
         }
-         }
+        if (Objects.equals(testMode, "legacy")) {
+            JSONObject requestBody = new JSONObject();
+            requestBody.put("mode", "legacy");
+
+            System.out.println("Test mode equals " + testMode + " setting to: legacy");
+            SerenityRest.given()
+                .accept("*/*")
+                .contentType("application/json")
+                .body(requestBody.toString())
+                .when()
+                .put(getTestUrl() + "/api/testing-support/app-mode");
+        }
+    }
 
     @AfterAll
     public static void afterAll() throws JSONException {
