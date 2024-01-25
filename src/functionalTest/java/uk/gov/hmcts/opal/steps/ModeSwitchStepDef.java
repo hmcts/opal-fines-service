cd33f88f36d961b97c95bfbe7d1277fccf1a5ae6
CadeFaulkner <cade.faulkner@hmcts.net>
2024-01-25 12:26:19 +0000

    utilising beforeall and afterall hooks to change test mode

--- src/functionalTest/java/uk/gov/hmcts/opal/steps/ModeSwitchStepDef.java
+++ src/functionalTest/java/uk/gov/hmcts/opal/steps/ModeSwitchStepDef.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.opal.steps;
 
+import io.cucumber.java.AfterAll;
+import io.cucumber.java.BeforeAll;
 import io.cucumber.java.en.Given;
 import net.serenitybdd.rest.SerenityRest;
 import org.json.JSONException;
@@ -10,6 +12,36 @@ import java.util.Objects;
 import static net.serenitybdd.rest.SerenityRest.then;
 
 public class ModeSwitchStepDef extends BaseStepDef {
+    @BeforeAll
+    public static void beforeAll() throws JSONException {
+        String testMode = System.getProperty("test.mode");
+
+         if(Objects.equals(testMode, "opal")){
+             JSONObject requestBody = new JSONObject();
+             requestBody.put("mode", "opal");
+
+             System.out.println("Test mode equals "+ testMode +" setting to: opal");
+             SerenityRest.given()
+                 .accept("*/*")
+                 .contentType("application/json")
+                 .body(requestBody.toString())
+                 .when()
+                 .put(getTestUrl() + "/api/testing-support/app-mode");
+         }
+        if(Objects.equals(testMode, "legacy")){
+            JSONObject requestBody = new JSONObject();
+            requestBody.put("mode", "legacy");
+
+            System.out.println("Test mode equals "+ testMode +" setting to: legacy");
+            SerenityRest.given()
+                .accept("*/*")
+                .contentType("application/json")
+                .body(requestBody.toString())
+                .when()
+                .put(getTestUrl() + "/api/testing-support/app-mode");
+        }
+         }
+
     @Given("this test is ran in {string} mode")
     public void runTestAgainstMode(String mode) throws JSONException {
         switch (mode) {
@@ -53,4 +85,20 @@ public class ModeSwitchStepDef extends BaseStepDef {
             .when()
             .put(getTestUrl() + "/api/testing-support/app-mode");
     }
+
+    @AfterAll
+    public static void afterAll() throws JSONException {
+
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("mode", "opal");
+
+        System.out.println("All tests ran setting back to: opal");
+        SerenityRest.given()
+            .accept("*/*")
+            .contentType("application/json")
+            .body(requestBody.toString())
+            .when()
+            .put(getTestUrl() + "/api/testing-support/app-mode");
+    }
+
 }
