a821a4c98156bab472703329fa23deaabfa0c695
Cade Faulkner <147704481+CadeFaulkner@users.noreply.github.com>
2024-02-13 17:27:10 +0000

    Integrate mode switch for automated tests (#153)

---
+++ src/functionalTest/java/uk/gov/hmcts/opal/steps/DefendantAccountAddNoteStepDef.java
@@ -0,0 +1,55 @@
+package uk.gov.hmcts.opal.steps;
+
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import net.serenitybdd.rest.SerenityRest;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Map;
+
+import static net.serenitybdd.rest.SerenityRest.then;
+import static org.junit.Assert.assertEquals;
+import static uk.gov.hmcts.opal.steps.BearerTokenStefDef.getToken;
+
+public class DefendantAccountAddNoteStepDef extends BaseStepDef {
+    @When("I make a request to the defendant account add notes api with")
+    public void postToDefAccountAddNotesApi(DataTable data) throws JSONException {
+        Map<String, String> dataToPost = data.asMap(String.class, String.class);
+
+        JSONObject postBody = new JSONObject();
+        for (String key : dataToPost.keySet()) {
+            postBody.put(key, dataToPost.get(key));
+        }
+        SerenityRest
+            .given()
+            .header("Authorization", "Bearer " + getToken())
+            .accept("*/*")
+            .contentType("application/json")
+            .body(postBody.toString())
+            .when()
+            .post(getTestUrl() + "/api/defendant-account/addNote");
+    }
+
+    @Then("the add notes response contains")
+    public void assertAddNotesResponse(DataTable data) {
+        Map<String, String> response = data.asMap(String.class, String.class);
+        then().assertThat()
+            .statusCode(201);
+
+        for (String key : response.keySet()) {
+            String apiResponseValue = then().extract().body().jsonPath().getString(key);
+            assertEquals("Values are not equal : ", response.get(key), apiResponseValue);
+        }
+    }
+
+    @Then("the following account note is returned in the ac details request")
+    public void theFollowingAccountNoteIsReturnedInTheAcDetailsRequest(DataTable data) {
+        Map<String, String> response = data.asMap(String.class, String.class);
+        for (String key : response.keySet()) {
+            String apiResponseValue = then().extract().body().jsonPath().getString(key);
+            assertEquals("Values are not equal : ", response.get(key), apiResponseValue);
+        }
+    }
+}
--- src/functionalTest/java/uk/gov/hmcts/opal/steps/HealthApiStepDef.java
+++ src/functionalTest/java/uk/gov/hmcts/opal/steps/HealthApiStepDef.java
@@ -6,12 +6,17 @@ import io.cucumber.java.en.Then;
 import net.serenitybdd.rest.SerenityRest;
 import org.hamcrest.Matchers;
 
+import static uk.gov.hmcts.opal.steps.BearerTokenStefDef.getToken;
+
 public class HealthApiStepDef extends BaseStepDef {
 
     @Then("I check the health of the fines api")
     public void checkHealthOfFinesApi() {
         System.out.println("Test URL: " + getTestUrl());
-        SerenityRest.when().get(getTestUrl() + "/health");
+        SerenityRest.given()
+            .header("Authorization", "Bearer " + getToken())
+            .when()
+            .get(getTestUrl() + "/health");
         SerenityRest.then().assertThat().statusCode(200).and().body("status", Matchers.is("UP"));
     }
 
