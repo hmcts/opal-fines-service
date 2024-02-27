ffaa82ef6e1ebdef15c3ce1189a2f597ea967f0b
CadeFaulkner <cade.faulkner@hmcts.net>
2024-02-27 16:22:29 +0000

    PO-220 integrate get token with defined user

--- src/functionalTest/java/uk/gov/hmcts/opal/steps/BearerTokenStefDef.java
+++ src/functionalTest/java/uk/gov/hmcts/opal/steps/BearerTokenStefDef.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.opal.steps;
 
 import io.cucumber.java.BeforeAll;
+import io.cucumber.java.en.When;
 import net.serenitybdd.rest.SerenityRest;
 
 import static net.serenitybdd.rest.SerenityRest.then;
@@ -8,6 +9,7 @@ import static net.serenitybdd.rest.SerenityRest.then;
 
 public class BearerTokenStefDef extends BaseStepDef {
     protected static String TOKEN;
+    protected static ThreadLocal<String> ALT_TOKEN = new ThreadLocal<>();
 
     @BeforeAll
     public static void setToken() {
@@ -21,7 +23,21 @@ public class BearerTokenStefDef extends BaseStepDef {
     }
 
     protected static String getToken() {
-        return TOKEN;
+        if (ALT_TOKEN.get() == null) {
+            return TOKEN;
+        } else return ALT_TOKEN.get();
+    }
+
+    @When("I am testing as the {string} user")
+    public void setTokenWithUser(String user) {
+        SerenityRest.given()
+            .accept("*/*")
+            .header("X-User-Email", user)
+            .contentType("application/json")
+            .when()
+            .get(getTestUrl() + "/api/testing-support/token/user");
+        then().assertThat().statusCode(200);
+        ALT_TOKEN.set(then().extract().body().jsonPath().getString("access_token"));
     }
 
 }
