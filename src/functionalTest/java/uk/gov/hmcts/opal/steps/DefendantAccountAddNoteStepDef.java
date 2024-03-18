81f53374c00e961055d5b2badb3cadfeac93eaa4
CadeFaulkner <cade.faulkner@hmcts.net>
2024-03-18 15:56:20 +0000

    tests for 235

--- src/functionalTest/java/uk/gov/hmcts/opal/steps/DefendantAccountAddNoteStepDef.java
+++ src/functionalTest/java/uk/gov/hmcts/opal/steps/DefendantAccountAddNoteStepDef.java
@@ -50,6 +50,12 @@ public class DefendantAccountAddNoteStepDef extends BaseStepDef {
             .statusCode(403);
     }
 
+    @Then("the add notes request returns an Internal Server Error")
+    public void assertAddNotesInternalServerError() {
+        then().assertThat()
+            .statusCode(500);
+    }
+
     @Then("the following account note is returned in the ac details request")
     public void theFollowingAccountNoteIsReturnedInTheAcDetailsRequest(DataTable data) {
         Map<String, String> response = data.asMap(String.class, String.class);
