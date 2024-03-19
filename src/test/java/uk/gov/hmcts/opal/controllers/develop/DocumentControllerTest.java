67ad572e4d3a326bda7c00f8c28e4bb5db5b80ba
RustyHMCTS <149379845+RustyHMCTS@users.noreply.github.com>
2024-03-19 17:27:51 +0000

    Move some controllers not used by FE into alt package (#273)

---
+++ src/test/java/uk/gov/hmcts/opal/controllers/develop/DocumentControllerTest.java
@@ -0,0 +1,66 @@
+package uk.gov.hmcts.opal.controllers.develop;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import uk.gov.hmcts.opal.controllers.develop.DocumentController;
+import uk.gov.hmcts.opal.dto.search.DocumentSearchDto;
+import uk.gov.hmcts.opal.entity.DocumentEntity;
+import uk.gov.hmcts.opal.service.opal.DocumentService;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class DocumentControllerTest {
+
+    @Mock
+    private DocumentService documentService;
+
+    @InjectMocks
+    private DocumentController documentController;
+
+    @Test
+    void testGetDocument_Success() {
+        // Arrange
+        DocumentEntity entity = DocumentEntity.builder().build();
+
+        when(documentService.getDocument(any(String.class))).thenReturn(entity);
+
+        // Act
+        ResponseEntity<DocumentEntity> response = documentController.getDocumentById("ID1");
+
+        // Assert
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(entity, response.getBody());
+        verify(documentService, times(1)).getDocument(any(String.class));
+    }
+
+    @Test
+    void testSearchDocuments_Success() {
+        // Arrange
+        DocumentEntity entity = DocumentEntity.builder().build();
+        List<DocumentEntity> documentList = List.of(entity);
+
+        when(documentService.searchDocuments(any())).thenReturn(documentList);
+
+        // Act
+        DocumentSearchDto searchDto = DocumentSearchDto.builder().build();
+        ResponseEntity<List<DocumentEntity>> response = documentController.postDocumentsSearch(searchDto);
+
+        // Assert
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(documentList, response.getBody());
+        verify(documentService, times(1)).searchDocuments(any());
+    }
+
+}
---
+++ src/test/java/uk/gov/hmcts/opal/controllers/develop/PartyControllerTest.java
@@ -0,0 +1,84 @@
+package uk.gov.hmcts.opal.controllers.develop;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import uk.gov.hmcts.opal.controllers.develop.PartyController;
+import uk.gov.hmcts.opal.dto.PartyDto;
+import uk.gov.hmcts.opal.dto.search.PartySearchDto;
+import uk.gov.hmcts.opal.entity.PartyEntity;
+import uk.gov.hmcts.opal.service.opal.PartyService;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class PartyControllerTest {
+
+    @Mock
+    private PartyService partyService;
+
+    @InjectMocks
+    private PartyController partyController;
+
+    @Test
+    void testCreateParty_Success() {
+        // Arrange
+        PartyDto partyDtoRequest = PartyDto.builder().build();
+        PartyDto partyDtoResponse = PartyDto.builder().partyId(1L).build();
+
+        when(partyService.saveParty(any(PartyDto.class))).thenReturn(partyDtoResponse);
+
+        // Act
+        ResponseEntity<PartyDto> response = partyController.createParty(partyDtoRequest);
+
+        // Assert
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertEquals(partyDtoResponse, response.getBody());
+        verify(partyService, times(1)).saveParty(any(PartyDto.class));
+    }
+
+    @Test
+    void testGetParty_Success() {
+        // Arrange
+        PartyDto partyDtoResponse = PartyDto.builder().partyId(1L).build(); //some id assigned by db sequence
+
+        when(partyService.getParty(any(Long.class))).thenReturn(partyDtoResponse);
+
+        // Act
+        ResponseEntity<PartyDto> response = partyController.getParty(1L);
+
+        // Assert
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(partyDtoResponse, response.getBody());
+        verify(partyService, times(1)).getParty(any(Long.class));
+    }
+
+    @Test
+    void testSearchParties_Success() {
+        // Arrange
+        PartyEntity entity = PartyEntity.builder().build();
+        List<PartyEntity> partyList = List.of(entity);
+
+        when(partyService.searchParties(any())).thenReturn(partyList);
+
+        // Act
+        PartySearchDto searchDto = PartySearchDto.builder().build();
+        ResponseEntity<List<PartyEntity>> response = partyController.postPartiesSearch(searchDto);
+
+        // Assert
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(partyList, response.getBody());
+        verify(partyService, times(1)).searchParties(any());
+    }
+
+}
