bbe2a32dca21edac5b6a060c4a3fe424f1cd47ac
Sabah u Din Irfan <sabah.irfan@gmail.com>
2024-03-19 16:32:52 +0000

    PO-242 update test user endpoints to return user sate (#272)

--- src/main/java/uk/gov/hmcts/opal/authorisation/service/AuthorisationService.java
+++ src/main/java/uk/gov/hmcts/opal/authorisation/service/AuthorisationService.java
@@ -3,8 +3,12 @@ package uk.gov.hmcts.opal.authorisation.service;
 import groovy.util.logging.Slf4j;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import uk.gov.hmcts.opal.authentication.model.SecurityToken;
+import uk.gov.hmcts.opal.authentication.service.AccessTokenService;
 import uk.gov.hmcts.opal.authorisation.model.UserState;
 import uk.gov.hmcts.opal.service.opal.UserService;
+
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -12,8 +16,22 @@ import uk.gov.hmcts.opal.service.opal.UserService;
 public class AuthorisationService {
 
     private final UserService userService;
+    private final AccessTokenService accessTokenService;
 
     public UserState getAuthorisation(String username) {
         return userService.getUserStateByUsername(username);
+    }
+
+    public SecurityToken getSecurityToken(String accessToken) {
+        var securityTokenBuilder = SecurityToken.builder()
+            .accessToken(accessToken);
+        Optional<String> preferredUsernameOptional = Optional.ofNullable(
+            accessTokenService.extractPreferredUsername(accessToken));
+
+        if (preferredUsernameOptional.isPresent()) {
+            UserState userStateOptional = this.getAuthorisation(preferredUsernameOptional.get());
+            securityTokenBuilder.userState(userStateOptional);
+        }
+        return securityTokenBuilder.build();
     }
 }
