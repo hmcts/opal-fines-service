0071f403ea6f1f8f0e8ec70b1021e9f0941dfe05
RustyHMCTS <149379845+RustyHMCTS@users.noreply.github.com>
2024-04-12 17:11:52 +0100

    Revert "PO-238: Log retention Scheduler"
    This reverts commit 83063004e62c1dc24332b2561317c956148478d5.

--- src/main/java/uk/gov/hmcts/opal/repository/LogAuditDetailRepository.java
+++ src/main/java/uk/gov/hmcts/opal/repository/LogAuditDetailRepository.java
@@ -2,14 +2,10 @@ package uk.gov.hmcts.opal.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.stereotype.Repository;
 import uk.gov.hmcts.opal.entity.LogAuditDetailEntity;
 
 @Repository
 public interface LogAuditDetailRepository extends JpaRepository<LogAuditDetailEntity, Long>,
     JpaSpecificationExecutor<LogAuditDetailEntity> {
-
-    @Procedure(name = "delete_expired_log_audit")
-    void deleteExpiredLogAudit();
 }
