2289a7eb5d4bb9c2e9b8a1faf62478dfef4e8d7c
Russell Dodd <russell.dodd@hmcts.net>
2024-05-13 17:21:12 +0100

    Tweaks to the Business Unit reference data

--- src/main/java/uk/gov/hmcts/opal/dto/reference/BusinessUnitReferenceDataResults.java
+++ src/main/java/uk/gov/hmcts/opal/dto/reference/BusinessUnitReferenceDataResults.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import uk.gov.hmcts.opal.entity.BusinessUnitEntity;
+import uk.gov.hmcts.opal.entity.projection.BusinessUnitReferenceData;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,11 +15,11 @@ import java.util.Optional;
 @AllArgsConstructor
 public class BusinessUnitReferenceDataResults {
     private Integer count;
-    private List<BusinessUnitEntity> refData;
+    private List<BusinessUnitReferenceData> refData;
 
     public static class BusinessUnitReferenceDataResultsBuilder {
         public BusinessUnitReferenceDataResults.BusinessUnitReferenceDataResultsBuilder refData(
-            List<BusinessUnitEntity> refData) {
+            List<BusinessUnitReferenceData> refData) {
             this.refData = refData;
             return this.count(Optional.ofNullable(refData).map(List::size).orElse(0));
         }
