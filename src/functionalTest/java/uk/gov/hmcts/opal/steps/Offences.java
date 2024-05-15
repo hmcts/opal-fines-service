b94e9127447695c73b987b7bd2745af9966ee614
rajanigandra <Rajani Gandra>
2024-05-15 15:29:53 +0100

    using logged info and changed assertion

--- src/functionalTest/java/uk/gov/hmcts/opal/steps/Offences.java
+++ src/functionalTest/java/uk/gov/hmcts/opal/steps/Offences.java
@@ -4,13 +4,17 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import net.serenitybdd.rest.SerenityRest;
 import org.hamcrest.Matchers;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static net.serenitybdd.rest.SerenityRest.then;
 import static uk.gov.hmcts.opal.config.Constants.OFFENCES_REF_DATA_URI;
 import static uk.gov.hmcts.opal.steps.BearerTokenStepDef.getToken;
 
+
 public class Offences extends BaseStepDef {
 
+    static Logger log = LoggerFactory.getLogger(Offences.class.getName());
     @When("I make a request to the offence ref data api with")
     public void getRequestToOffencesRefData() {
         SerenityRest
@@ -28,10 +32,12 @@ public class Offences extends BaseStepDef {
     @Then("the offence ref data matching to result")
     public void theRefDataMatchingToResult() {
         int totalCount = then().extract().jsonPath().getInt("count");
-        System.out.println("total count is : " + totalCount);
+        int refDataList = then().extract().jsonPath().getList("refData").size();
+        log.info("total count is : " + totalCount);
+        log.info("Total records in the json response" +refDataList);
         then().assertThat()
             .statusCode(200)
-            .body("count", Matchers.equalTo(totalCount));
+            .body("count", Matchers.equalTo(refDataList));
     }
 
 }
