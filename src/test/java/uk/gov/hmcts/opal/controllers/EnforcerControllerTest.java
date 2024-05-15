d506ce117c16d52a99620d77d92ef2cd50b1fe4a
Russell Dodd <russell.dodd@hmcts.net>
2024-05-15 09:04:36 +0100

    Add Enforcer reference data endpoint & associated code

--- src/test/java/uk/gov/hmcts/opal/controllers/EnforcerControllerTest.java
+++ src/test/java/uk/gov/hmcts/opal/controllers/EnforcerControllerTest.java
@@ -1,4 +1,4 @@
-package uk.gov.hmcts.opal.controllers.develop;
+package uk.gov.hmcts.opal.controllers;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -7,12 +7,14 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import uk.gov.hmcts.opal.controllers.develop.EnforcerController;
+import uk.gov.hmcts.opal.dto.reference.EnforcerReferenceDataResults;
 import uk.gov.hmcts.opal.dto.search.EnforcerSearchDto;
 import uk.gov.hmcts.opal.entity.EnforcerEntity;
+import uk.gov.hmcts.opal.entity.projection.EnforcerReferenceData;
 import uk.gov.hmcts.opal.service.opal.EnforcerService;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
@@ -63,4 +65,25 @@ class EnforcerControllerTest {
         verify(enforcerService, times(1)).searchEnforcers(any());
     }
 
+    @Test
+    void testGetEnforcersRefData_Success() {
+        // Arrange
+        EnforcerReferenceData refData = new EnforcerReferenceData(1L, (short)2,
+                                                                  "Enforcers Ltd", "Enforcers Wales Ltd");
+        List<EnforcerReferenceData> refDataList = List.of(refData);
+
+        when(enforcerService.getReferenceData(any())).thenReturn(refDataList);
+
+        // Act
+        Optional<String> filter = Optional.empty();
+        ResponseEntity<EnforcerReferenceDataResults> response = enforcerController
+            .getEnforcersRefData(filter);
+
+        // Assert
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        EnforcerReferenceDataResults refDataResults = response.getBody();
+        assertEquals(1, refDataResults.getCount());
+        assertEquals(refDataList, refDataResults.getRefData());
+        verify(enforcerService, times(1)).getReferenceData(any());
+    }
 }
