5cca0610568e4266e7144c42b81219c874c52421
rajanigandra <167096333+rajanigandra@users.noreply.github.com>
2024-05-21 17:07:52 +0100

    Po 313 bus units ref data (#367)
    * Tweaks to the Business Unit reference data
    
    * Completed automation test for business unit ref data
    
    * chore(deps): update plugin org.flywaydb.flyway to v10.13.0
    
    * chore(deps): update helm release java to v5.2.1
    
    * applied filter to reduce the no.of records displaying on console
    
    ---------
    
    Co-authored-by: Russell Dodd <russell.dodd@hmcts.net>
    Co-authored-by: rajanigandra <Rajani Gandra>
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Cade Faulkner <147704481+CadeFaulkner@users.noreply.github.com>

--- src/functionalTest/java/uk/gov/hmcts/opal/config/Constants.java
+++ src/functionalTest/java/uk/gov/hmcts/opal/config/Constants.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.opal.config;
 
 public class Constants {
 
+    public static final String BUSINESS_UNIT_REF_DATA_URI = "/api/business-unit/ref-data/";
     public static final String COURTS_REF_DATA_URI = "/api/court/ref-data/";
     public static final String LJA_REF_DATA_URI = "/api/local-justice-area/ref-data/";
     public static final String OFFENCES_REF_DATA_URI = "/api/offence/ref-data/";
