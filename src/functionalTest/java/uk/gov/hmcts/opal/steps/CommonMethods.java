1bd02eac05390c9a2a992c317f4ae2139263bda5
rajanigandra <Rajani Gandra>
2024-05-24 13:22:43 +0100

    added tests for Enforcer ref data and refactored ref data scenarios

---
+++ src/functionalTest/java/uk/gov/hmcts/opal/steps/CommonMethods.java
@@ -0,0 +1,20 @@
+package uk.gov.hmcts.opal.steps;
+
+import net.serenitybdd.rest.SerenityRest;
+import uk.gov.hmcts.opal.steps.BaseStepDef;
+
+import static uk.gov.hmcts.opal.steps.BearerTokenStepDef.getToken;
+
+public class CommonMethods extends BaseStepDef {
+
+    public void getRequest(String refDataUri) {
+        SerenityRest
+            .given()
+            .accept("*/*")
+            .header("Authorization", "Bearer " + getToken())
+            .contentType("application/json")
+            .when()
+            .get(getTestUrl() + refDataUri);
+    }
+
+}
