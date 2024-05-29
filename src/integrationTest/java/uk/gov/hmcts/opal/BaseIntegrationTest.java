924a18b49b4107475cf8c370ab4d2bdb2d7fb4ff
Sabah u din Irfan <sabah.irfan@gmail.com>
2024-05-29 23:28:33 +0100

    PO-388: Upgrade springboot version

--- src/integrationTest/java/uk/gov/hmcts/opal/BaseIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/opal/BaseIntegrationTest.java
@@ -1,10 +1,21 @@
 package uk.gov.hmcts.opal;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
+import uk.gov.hmcts.opal.scheduler.config.CronJobConfiguration;
+import uk.gov.hmcts.opal.scheduler.config.QuartzConfiguration;
+import uk.gov.hmcts.opal.scheduler.service.JobService;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles({"integration"})
 public class BaseIntegrationTest {
+
+    @MockBean
+    CronJobConfiguration cronJobConfiguration;
+    @MockBean
+    QuartzConfiguration quartzConfiguration;
+    @MockBean
+    JobService jobService;
 
 }
