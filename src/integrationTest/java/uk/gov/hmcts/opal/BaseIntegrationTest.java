f4c8b09a904b4d678e15be056183405786042d81
Sabah u Din Irfan <sabah.irfan@gmail.com>
2024-05-30 16:45:03 +0100

    PO-388: Upgrade springboot version (#381)
    * PO-388: Upgrade springboot version
    
    * PO-388: Upgrade springboot version
    
    * PO-388: Upgrade springboot version
    
    * add postgress flyway module

--- src/integrationTest/java/uk/gov/hmcts/opal/BaseIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/opal/BaseIntegrationTest.java
@@ -2,16 +2,11 @@ package uk.gov.hmcts.opal;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import uk.gov.hmcts.opal.scheduler.config.CronJobConfiguration;
 import uk.gov.hmcts.opal.scheduler.config.QuartzConfiguration;
 import uk.gov.hmcts.opal.scheduler.service.JobService;
 
-@Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles({"integration"})
 public class BaseIntegrationTest {
@@ -23,7 +18,4 @@ public class BaseIntegrationTest {
     @MockBean
     JobService jobService;
 
-    @ServiceConnection
-    @Container
-    static PostgreSQLContainer databaseContainer = new PostgreSQLContainer<>("postgres:15-alpine");
 }
