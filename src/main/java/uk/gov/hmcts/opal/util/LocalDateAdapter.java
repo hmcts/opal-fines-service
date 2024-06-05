c275ebc5857114b3c095d0c6650bc18acc5f577a
DeclanClarkeCGI <142809814+DeclanClarkeCGI@users.noreply.github.com>
2024-06-05 21:25:28 +0100

    jaxb util classes

---
+++ src/main/java/uk/gov/hmcts/opal/util/LocalDateAdapter.java
@@ -0,0 +1,17 @@
+package uk.gov.hmcts.opal.util;
+
+import jakarta.xml.bind.annotation.adapters.XmlAdapter;
+import java.time.LocalDate;
+
+public class LocalDateAdapter extends XmlAdapter<String, LocalDate> {
+
+    @Override
+    public LocalDate unmarshal(String v) throws Exception {
+        return LocalDate.parse(v);
+    }
+
+    @Override
+    public String marshal(LocalDate v) throws Exception {
+        return v.toString();
+    }
+}
