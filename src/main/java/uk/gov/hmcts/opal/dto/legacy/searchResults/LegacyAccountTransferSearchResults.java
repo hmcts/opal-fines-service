8de91210c71f811a992f0be2b6c872a41c213375
DeclanClarkeCGI <142809814+DeclanClarkeCGI@users.noreply.github.com>
2024-06-27 10:20:54 +0100

    Add missing wrapper class and remove unused generic wrapper

---
+++ src/main/java/uk/gov/hmcts/opal/dto/legacy/searchResults/LegacyAccountTransferSearchResults.java
@@ -0,0 +1,19 @@
+package uk.gov.hmcts.opal.dto.legacy.searchResults;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import lombok.Data;
+import uk.gov.hmcts.opal.entity.AccountTransferEntity;
+
+import java.util.List;
+
+@Data
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class LegacyAccountTransferSearchResults {
+
+    @XmlElement( name = "accountTransferEntity")
+    private List<AccountTransferEntity> accountTransferEntities;
+}
