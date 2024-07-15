18090deef219bc94beb26ab88a9e965bc6495b81
Russell Dodd <russell.dodd@hmcts.net>
2024-07-15 11:41:13 +0100

    PO-495: Infra code for HmrcRequest

---
+++ src/main/java/uk/gov/hmcts/opal/controllers/develop/HmrcRequestController.java
@@ -0,0 +1,58 @@
+package uk.gov.hmcts.opal.controllers.develop;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.opal.dto.search.HmrcRequestSearchDto;
+import uk.gov.hmcts.opal.entity.HmrcRequestEntity;
+import uk.gov.hmcts.opal.service.HmrcRequestServiceInterface;
+
+import java.util.List;
+
+import static uk.gov.hmcts.opal.util.HttpUtil.buildResponse;
+
+
+@RestController
+@RequestMapping("/dev/hmrc-request")
+@Slf4j(topic = "HmrcRequestController")
+@Tag(name = "HmrcRequest Controller")
+public class HmrcRequestController {
+
+    private final HmrcRequestServiceInterface hmrcRequestService;
+
+    public HmrcRequestController(@Qualifier("hmrcRequestService") HmrcRequestServiceInterface hmrcRequestService) {
+        this.hmrcRequestService = hmrcRequestService;
+    }
+
+    @GetMapping(value = "/{hmrcRequestId}")
+    @Operation(summary = "Returns the HmrcRequest for the given hmrcRequestId.")
+    public ResponseEntity<HmrcRequestEntity> getHmrcRequestById(@PathVariable Long hmrcRequestId) {
+
+        log.info(":GET:getHmrcRequestById: hmrcRequestId: {}", hmrcRequestId);
+
+        HmrcRequestEntity response = hmrcRequestService.getHmrcRequest(hmrcRequestId);
+
+        return buildResponse(response);
+    }
+
+    @PostMapping(value = "/search", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Searches HMRC Requests based upon criteria in request body")
+    public ResponseEntity<List<HmrcRequestEntity>> postHmrcRequestsSearch(@RequestBody HmrcRequestSearchDto criteria) {
+        log.info(":POST:postHmrcRequestsSearch: query: \n{}", criteria);
+
+        List<HmrcRequestEntity> response = hmrcRequestService.searchHmrcRequests(criteria);
+
+        return buildResponse(response);
+    }
+
+
+}
