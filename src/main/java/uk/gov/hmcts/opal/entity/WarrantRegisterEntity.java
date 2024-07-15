6895cc1c227167210f1445547965708f3618a529
Russell Dodd <russell.dodd@hmcts.net>
2024-07-15 11:45:27 +0100

    PO-495: Infra code for WarrantRegister

---
+++ src/main/java/uk/gov/hmcts/opal/entity/WarrantRegisterEntity.java
@@ -0,0 +1,46 @@
+package uk.gov.hmcts.opal.entity;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "warrant_registers")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "warrantRegisterId")
+public class WarrantRegisterEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "warrant_register_id_seq_generator")
+    @SequenceGenerator(name = "warrant_register_id_seq_generator", sequenceName = "warrant_register_id_seq",
+        allocationSize = 1)
+    @Column(name = "warrant_register_id", nullable = false)
+    private Long warrantRegisterId;
+
+    @ManyToOne
+    @JoinColumn(name = "business_unit_id", nullable = false)
+    private BusinessUnitEntity businessUnit;
+
+    @Column(name = "enforcer_id", nullable = false)
+    private Long enforcerId;
+
+    @Column(name = "enforcement_id")
+    private Long enforcementId;
+}
---
+++ src/main/java/uk/gov/hmcts/opal/repository/jpa/WarrantRegisterSpecs.java
@@ -0,0 +1,36 @@
+package uk.gov.hmcts.opal.repository.jpa;
+
+import jakarta.persistence.criteria.From;
+import jakarta.persistence.criteria.Join;
+import org.springframework.data.jpa.domain.Specification;
+import uk.gov.hmcts.opal.dto.search.WarrantRegisterSearchDto;
+import uk.gov.hmcts.opal.entity.BusinessUnitEntity;
+import uk.gov.hmcts.opal.entity.WarrantRegisterEntity;
+import uk.gov.hmcts.opal.entity.WarrantRegisterEntity_;
+
+import static uk.gov.hmcts.opal.repository.jpa.BusinessUnitSpecs.equalsBusinessUnitIdPredicate;
+
+public class WarrantRegisterSpecs extends EntitySpecs<WarrantRegisterEntity> {
+
+    public Specification<WarrantRegisterEntity> findBySearchCriteria(WarrantRegisterSearchDto criteria) {
+        return Specification.allOf(specificationList(
+            notBlank(criteria.getWarrantRegisterId()).map(WarrantRegisterSpecs::equalsWarrantRegisterId),
+            numericShort(criteria.getBusinessUnitId()).map(WarrantRegisterSpecs::equalsBusinessUnitId)
+        ));
+    }
+
+    public static Specification<WarrantRegisterEntity> equalsWarrantRegisterId(String warrantRegisterId) {
+        return (root, query, builder) -> builder.equal(root.get(WarrantRegisterEntity_.warrantRegisterId),
+                                                       warrantRegisterId);
+    }
+
+    public static Specification<WarrantRegisterEntity> equalsBusinessUnitId(Short businessUnitId) {
+        return (root, query, builder) ->
+            equalsBusinessUnitIdPredicate(joinBusinessUnit(root), builder, businessUnitId);
+    }
+
+    public static Join<WarrantRegisterEntity, BusinessUnitEntity> joinBusinessUnit(
+        From<?, WarrantRegisterEntity> from) {
+        return from.join(WarrantRegisterEntity_.businessUnit);
+    }
+}
