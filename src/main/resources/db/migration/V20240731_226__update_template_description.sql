f8fdc9db06ec36dc364b6d78a11454bb606cea11
ian-readman <ian.readman@hmcts.net>
2024-07-31 09:48:40 +0100

    PO-570 Change Template Description

--- src/main/resources/db/migration/V20240731_226__update_template_description.sql
+++ src/main/resources/db/migration/V20240731_226__update_template_description.sql
@@ -1,23 +1,16 @@
 /**
+*
 * OPAL Program
 *
-* MODULE      : update_welsh_ad_business_units.sql
+* MODULE      : update_template.sql
 *
-* DESCRIPTION : Update the business_units table to indicate the Welsh business units for business unit type of Accounting Division. This is to replace previous work done for business unit type of Area
+* DESCRIPTION : Update template name to "Collection Order - Authorised Powers"
 *
 * VERSION HISTORY:
 *
-* Date          Author      Version     Nature of Change
-* ----------    -------     --------    -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-* 25/07/2024    A Dennis    1.0         PO-562 Update the business_units table to indicate the Welsh business units for business unit type of Accounting Division. This is to replace previous work done for business unit type of Area  
+* Date          Author       Version     Nature of Change 
+* ----------    --------     --------    --------------------------------------
+* 30/07/2024    I Readman    1.0         PO-570 Update incorrect template name
 *
-**/
--- Nullify previous work where business unit type was Area
-UPDATE business_units
-SET    welsh_language       = NULL
-WHERE  business_unit_id     IN (26, 15, 11, 31);
-
--- Set it for where business unit type is Accounting Division
-UPDATE business_units
-SET    welsh_language       = TRUE
-WHERE  business_unit_id     IN (78, 58, 53, 85);  -- these are the Accounting Divisions for North Wales, Gwent, Dyfed Powys, South Wales in that order
+**/  
+UPDATE templates SET template_name = 'Collection Order - Authorised Powers' WHERE template_id = 500000004;
