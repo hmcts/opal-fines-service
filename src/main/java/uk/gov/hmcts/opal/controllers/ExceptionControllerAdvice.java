c9977fd324fcc88da01c3b7bb36142acbc5c2a9b
Russell Dodd <russell.dodd@hmcts.net>
2024-08-06 09:34:51 +0100

    PO-511: Allow for custom HTTP response codes on error

---
+++ src/main/java/uk/gov/hmcts/opal/controllers/ExceptionControllerAdvice.java
@@ -0,0 +1,64 @@
+package uk.gov.hmcts.opal.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.PropertyValueException;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
+import org.springframework.dao.InvalidDataAccessResourceUsageException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Map;
+
+@ControllerAdvice
+@Slf4j(topic = "ExceptionControllerAdvice")
+public class ExceptionControllerAdvice {
+
+    @ExceptionHandler
+    public ResponseEntity<Map<String, String>> handlePropertyValueException(PropertyValueException pve) {
+        log.error(":handlePropertyValueException: {}", pve.getMessage());
+        Map<String, String> body = Map.of(
+            "errorMessage", pve.getMessage(),
+            "entity", pve.getEntityName(),
+            "property", pve.getPropertyName()
+        );
+        return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT).body(body);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Map<String, String>> handleHttpMessageNotReadableException(
+        HttpMessageNotReadableException hmnre) {
+
+        log.error(":handleHttpMessageNotReadableException: {}", hmnre.getMessage());
+        Map<String, String> body = Map.of(
+            "errorMessage", hmnre.getMessage()
+        );
+        return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT).body(body);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Map<String, String>> handleInvalidDataAccessApiUsageException(
+        InvalidDataAccessApiUsageException idaaue) {
+
+        log.error(":handleInvalidDataAccessApiUsageException: {}", idaaue.getMessage());
+        Map<String, String> body = Map.of(
+            "errorMessage", idaaue.getMessage()
+        );
+        return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT).body(body);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Map<String, String>> handleInvalidDataAccessResourceUsageException(
+        InvalidDataAccessResourceUsageException idarue) {
+
+        log.error(":handleInvalidDataAccessApiUsageException: {}", idarue.getMessage());
+        log.error(":handleInvalidDataAccessApiUsageException:", idarue.getRootCause());
+
+        Map<String, String> body = Map.of(
+            "errorMessage", idarue.getMessage()
+        );
+        return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT).body(body);
+    }
+}
