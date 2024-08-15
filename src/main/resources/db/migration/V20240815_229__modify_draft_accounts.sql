652af53b71410473a999d9254198e485e701cee7
abrahamdennis <abraham.dennis@hmcts.net>
2024-08-15 15:21:20 +0100

    PO-623 modified the DRAFT_ACCOUNTS table to add new column and rename some columns and amended column descriptions

---
+++ src/main/resources/db/migration/V20240815_229__modify_draft_accounts.sql
@@ -0,0 +1,28 @@
+/**
+*
+* OPAL Program
+*
+* MODULE      : modify_draft_accounts.sql
+*
+* DESCRIPTION : Modified DRAFT_ACCOUNTS table with some new columns and renamed others.
+*
+* VERSION HISTORY:
+*
+* Date          Author       Version     Nature of Change 
+* ----------    --------     --------    ------------------------------------------------------------------------------
+* 15/08/2024    A Dennis     1.0         PO-623 Modified DRAFT_ACCOUNTS table with some new columns and renamed others.
+*
+**/     
+
+ALTER TABLE draft_accounts
+RENAME COLUMN created_by TO submitted_by;
+
+ALTER TABLE draft_accounts
+RENAME COLUMN account_summary_data TO account_snapshot;
+
+ALTER TABLE draft_accounts
+ADD COLUMN account_number        varchar(25);
+
+COMMENT ON COLUMN draft_accounts.account_number IS 'The Opal Account Number (2char letter code+account number)';
+COMMENT ON COLUMN draft_accounts.account_id IS 'Account ID created on validation';
+COMMENT ON COLUMN draft_accounts.created_date IS 'Date this record was first created (the created date is not updated by successive submits, only the submitted by)';
