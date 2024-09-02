3d3196709fa1b33da6863e15ebe79299c01aedc2
DeclanClarkeCGI <142809814+DeclanClarkeCGI@users.noreply.github.com>
2024-09-02 10:19:34 +0100

    add more global exception handling

---
+++ src/main/java/uk/gov/hmcts/opal/controllers/exception/DataLookUpError.java
@@ -0,0 +1,27 @@
+package uk.gov.hmcts.opal.controllers.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import uk.gov.hmcts.opal.exception.OpalApiError;
+
+@Getter
+@RequiredArgsConstructor
+public enum DataLookUpError implements OpalApiError {
+
+    NO_CONTENT_FOUND_AT_URI("100",
+        HttpStatus.NOT_FOUND,
+        "The Resource identified by the provided URI cannot be found");
+
+    private static final String ERROR_TYPE_PREFIX = "DATA_LOOK_UP";
+
+    private final String errorTypeNumeric;
+    private final HttpStatus httpStatus;
+    private final String title;
+
+    @Override
+    public String getErrorTypePrefix() {
+        return ERROR_TYPE_PREFIX;
+    }
+
+}
--- src/test/java/uk/gov/hmcts/opal/controllers/advice/GlobalExceptionHandlerTest.java
+++ src/test/java/uk/gov/hmcts/opal/controllers/advice/GlobalExceptionHandlerTest.java
@@ -1,20 +1,32 @@
 package uk.gov.hmcts.opal.controllers.advice;
 
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
+import org.hibernate.PropertyValueException;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
+import org.springframework.dao.InvalidDataAccessResourceUsageException;
+import org.springframework.http.HttpInputMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import uk.gov.hmcts.opal.authentication.exception.AuthenticationError;
 import uk.gov.hmcts.opal.authentication.exception.MissingRequestHeaderException;
 import uk.gov.hmcts.opal.authentication.service.AccessTokenService;
 import uk.gov.hmcts.opal.authorisation.aspect.PermissionNotAllowedException;
 import uk.gov.hmcts.opal.authorisation.model.Permissions;
+import uk.gov.hmcts.opal.exception.OpalApiException;
 import uk.gov.hmcts.opal.launchdarkly.FeatureDisabledException;
+
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
@@ -90,5 +102,74 @@ class GlobalExceptionHandlerTest {
 
         // Assert
         assertEquals(HttpStatus.FORBIDDEN, response.getStatusCode());
+    }
+
+    @Test
+    void handlePropertyValueException() {
+        // Arrange
+        PropertyValueException pve = new PropertyValueException("A Test Message", "DraftAccountEntity", "account");
+        // Act
+        ResponseEntity<Map<String, String>> response = globalExceptionHandler.handlePropertyValueException(pve);
+        // Assert
+        assertEquals(org.htmlunit.http.HttpStatus.IM_A_TEAPOT_418, response.getStatusCode().value());
+    }
+
+    @Test
+    void handleHttpMessageNotReadableException() {
+        // Arrange
+        HttpInputMessage input = Mockito.mock(HttpInputMessage.class);
+        HttpMessageNotReadableException hmnre = new HttpMessageNotReadableException("A Test Message", input);
+        // Act
+        ResponseEntity<Map<String, String>> response = globalExceptionHandler
+            .handleHttpMessageNotReadableException(hmnre);
+        // Assert
+        assertEquals(org.htmlunit.http.HttpStatus.IM_A_TEAPOT_418, response.getStatusCode().value());
+    }
+
+    @Test
+    void handleInvalidDataAccessApiUsageException() {
+        // Arrange
+        InvalidDataAccessApiUsageException idaaue = new InvalidDataAccessApiUsageException("A Test Message");
+        // Act
+        ResponseEntity<Map<String, String>> response = globalExceptionHandler
+            .handleInvalidDataAccessApiUsageException(idaaue);
+        // Assert
+        assertEquals(org.htmlunit.http.HttpStatus.IM_A_TEAPOT_418, response.getStatusCode().value());
+    }
+
+    @Test
+    void handleInvalidDataAccessResourceUsageException() {
+        // Arrange
+        InvalidDataAccessResourceUsageException idarue = new InvalidDataAccessResourceUsageException("A Test Message");
+        // Act
+        ResponseEntity<Map<String, String>> response = globalExceptionHandler
+            .handleInvalidDataAccessResourceUsageException(idarue);
+        // Assert
+        assertEquals(org.htmlunit.http.HttpStatus.IM_A_TEAPOT_418, response.getStatusCode().value());
+    }
+
+    @Test
+    void handleEntityNotFoundException_ReturnsNotFound() {
+        EntityNotFoundException ex = new EntityNotFoundException("Entity not found");
+        ResponseEntity<Map<String, String>> response = globalExceptionHandler.handleEntityNotFoundException(ex);
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals("Entity not found", response.getBody().get(GlobalExceptionHandler.ERROR_MESSAGE));
+    }
+
+    @Test
+    void handleOpalApiException_ReturnsInternalServerError() {
+        OpalApiException ex = new OpalApiException(AuthenticationError.FAILED_TO_PARSE_ACCESS_TOKEN,
+                                                   "Internal Server Error");
+        ResponseEntity<Map<String, String>> response = globalExceptionHandler.handleOpalApiException(ex);
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+        assertEquals("Failed to parse access token. Internal Server Error", response.getBody().get("message"));
+    }
+
+    @Test
+    void handleHttpMediaTypeNotAcceptableException_ReturnsNotAcceptable() {
+        HttpMediaTypeNotAcceptableException ex = new HttpMediaTypeNotAcceptableException("Not acceptable");
+        ResponseEntity<Map<String, String>> response = globalExceptionHandler
+            .handleHttpMediaTypeNotAcceptableException(ex);
+        assertEquals(HttpStatus.NOT_ACCEPTABLE, response.getStatusCode());
     }
 }
