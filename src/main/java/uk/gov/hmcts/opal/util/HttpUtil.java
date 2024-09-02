f59bb399826e4f0aa11943d72feb5374436e2f54
DeclanClarkeCGI <142809814+DeclanClarkeCGI@users.noreply.github.com>
2024-09-02 10:17:19 +0100

    change nocontent to notfound

--- src/main/java/uk/gov/hmcts/opal/util/HttpUtil.java
+++ src/main/java/uk/gov/hmcts/opal/util/HttpUtil.java
@@ -2,24 +2,36 @@ package uk.gov.hmcts.opal.util;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import uk.gov.hmcts.opal.authentication.service.AccessTokenService;
 
 import java.util.List;
 import java.util.Optional;
 
+@SuppressWarnings("unchecked")
 public class HttpUtil {
 
-    public static <T> ResponseEntity<List<T>> buildResponse(List<T> contents) {
-        if (contents == null || contents.isEmpty()) {
-            return ResponseEntity.noContent().build();
-        }
+    private static final String NOT_FOUND_MESSAGE =
+        """
+    { "error": "Not Found", "message": "No resource found at provided URI"}""";
 
+    private static final MultiValueMap<String, String> HEADERS;
+
+    static {
+        HEADERS = new LinkedMultiValueMap<>();
+        HEADERS.add("content-type", "application/json");
+    }
+
+    public static <T> ResponseEntity<List<T>> buildResponse(List<T> contents) {
+        //return list even if empty
         return ResponseEntity.ok(contents);
     }
 
     public static <T> ResponseEntity<T> buildResponse(T contents) {
         if (contents == null) {
-            return ResponseEntity.noContent().build();
+            contents = (T) NOT_FOUND_MESSAGE;
+            return new ResponseEntity<>(contents, HEADERS, HttpStatus.NOT_FOUND);
         }
 
         return ResponseEntity.ok(contents);
@@ -27,7 +39,8 @@ public class HttpUtil {
 
     public static <T> ResponseEntity<T> buildCreatedResponse(T contents) {
         if (contents == null) {
-            return ResponseEntity.noContent().build();
+            contents = (T) NOT_FOUND_MESSAGE;
+            return new ResponseEntity<>(contents, HEADERS, HttpStatus.NOT_FOUND);
         }
 
         return new ResponseEntity<>(contents, HttpStatus.CREATED);
