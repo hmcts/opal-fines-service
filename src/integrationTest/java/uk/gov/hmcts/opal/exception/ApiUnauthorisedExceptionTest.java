514013e97a4ad9e9a0100000ed788b526aa5c19a
DeclanClarkeCGI <142809814+DeclanClarkeCGI@users.noreply.github.com>
2024-09-03 09:35:32 +0100

    remove test container

--- src/integrationTest/java/uk/gov/hmcts/opal/exception/ApiUnauthorisedExceptionTest.java
+++ src/integrationTest/java/uk/gov/hmcts/opal/exception/ApiUnauthorisedExceptionTest.java
@@ -1,17 +1,10 @@
 package uk.gov.hmcts.opal.exception;
 
-import com.github.dockerjava.api.model.ExposedPort;
-import com.github.dockerjava.api.model.HostConfig;
-import com.github.dockerjava.api.model.PortBinding;
-import com.github.dockerjava.api.model.Ports;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -19,27 +12,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@Testcontainers
 public class ApiUnauthorisedExceptionTest {
 
     @Autowired
     private MockMvc mockMvc;
-
-    private static final int DB_HOST_PORT = 5432;
-    private static final int DB_EXPOSED_PORT = 5432;
-    private static final PortBinding DB_PORT_BINDING =
-        new PortBinding(Ports.Binding.bindPort(DB_HOST_PORT), new ExposedPort(DB_EXPOSED_PORT));
-
-    @Container
-    public static PostgreSQLContainer<?> postgresContainer =
-        new PostgreSQLContainer<>("postgres:15-alpine")
-            .withCreateContainerCmdModifier(cmd ->
-                                                cmd.withHostConfig(new HostConfig().withPortBindings(DB_PORT_BINDING)))
-            .withExposedPorts(DB_EXPOSED_PORT)
-            .withDatabaseName("opal-fines-db")
-            .withUsername("opal-fines")
-            .withPassword("opal-fines")
-            .withReuse(true);
 
     @Test
     void shouldReturn401WhenUnauthorized() throws Exception {
