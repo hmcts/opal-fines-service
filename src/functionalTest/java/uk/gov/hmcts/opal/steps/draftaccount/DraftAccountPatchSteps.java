3dc8ab6b9c9e5f5d2ad8be35d88fdc603ec84226
CadeFaulkner <cade.faulkner@hmcts.net>
2024-10-07 14:42:51 +0100

    tests for PO-745 and checkstyle changes

---
+++ src/functionalTest/java/uk/gov/hmcts/opal/steps/draftaccount/DraftAccountPatchSteps.java
@@ -0,0 +1,48 @@
+package uk.gov.hmcts.opal.steps.draftaccount;
+
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.When;
+import net.serenitybdd.rest.SerenityRest;
+import org.json.JSONException;
+import org.json.JSONObject;
+import uk.gov.hmcts.opal.steps.BaseStepDef;
+import uk.gov.hmcts.opal.utils.DraftAccountUtils;
+
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+
+import static uk.gov.hmcts.opal.steps.BearerTokenStepDef.getToken;
+
+public class DraftAccountPatchSteps extends BaseStepDef {
+    @When("I patch the draft account with the following details")
+    public void patchDraftAccount(DataTable data) throws JSONException {
+        Map<String, String> dataToPatch = data.asMap(String.class, String.class);
+        JSONObject patchBody = new JSONObject();
+        JSONObject timelineData = new JSONObject();
+
+        patchBody.put("account_status", dataToPatch.get("account_status"));
+        patchBody.put("validated_by", dataToPatch.get("validated_by"));
+
+        timelineData.put("username", dataToPatch.get("validated_by"));
+        timelineData.put("status", dataToPatch.get("account_status"));
+
+        ZonedDateTime currentDateTime = ZonedDateTime.now();
+        timelineData.put("status_date", currentDateTime.format(DateTimeFormatter.ISO_INSTANT));
+
+        if (dataToPatch.containsKey("reason_text")) {
+            timelineData.put("reason_text", dataToPatch.get("reason_text"));
+        }
+        patchBody.put("timeline_data", timelineData);
+
+        String draftAccountId = DraftAccountUtils.getAllDraftAccountIds().getFirst();
+        SerenityRest
+            .given()
+            .header("Authorization", "Bearer " + getToken())
+            .accept("*/*")
+            .contentType("application/json")
+            .body(patchBody.toString())
+            .when()
+            .patch(getTestUrl() + "/draft-accounts/" + draftAccountId);
+    }
+}
