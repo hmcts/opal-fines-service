f1120906fa024d6900e8207c4f87c7fc1b810092
ian-readman <ian.readman@hmcts.net>
2024-10-10 09:24:28 +0100

    PO-666 Create ERROR_MESSAGES table

---
+++ src/main/resources/db/migration/V20241010_266__error_messages.sql
@@ -0,0 +1,24 @@
+/**
+* OPAL Program
+*
+* MODULE      : error_messages.sql
+*
+* DESCRIPTION : Create the ERROR_MESSAGES table.
+*
+* VERSION HISTORY:
+*
+* Date          Author       Version     Nature of Change
+* ----------    --------     --------    -------------------------------------
+* 10/10/2024    I Readman    1.0         PO-666 Create the ERROR_MESSAGES table
+*
+**/
+
+CREATE TABLE error_messages
+(
+ error_code           bigint          not null
+,error_message        varchar(50)     not null
+,CONSTRAINT em_error_code_pk PRIMARY KEY (error_code)
+);
+
+COMMENT ON COLUMN error_messages.error_code IS 'Unique ID of the user defined error message';
+COMMENT ON COLUMN error_messages.error_message IS 'Descriptive wording of the error message';
