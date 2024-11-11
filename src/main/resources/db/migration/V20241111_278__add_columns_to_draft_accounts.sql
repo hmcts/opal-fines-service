3f6f2b6e29999ca638ff97c9ee38b64be98e7187
abrahamdennis <abraham.dennis@hmcts.net>
2024-11-11 08:53:32 +0000

    PO-937 Add new columns submitted_by_username, account_status_date, status_message to the DRAFT_ACCOUNTS table

---
+++ src/main/resources/db/migration/V20241111_278__add_columns_to_draft_accounts.sql
@@ -0,0 +1,24 @@
+/**
+* OPAL Program
+*
+* MODULE      : add_columns_to draft_accounts.sql
+*
+* DESCRIPTION : Add new columns submitted_by_username, account_status_date, status_message to the DRAFT_ACCOUNTS table
+*
+* VERSION HISTORY:
+*
+* Date          Author      Version     Nature of Change
+* ----------    -------     --------    -------------------------------------------------------------------------------------------------------------
+* 11/11/2024    A Dennis    1.0         PO-937 Add new columns submitted_by_username, account_status_date, status_message to the DRAFT_ACCOUNTS table
+*
+**/
+
+ALTER TABLE draft_accounts 
+ADD COLUMN submitted_by_username varchar(100) NOT NULL,
+ADD COLUMN account_status_date timestamp NOT NULL,
+ADD COLUMN status_message text;
+
+COMMENT ON COLUMN draft_accounts.submitted_by_username IS 'Name value of the submitting user from the AAD Access Token';
+COMMENT ON COLUMN draft_accounts.account_status_date IS 'The date of update of account status';
+COMMENT ON COLUMN draft_accounts.status_message IS 'Any system messages, warnings, related to the status';
+COMMENT ON COLUMN draft_accounts.account_status IS 'One of Submitted, Resubmitted, Rejected, Deleted, Approved, Pending, Error in Publishing';
