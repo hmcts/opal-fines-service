158d9a054dffb3f9d509266cde19726e500b6fac
DeclanClarkeCGI <142809814+DeclanClarkeCGI@users.noreply.github.com>
2024-12-05 16:44:31 +0000

    Checkstyle !!!

--- src/integrationTest/java/uk/gov/hmcts/opal/SingletonPostgreSQLContainer.java
+++ src/integrationTest/java/uk/gov/hmcts/opal/SingletonPostgreSQLContainer.java
@@ -4,17 +4,17 @@ import org.testcontainers.containers.PostgreSQLContainer;
 
 public class SingletonPostgreSQLContainer {
 
-        private static final PostgreSQLContainer<?> INSTANCE =
-            new PostgreSQLContainer<>("postgres:17.0")
-                .withDatabaseName("testdb")
-                .withUsername("testuser")
-                .withPassword("testpass");
+    private static final PostgreSQLContainer<?> INSTANCE =
+        new PostgreSQLContainer<>("postgres:17.0")
+            .withDatabaseName("testdb")
+            .withUsername("testuser")
+            .withPassword("testpass");
 
-        static {
-            INSTANCE.start(); // Start the container when the class is loaded
-        }
-
-        public static PostgreSQLContainer<?> getInstance() {
-            return INSTANCE;
-        }
+    static {
+        INSTANCE.start(); // Start the container when the class is loaded
     }
+
+    public static PostgreSQLContainer<?> getInstance() {
+        return INSTANCE;
+    }
+}
