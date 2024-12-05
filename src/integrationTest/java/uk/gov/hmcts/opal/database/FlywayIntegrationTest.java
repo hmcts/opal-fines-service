d39d4ba4a432892fd1436fcc56c5fb9c17ff2a1a
abrahamdennis <148991960+abrahamdennis@users.noreply.github.com>
2024-12-05 17:00:51 +0000

    PO-1012 create FLYWAY LOCATIONS to manage code deployment into databa… (#659)
    * PO-1012 create FLYWAY LOCATIONS to manage code deployment into database environments
    
    * introduced FLYWAY_LOCATIONS property
    
    * remove script
    
    * remove debug logging for flyway
    
    * Flyway changes and base test container for integration  tests
    
    * Checkstyle !!!
    
    ---------
    
    Co-authored-by: DeclanClarkeCGI <142809814+DeclanClarkeCGI@users.noreply.github.com>

---
+++ src/integrationTest/java/uk/gov/hmcts/opal/database/FlywayIntegrationTest.java
@@ -0,0 +1,33 @@
+package uk.gov.hmcts.opal.database;
+
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.MigrationInfo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import uk.gov.hmcts.opal.BaseIntegrationTest;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+@ActiveProfiles("integration")
+public class FlywayIntegrationTest extends BaseIntegrationTest {
+
+    @Autowired
+    Flyway flyway;
+
+    @Test
+    public void testFlywayMigrationsDev() {
+
+        List<MigrationInfo> migrations = Arrays.asList(flyway.info().all());
+        assertTrue(migrations.stream().anyMatch(m -> m.getScript().contains("_001_")));
+        assertTrue(migrations.stream().anyMatch(m -> m.getScript().contains("_005_")));
+
+    }
+}
+
+
