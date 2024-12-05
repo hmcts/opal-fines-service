a7db93a5bd5a9d39ee0331bede50d3f8bb04c8d7
DeclanClarkeCGI <142809814+DeclanClarkeCGI@users.noreply.github.com>
2024-12-05 16:18:24 +0000

    Flyway changes and base test container for integration  tests

---
+++ src/integrationTest/java/uk/gov/hmcts/opal/SingletonPostgreSQLContainer.java
@@ -0,0 +1,20 @@
+package uk.gov.hmcts.opal;
+
+import org.testcontainers.containers.PostgreSQLContainer;
+
+public class SingletonPostgreSQLContainer {
+
+        private static final PostgreSQLContainer<?> INSTANCE =
+            new PostgreSQLContainer<>("postgres:17.0")
+                .withDatabaseName("testdb")
+                .withUsername("testuser")
+                .withPassword("testpass");
+
+        static {
+            INSTANCE.start(); // Start the container when the class is loaded
+        }
+
+        public static PostgreSQLContainer<?> getInstance() {
+            return INSTANCE;
+        }
+    }
