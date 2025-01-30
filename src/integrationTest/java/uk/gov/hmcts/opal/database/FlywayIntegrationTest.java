74a8f6f859f4c815edbf805bf678227912995359
DeclanClarkeCGI <142809814+DeclanClarkeCGI@users.noreply.github.com>
2025-01-30 15:31:38 +0000

    Introduce test container adn update draft accounts integ test to use it

--- src/integrationTest/java/uk/gov/hmcts/opal/database/FlywayIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/opal/database/FlywayIntegrationTest.java
@@ -4,17 +4,17 @@ import org.flywaydb.core.Flyway;
 import org.flywaydb.core.api.MigrationInfo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import uk.gov.hmcts.opal.AbstractIntegrationTest;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
+
 @ActiveProfiles("integration")
-public class FlywayIntegrationTest {
+public class FlywayIntegrationTest extends AbstractIntegrationTest {
 
     @Autowired
     Flyway flyway;
