c34e9a2f2f20aa551f522a4538094292569b418f
DeclanClarkeCGI <142809814+DeclanClarkeCGI@users.noreply.github.com>
2025-01-30 15:49:31 +0000

    Introduce test container adn update draft accounts integ test to use it

--- src/integrationTest/java/uk/gov/hmcts/opal/openapi/OpenAPIPublisherTest.java
+++ src/integrationTest/java/uk/gov/hmcts/opal/openapi/OpenAPIPublisherTest.java
@@ -4,8 +4,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import uk.gov.hmcts.opal.AbstractIntegrationTest;
 
 import java.io.OutputStream;
 import java.nio.file.Files;
@@ -19,8 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * Each CI run on master should automatically save and upload (if updated) documentation.
  */
 @AutoConfigureMockMvc
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class OpenAPIPublisherTest {
+class OpenAPIPublisherTest extends AbstractIntegrationTest {
 
     @Autowired
     private MockMvc mvc;
