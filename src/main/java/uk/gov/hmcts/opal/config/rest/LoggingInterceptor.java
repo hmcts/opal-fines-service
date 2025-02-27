efdbb76b21b990b6b3b3b4114d7659e9644bb990
DeclanClarkeCGI <142809814+DeclanClarkeCGI@users.noreply.github.com>
2025-02-27 09:23:44 +0000

    more logging to debug pipeline issue

--- src/main/java/uk/gov/hmcts/opal/config/rest/LoggingInterceptor.java
+++ src/main/java/uk/gov/hmcts/opal/config/rest/LoggingInterceptor.java
@@ -2,14 +2,17 @@ package uk.gov.hmcts.opal.config.rest;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpRequest;
-import org.springframework.http.client.*;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
 
 import java.io.IOException;
 
 @Slf4j
 public class LoggingInterceptor implements ClientHttpRequestInterceptor {
     @Override
-    public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+    public ClientHttpResponse intercept(HttpRequest request, byte[] body,
+                                        ClientHttpRequestExecution execution) throws IOException {
         log.info("Request: {} {}", request.getMethod(), request.getURI());
         log.info("Headers: {}", request.getHeaders());
         log.info("Body: {}", new String(body));
