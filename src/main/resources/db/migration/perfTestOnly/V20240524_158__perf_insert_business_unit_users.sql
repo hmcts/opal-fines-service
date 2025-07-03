18fa46806470c9b51e386fd6de11ada8d4709b3d
Abraham Dennis <abraham.dennis@hmcts.net>
2025-07-03 20:13:12 +0100

    PO-1921 initial load of database scripts for perfTestOnly FLYWAY folder

---
+++ src/main/resources/db/migration/perfTestOnly/V20240524_158__perf_insert_business_unit_users.sql
@@ -0,0 +1,144 @@
+/**
+* OPAL Program
+*
+* MODULE      : insert_business_unit_users.sql
+*
+* DESCRIPTION : Inserts rows of data into the BUSINESS_UNIT_USERS table to match that in Legacy GoB. 
+*
+* VERSION HISTORY:
+*
+* Date          Author      Version     Nature of Change
+* ----------    -------     --------    ---------------------------------------------------------------------------------------------------------
+* 24/05/2024    A Dennis    1.0         PO-380 Inserts rows of data into the BUSINESS_UNIT_USERS table to match that in Legacy GoB
+*
+**/
+
+INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L047SA'
+  ,43
+  ,500000003
+  );
+
+INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L111SA'
+  ,74
+  ,500000003
+  );
+
+INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L065JG'
+  ,70
+  ,500000000
+  );
+
+INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L066JG'
+  ,68
+  ,500000000
+  );
+
+INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L067JG'
+  ,73
+  ,500000000
+  );
+
+INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L073JG'
+  ,71
+  ,500000000
+  );
+
+INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L077JG'
+  ,67
+  ,500000000
+  );
+
+INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L078JG'
+  ,69
+  ,500000000
+  );
+
+INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L080JG'
+  ,61
+  ,500000000
+  );
+
+INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L026SH'
+  ,60
+  ,500000002
+  );
