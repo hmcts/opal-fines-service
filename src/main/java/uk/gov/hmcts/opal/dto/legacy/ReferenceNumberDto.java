8209dbfb48457bbfa101efee627b31ccbb972a91
Aneesah33 <aneesah.khan@hmcts.net>
2025-08-05 15:12:33 +0100

    PO-1901- referenceNumberDto changed the annotations to match the request data items ( added @NotNull for mandotary fields)

--- src/main/java/uk/gov/hmcts/opal/dto/legacy/ReferenceNumberDto.java
+++ src/main/java/uk/gov/hmcts/opal/dto/legacy/ReferenceNumberDto.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.opal.dto.legacy;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,11 +16,14 @@ import lombok.NoArgsConstructor;
 public class ReferenceNumberDto {
 
     @JsonProperty("organisation")
+    @NotNull
     private Boolean organisation;
 
     @JsonProperty("account_number")
+    @NotNull
     private String accountNumber;
 
     @JsonProperty("prosecutor_case_reference")
+    @NotNull
     private String prosecutorCaseReference;
 }
