4c96f18ea0ba40803025c013baf10633840a2115
ShanCGI <shantanu.datta@hmcts.net>
2025-08-13 13:01:46 +0100

    PO-1564 - DefendantAccountAtAGlance

---
+++ src/main/java/uk/gov/hmcts/opal/dto/DefendantAccountAtAGlanceResponseDto.java
@@ -0,0 +1,15 @@
+package uk.gov.hmcts.opal.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class DefendantAccountAtAGlanceResponseDto implements ToJsonString{
+}
