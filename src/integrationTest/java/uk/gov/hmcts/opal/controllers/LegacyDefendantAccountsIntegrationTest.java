0b06e8d9081fb7ce9b0ee123ba3ff9bbc91ea7b5
Russell Dodd <russell.dodd@hmcts.net>
2025-08-13 14:44:02 +0100

    PO-1907 Enable previously disabled legacy integration tests for Get Header Summary.

--- src/integrationTest/java/uk/gov/hmcts/opal/controllers/LegacyDefendantAccountsIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/opal/controllers/LegacyDefendantAccountsIntegrationTest.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.opal.controllers;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -9,13 +8,11 @@ import org.springframework.test.context.ActiveProfiles;
 @Slf4j(topic = "opal.LegacyDefendantAccountsIntegrationTest")
 class LegacyDefendantAccountsIntegrationTest extends DefendantAccountsControllerIntegrationTest {
 
-    @Disabled("See DTSPO-27066. A running instance of Legacy Stub App is required to execute this test")
     @Test
     void testGetHeaderSummary() throws Exception {
         super.getHeaderSummaryImpl(log);
     }
 
-    @Disabled("See DTSPO-27066. A running instance of Legacy Stub App is required to execute this test")
     @Test
     void testGetHeaderSummary_500Error() throws Exception {
         super.getHeaderSummaryImpl_500Error(log);
