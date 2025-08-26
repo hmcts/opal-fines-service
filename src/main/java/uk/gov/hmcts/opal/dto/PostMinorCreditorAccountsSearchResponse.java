41ebdc313b6c4cfcc8e0ca23c748a9d9d007eef5
marcmollins <130585648+marcmollins@users.noreply.github.com>
2025-08-26 09:23:10 +0100

    PO 1902 search minor creditors post (#1053)
    * 1PO-1902-Search-Minor-Creditors-POST - committing initial changes
    
    * 1PO-1902-Search-Minor-Creditors-POST - Updating files to follow example of PO-1907.
    
    * 1PO-1902-Search-Minor-Creditors-POST - updated function name and deleted unused test class
    
    * 1PO-1902-Search-Minor-Creditors-POST - Updated the file due to issues with Checkstyle
    
    * 1PO-1902-Search-Minor-Creditors-POST - Updated files due to issues with Checkstyle
    
    * 1PO-1902-Search-Minor-Creditors-POST - Updated files due to issues with Checkstyle
    
    * 1PO-1902-Search-Minor-Creditors-POST - Updated files due to issues with Checkstyle
    
    * 1PO-1902-Search-Minor-Creditors-POST - Updated logging
    
    * 1PO-1902-Search-Minor-Creditors-POST - Updated test data
    
    * 1PO-1902-Search-Minor-Creditors-POST - updating variable
    
    * 1PO-1902-Search-Minor-Creditors-POST
    
    Fixed all issues with integration tests and updated entities
    
    * 1PO-1902-Search-Minor-Creditors-POST
    
    Disabling tests
    
    * 1PO-1902-Search-Minor-Creditors-POST
    
    updating unit tests
    
    * PO-1902-Search-Minor-Creditors-POST
    
    fixing checkstyle
    
    * PO-1902-Search-Minor-Creditors-POST
    
    fixing tests
    
    * PO-1902-Search-Minor-Creditors-POST
    
    fixing tests
    
    * PO-1902-Search-Minor-Creditors-POST
    
    updating service with null schema file
    
    * PO-1902-Search-Minor-Creditors-POST
    
    updating test coverage
    
    * PO-1902-Search-Minor-Creditors-POST
    
    updating test coverage
    
    * PO-1902-Search-Minor-Creditors-POST
    
    updating test coverage
    
    * PO-1902-Search-Minor-Creditors-POST
    
    fixing sonarcube issues
    
    * PO-1902-Search-Minor-Creditors-POST
    
    updated Root XML element to be response. I have updated Opal debug logs and exception message to alert that the search has not yet been implemented.
    
    * PO-1902-Search-Minor-Creditors-POST
    
    updated Root XML element to be response. I have updated Opal debug logs and exception message to alert that the search has not yet been implemented.
    
    * PO-1902-Search-Minor-Creditors-POST
    
    moved Creditor and MinorCreditorSearch to the DTO package from entity package.
    
    ---------
    
    Co-authored-by: jonathanDuffy <jonathan.duffy@cgi.com>

---
+++ src/main/java/uk/gov/hmcts/opal/dto/PostMinorCreditorAccountsSearchResponse.java
@@ -0,0 +1,27 @@
+package uk.gov.hmcts.opal.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class PostMinorCreditorAccountsSearchResponse implements ToJsonString {
+
+    @JsonProperty("count")
+    private int count;
+
+    @JsonProperty("creditor_accounts")
+    private List<CreditorAccountDto> creditorAccounts;
+}
