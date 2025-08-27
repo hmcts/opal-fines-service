162f05eece8d8d2b6a749138e211b6c2d93c77b4
Aneesah33 <aneesah.khan@hmcts.net>
2025-08-27 11:22:43 +0100

    Refactor: move legacy/common DTOs, schemas, and service mapping into new branch

---
+++ src/main/java/uk/gov/hmcts/opal/dto/legacy/common/LegacyPartyDetails.java
@@ -0,0 +1,30 @@
+package uk.gov.hmcts.opal.dto.legacy.common;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@XmlRootElement(name = "party_details")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class LegacyPartyDetails {
+    @XmlElement(name = "defendant_account_party_id")
+    private String defendantAccountPartyId;
+
+    @XmlElement(name = "organisation_flag")
+    private Boolean organisationFlag;
+
+    @XmlElement(name = "organisation_details")
+    private OrganisationDetails organisationDetails;
+
+    @XmlElement(name = "individual_details")
+    private IndividualDetails individualDetails;
+}
