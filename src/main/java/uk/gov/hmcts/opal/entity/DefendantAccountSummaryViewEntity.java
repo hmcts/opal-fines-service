f9b1bc9b3ae26874277a1732a1d82c4d05dc0c44
ShanCGI <223634236+ShanCGI@users.noreply.github.com>
2025-08-28 13:34:57 +0100

    PO-1564 - DefendantAccountAtAGlance - ViewEntity

---
+++ src/main/java/uk/gov/hmcts/opal/entity/DefendantAccountSummaryViewEntity.java
@@ -0,0 +1,165 @@
+package uk.gov.hmcts.opal.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "v_defendant_accounts_summary")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DefendantAccountSummaryViewEntity {
+
+    @Id
+    @Column(name = "defendant_account_id", nullable = false)
+    private Long defendantAccountId;
+
+    @Column(name = "version_number", nullable = false)
+    private Long versionNumber;
+
+    @Column(name = "account_number", nullable = false)
+    private String accountNumber;
+
+    @Column(name = "debtor_type", nullable = false)
+    private String debtorType;
+
+    @Column(name = "last_enforcement", nullable = false)
+    private String lastEnforcement;
+
+    @Column(name = "last_enf_title", nullable = false)
+    private String lastEnforcementTitle;
+
+    @Column(name = "collection_order")
+    private Boolean collectionOrder;
+
+    @Column(name = "account_comments")
+    private String accountComments;
+
+    @Column(name = "account_note_1")
+    private String accountNote1;
+
+    @Column(name = "account_note_2")
+    private String accountNote2;
+
+    @Column(name = "account_note_3")
+    private String accountNote3;
+
+    // Enforcement Status
+
+    @Column(name = "jail_days")
+    private Integer jailDays;
+
+    @Column(name = "enf_override_result_id")
+    private String enforcementOverrideResultId;
+
+    @Column(name = "enforcer_id")
+    private Long enforcerId;
+
+    @Column(name = "enforcer_name")
+    private String enforcerName;
+
+    @Column(name = "lja_id")
+    private String ljaId;
+
+    @Column(name = "lja_name")
+    private String ljaName;
+
+    @Column(name = "enf_override_title")
+    private String enforcementOverrideTitle;
+
+    @Column(name = "last_movement_date")
+    private LocalDateTime lastMovementDate;
+
+    @Column(name = "document_language")
+    private String documentLanguage;
+
+    @Column(name = "hearing_language")
+    private String hearingLanguage;
+
+    // Party Details
+
+    @Column(name = "party_id")
+    private Long partyId;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "forenames")
+    private String forenames;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "birth_date")
+    private LocalDateTime birthDate;
+
+    @Column(name = "age")
+    private Integer age;
+
+    @Column(name = "organisation")
+    private Boolean organisation;
+
+    @Column(name = "organisation_name")
+    private String organisationName;
+
+    @Column(name = "address_line_1")
+    private String addressLine1;
+
+    @Column(name = "address_line_2")
+    private String addressLine2;
+
+    @Column(name = "address_line_3")
+    private String addressLine3;
+
+    @Column(name = "address_line_4")
+    private String addressLine4;
+
+    @Column(name = "address_line_5")
+    private String addressLine5;
+
+    @Column(name = "postcode")
+    private String postcode;
+
+    @Column(name = "national_insurance_number")
+    private String nationalInsuranceNumber;
+
+    @Column(name = "alias_id")
+    private String aliasId;
+
+    @Column(name = "sequence_number")
+    private Integer sequenceNumber;
+
+    @Column(name = "alias_org_name")
+    private String aliasOrgName;
+
+    @Column(name = "alias_forenames")
+    private String aliasForenames;
+
+    @Column(name = "alias_surname")
+    private String aliasSurname;
+
+    @Column(name = "terms_type_code")
+    private String termsTypeCode;
+
+    @Column(name = "instalment_period")
+    private String instalmentPeriod;
+
+    @Column(name = "instalment_amount")
+    private BigDecimal instalmentAmount;
+
+    @Column(name = "instalment_lump_sum")
+    private BigDecimal instalmentLumpSum;
+
+    @Column(name = "effective_date")
+    private LocalDateTime effectiveDate;
+}
