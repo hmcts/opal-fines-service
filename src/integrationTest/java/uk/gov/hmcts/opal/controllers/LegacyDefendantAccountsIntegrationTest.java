4c72641a49e3395b841b21444161d0b7807df13e
Jonathan.Duffy <Jonathan.Duffy@cgi.com>
2025-08-29 15:19:44 +0100

    Completed Testing of Legacy Tests

--- src/integrationTest/java/uk/gov/hmcts/opal/controllers/LegacyDefendantAccountsIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/opal/controllers/LegacyDefendantAccountsIntegrationTest.java
@@ -1,28 +1,34 @@
 package uk.gov.hmcts.opal.controllers;
 
-import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.context.ActiveProfiles;
+
+import lombok.extern.slf4j.Slf4j;
 
 @ActiveProfiles({"integration", "legacy"})
 @Slf4j(topic = "opal.LegacyDefendantAccountsIntegrationTest")
 class LegacyDefendantAccountsIntegrationTest extends DefendantAccountsControllerIntegrationTest {
 
+    @Disabled("A running instance of Legacy Stub App is required to execute this test")
     @Test
     void testGetHeaderSummary() throws Exception {
         super.getHeaderSummaryImpl(log);
     }
 
+    @Disabled("A running instance of Legacy Stub App is required to execute this test")
     @Test
     void testGetHeaderSummary_500Error() throws Exception {
         super.getHeaderSummaryImpl_500Error(log);
     }
 
+    @Disabled("A running instance of Legacy Stub App is required to execute this test")
     @Test
     void testSearchDefendantAccounts() throws Exception {
         super.testPostDefendantAccountsSearch(log);
     }
 
+    @Disabled("A running instance of Legacy Stub App is required to execute this test")
     @Test
     void testSearchDefendantAccount_NoAccountsFound() throws Exception {
         super.testPostDefendantAccountsSearch_WhenNoDefendantAccountsFound(log);
