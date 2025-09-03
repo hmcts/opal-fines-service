611ced2929568389638de43fed3c22355a38751a
marcmollins <130585648+marcmollins@users.noreply.github.com>
2025-09-03 13:05:50 +0100

    Po 713 search minor creditors post opal mode (#1105)
    * 1PO-1902-Search-Minor-Creditors-POST - committing initial changes
    
    * 1PO-1902-Search-Minor-Creditors-POST - Updating files to follow example of PO-1907.
    
    * 1PO-1902-Search-Minor-Creditors-POST - updated function name and deleted unused test class
    
    * 1PO-1902-Search-Minor-Creditors-POST - Updated the file due to issues with Checkstyle
    
    * 1PO-1902-Search-Minor-Creditors-POST - Updated files due to issues with Checkstyle
    
    * 1PO-1902-Search-Minor-Creditors-POST - Updated files due to issues with Checkstyle
    
    * 1PO-1902-Search-Minor-Creditors-POST - Updated files due to issues with Checkstyle
    
    * 1PO-1902-Search-Minor-Creditors-POST - Updated logging
    
    * 1PO-1902-Search-Minor-Creditors-POST - Updated test data
    
    * 1PO-1902-Search-Minor-Creditors-POST - updating variable
    
    * 1PO-1902-Search-Minor-Creditors-POST
    
    Fixed all issues with integration tests and updated entities
    
    * 1PO-1902-Search-Minor-Creditors-POST
    
    Disabling tests
    
    * 1PO-1902-Search-Minor-Creditors-POST
    
    updating unit tests
    
    * PO-1902-Search-Minor-Creditors-POST
    
    fixing checkstyle
    
    * PO-1902-Search-Minor-Creditors-POST
    
    fixing tests
    
    * PO-1902-Search-Minor-Creditors-POST
    
    fixing tests
    
    * PO-1902-Search-Minor-Creditors-POST
    
    updating service with null schema file
    
    * PO-1902-Search-Minor-Creditors-POST
    
    updating test coverage
    
    * PO-1902-Search-Minor-Creditors-POST
    
    updating test coverage
    
    * PO-1902-Search-Minor-Creditors-POST
    
    updating test coverage
    
    * PO-1902-Search-Minor-Creditors-POST
    
    fixing sonarcube issues
    
    * PO-1902-Search-Minor-Creditors-POST
    
    updated Root XML element to be response. I have updated Opal debug logs and exception message to alert that the search has not yet been implemented.
    
    * PO-1902-Search-Minor-Creditors-POST
    
    updated Root XML element to be response. I have updated Opal debug logs and exception message to alert that the search has not yet been implemented.
    
    * PO-1902-Search-Minor-Creditors-POST
    
    moved Creditor and MinorCreditorSearch to the DTO package from entity package.
    
    * PO-713-Search-Minor-Creditors-POST-Opal-Mode
    
    Initial commit of changes
    
    * PO-713-Search-Minor-Creditors-POST-Opal-Mode
    
    Initial commit of changes
    
    * PO-713-Search-Minor-Creditors-POST-Opal-Mode
    
    keeping branch up to date with changes
    
    * PO-713-Search-Minor-Creditors-POST-Opal-Mode
    
    added new integration tests for Opal mode
    
    * PO-713-Search-Minor-Creditors-POST-Opal-Mode
    
    fixing checkstyle
    
    * PO-713-Search-Minor-Creditors-POST-Opal-Mode
    
    updating files
    
    * Fixed failing integration tests
    
    * PO-713-Search-Minor-Creditors-POST-Opal-Mode
    
    updating test
    
    * PO-713-Search-Minor-Creditors-POST-Opal-Mode
    
    added tests for authentication and authorization
    
    * PO-713-Search-Minor-Creditors-POST-Opal-Mode
    
    checkstyle updates
    
    * PO-713-Search-Minor-Creditors-POST-Opal-Mode
    
    fixing a file I accidentally changed
    
    * PO-713-Search-Minor-Creditors-POST-Opal-Mode
    
    changing account balance to bigDecimal to avoid floating point issues in future
    
    * PO-713-Search-Minor-Creditors-POST-Opal-Mode
    
    updated logic after changing response to use BigDecimal, also fixed tests
    
    * add integration tests for active accounts and multi-parameter searches
    
    * update tests to remove company for the moment
    
    * PO-713-Search-Minor-Creditors-POST-Opal-Mode
    
    updated specs to include exact match for surname, forename and organisation name.
    
    * PO-713-Search-Minor-Creditors-POST-Opal-Mode
    
    fixed checkstyle errors
    
    * Integration tests for company names
    
    * Adding verification that organisation is true
    
    * PO-713-Search-Minor-Creditors-POST-Opal-Mode
    
    updated builder to use correct organisation name
    
    * PO-713-Search-Minor-Creditors-POST-Opal-Mode
    
    checkstyle error
    
    * Add integration tests for "starts with" behavior on company address and postcode
    
    * character limit
    
    ---------
    
    Co-authored-by: benedwards <ben.edwards@cgi.com>
    Co-authored-by: DeclanClarkeCGI <142809814+DeclanClarkeCGI@users.noreply.github.com>
    Co-authored-by: Jonathan.Duffy <Jonathan.Duffy@cgi.com>

--- src/main/java/uk/gov/hmcts/opal/service/opal/OpalMinorCreditorService.java
+++ src/main/java/uk/gov/hmcts/opal/service/opal/OpalMinorCreditorService.java
@@ -1,21 +1,80 @@
 package uk.gov.hmcts.opal.service.opal;
 
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
-import uk.gov.hmcts.opal.dto.PostMinorCreditorAccountsSearchResponse;
+import uk.gov.hmcts.opal.dto.CreditorAccountDto;
+import uk.gov.hmcts.opal.dto.DefendantDto;
 import uk.gov.hmcts.opal.dto.MinorCreditorSearch;
+import uk.gov.hmcts.opal.dto.PostMinorCreditorAccountsSearchResponse;
+import uk.gov.hmcts.opal.entity.minorcreditor.MinorCreditorEntity;
+import uk.gov.hmcts.opal.repository.MinorCreditorRepository;
+import uk.gov.hmcts.opal.repository.jpa.MinorCreditorSpecs;
 import uk.gov.hmcts.opal.service.iface.MinorCreditorServiceInterface;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 @Slf4j(topic = "opal.OpalMinorCreditorService")
 @RequiredArgsConstructor
 public class OpalMinorCreditorService implements MinorCreditorServiceInterface {
 
+    private final MinorCreditorRepository minorCreditorRepository;
+
+    private final MinorCreditorSpecs specs = new MinorCreditorSpecs();
+
     @Override
-    public PostMinorCreditorAccountsSearchResponse searchMinorCreditors(MinorCreditorSearch minorCreditorSearchDto) {
-        log.debug(":searchMinorCreditor: Search not yet implemented");
-        throw new EntityNotFoundException("Search not Implemented");
+    public PostMinorCreditorAccountsSearchResponse searchMinorCreditors(MinorCreditorSearch criteria) {
+        Specification<MinorCreditorEntity> spec =
+            specs.findBySearchCriteria(criteria);
+
+        List<MinorCreditorEntity> results =
+            minorCreditorRepository.findAll(spec);
+
+        return toResponse(results);
+
+    }
+
+    private CreditorAccountDto toCreditorAccountDto(MinorCreditorEntity entity) {
+        return CreditorAccountDto.builder()
+            .creditorAccountId(String.valueOf(entity.getCreditorId()))
+            .accountNumber(entity.getAccountNumber())
+            .organisation(entity.isOrganisation())
+            .organisationName(entity.getOrganisationName()) // confirm if this is creditor org or defendant org
+            .firstnames(entity.getForenames())
+            .surname(entity.getSurname())
+            .addressLine1(entity.getAddressLine1())
+            .postcode(entity.getPostCode())
+            .businessUnitName(entity.getBusinessUnitName())
+            .businessUnitId(String.valueOf(entity.getBusinessUnitId()))
+            .accountBalance(java.util.Optional.ofNullable(entity.getCreditorAccountBalance())
+                                .map(BigDecimal::valueOf)
+                                .orElse(BigDecimal.ZERO))
+            .defendant(toDefendantDto(entity))
+            .build();
+    }
+
+    private DefendantDto toDefendantDto(MinorCreditorEntity entity) {
+        return DefendantDto.builder()
+            .defendantAccountId(entity.getDefendantAccountId() != null
+                                    ? String.valueOf(entity.getDefendantAccountId()) : null)
+            .organisation(entity.isOrganisation()) // or map from a column if you have a defendant_organisation flag
+            .organisationName(entity.getDefendantOrganisationName())
+            .firstnames(entity.getDefendantFornames())
+            .surname(entity.getDefendantSurname())
+            .build();
+    }
+
+    private PostMinorCreditorAccountsSearchResponse toResponse(List<MinorCreditorEntity> entities) {
+        List<CreditorAccountDto> accounts = entities.stream()
+            .map(this::toCreditorAccountDto)
+            .toList();
+
+        return PostMinorCreditorAccountsSearchResponse.builder()
+            .count(accounts.size())
+            .creditorAccounts(accounts.isEmpty() ? null : accounts)
+            .build();
     }
 }
