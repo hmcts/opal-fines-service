4f3abd39057e7105137cfc7a98e96f9c1de2d543
Abraham Dennis <abraham.dennis@hmcts.net>
2025-09-08 16:33:51 +0100

    PO-2083 the intial database scripts for ithcOnly environment

---
+++ src/main/resources/db/migration/ithcOnly/V20240218_085__ithc_insert_business_unit_users.sql
@@ -0,0 +1,416 @@
+/**
+* OPAL Program
+*
+* MODULE      : insert_business_unit_users.sql
+*
+* DESCRIPTION : Inserts rows of data into the BUSINESS_UNIT_USERS table to match that in Legacy GoB. 
+*
+* VERSION HISTORY:
+*
+* Date          Author      Version     Nature of Change
+* ----------    -------     --------    ---------------------------------------------------------------------------------------------------------
+* 18/02/2024    A Dennis    1.0         PO-179 Inserts rows of data into the BUSINESS_UNIT_USERS table to match that in Legacy GoB
+*
+**/
+INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L024US'
+  ,24
+  ,'Suffolk.user'
+  );
+
+INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L013KG'
+  ,13
+  ,'gl.userfour'
+  );
+
+INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L016KG'
+  ,16
+  ,'gl.userfour'
+  );
+
+  INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L017KG'
+  ,17
+  ,'gl.userfour'
+  );
+
+  INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L019KG'
+  ,19
+  ,'gl.userfour'
+  );
+
+  INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L025KG'
+  ,25
+  ,'gl.userfour'
+  );
+
+  INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L032KG'
+  ,32
+  ,'gl.userfour'
+  );
+
+  INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L040KG'
+  ,40
+  ,'gl.userfour'
+  );
+
+  INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L062KG'
+  ,62
+  ,'gl.userfour'
+  );
+
+  INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L063KG'
+  ,63
+  ,'gl.userfour'
+  );
+
+  INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L064KG'
+  ,64
+  ,'gl.userfour'
+  );
+
+  INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L065KG'
+  ,65
+  ,'gl.userfour'
+  );
+
+  INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L066KG'
+  ,66
+  ,'gl.userfour'
+  );
+
+  INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L067KG'
+  ,67
+  ,'gl.userfour'
+  );
+
+  INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L068KG'
+  ,68
+  ,'gl.userfour'
+  );
+
+  INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L069KG'
+  ,69
+  ,'gl.userfour'
+  );
+
+  INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L070KG'
+  ,70
+  ,'gl.userfour'
+  );
+
+  INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L071KG'
+  ,71
+  ,'gl.userfour'
+  );
+
+  INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L072KG'
+  ,72
+  ,'gl.userfour'
+  );
+
+  INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L073KG'
+  ,73
+  ,'gl.userfour'
+  );
+
+  INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L074KG'
+  ,74
+  ,'gl.userfour'
+  );
+
+  INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L075KG'
+  ,75
+  ,'gl.userfour'
+  );
+
+  INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L076KG'
+  ,76
+  ,'gl.userfour'
+  );
+
+  INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L077KG'
+  ,77
+  ,'gl.userfour'
+  );
+
+  INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L078KG'
+  ,78
+  ,'gl.userfour'
+  );
+
+  INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L079KG'
+  ,79
+  ,'gl.userfour'
+  );
+
+  INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L080KG'
+  ,80
+  ,'gl.userfour'
+  );
+
+  INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L081KG'
+  ,81
+  ,'gl.userfour'
+  );
+
+  INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L094GH'
+  ,94
+  ,'humber.usertwo'
+  );
+
+  INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L095GH'
+  ,95
+  ,'humber.usertwo'
+  );
+
+INSERT INTO BUSINESS_UNIT_USERS
+  (
+   business_unit_user_id	         
+  ,business_unit_id	       
+  ,user_id   
+  )
+VALUES
+  (
+   'L096GH'
+  ,96
+  ,'humber.usertwo'
+  );
