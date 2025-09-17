c77358af1b905ff9f0d1f6a65538aaf8d26b443e
marcmollins <Marc.Mollins@HMCTS.NET>
2025-09-17 16:49:01 +0100

    PO-1566-Add-Note
    making sure test runs everywhere

--- src/integrationTest/java/uk/gov/hmcts/opal/controllers/OpalNotesIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/opal/controllers/OpalNotesIntegrationTest.java
@@ -1,12 +1,12 @@
 package uk.gov.hmcts.opal.controllers;
 
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_CLASS;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_CLASS;
+
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
-
-import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_CLASS;
-import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_CLASS;
 
 @ActiveProfiles({"integration", "opal"})
 @Sql(scripts = "classpath:db/insertData/insert_into_defendant_accounts.sql", executionPhase = BEFORE_TEST_CLASS)
