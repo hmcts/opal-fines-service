00a6ffcb20c1e08ab65e18c710df599b4166a66e
marcmollins <Marc.Mollins@HMCTS.NET>
2025-10-14 14:28:26 +0100

    PO-2119-Add-retriable-boolean-to-problem-JSON
    updated the GlobalExceptionHandler to include a retry property (always false currently) and updated tests to check for the property

--- src/main/java/uk/gov/hmcts/opal/controllers/advice/GlobalExceptionHandler.java
+++ src/main/java/uk/gov/hmcts/opal/controllers/advice/GlobalExceptionHandler.java
@@ -504,7 +504,7 @@ public class GlobalExceptionHandler {
         problemDetail.setType(URI.create("https://hmcts.gov.uk/problems/" + typeUri));
         problemDetail.setInstance(URI.create("https://hmcts.gov.uk/problems/instance/" + opalOperationId));
         problemDetail.setProperty("operation_id", opalOperationId);
-        problemDetail.setProperty("retry", retry);
+        problemDetail.setProperty("retryable", retry);
         return problemDetail;
     }
 
