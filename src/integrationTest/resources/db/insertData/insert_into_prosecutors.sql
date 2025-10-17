73fbad5dc1bd671b025fd6277da3f6b3c33e4df9
brumbyp1 <piers.brumby1@hmcts.net>
2025-10-17 11:29:53 +0100

    PO-1722: Add scripts to insert reference data into prosecutors table and increase prosecutors address_line_1 column size (#1266)
    Co-authored-by: jonathanDuffy <jonathan.duffy@cgi.com>

--- src/integrationTest/resources/db/insertData/insert_into_prosecutors.sql
+++ src/integrationTest/resources/db/insertData/insert_into_prosecutors.sql
@@ -8,8 +8,9 @@
 * VERSION HISTORY:
 *
 * Date          Author       Version     Nature of Change
-* ----------    --------     --------    ---------------------------------------------------------------------------------------------------------
+* ----------    --------     --------    --------------------------------------------------------------------------------------------------------------
 * 30/06/2025    R DODD        1.0         PO1787 Insert test data for prosecutors integration tests
+* 15/10/2025    P Brumby      1.1         PO1722 Change test data for prosecutors integration tests so prosecutor_id does not clash with reference data
 *
 **/
 
@@ -22,19 +23,19 @@ prosecutor_id, name, prosecutor_code
 )
 VALUES
 (
-001, 'AA1 Chief Prosecutor', 'AA01'
+001111, 'AA1 Chief Prosecutor', 'AA01'
 , '9 Prosecutor Street', 'Prosecutorville', 'Prosecutorton'
 , NULL, NULL, 'PR01 2PR'
 , NULL
 ),
 (
-002, 'AA2 Assistant Prosecutor', 'AA02'
+002222, 'AA2 Assistant Prosecutor', 'AA02'
 , '49 Prosecutor Street', 'Prosecutorville', 'Prosecutorton'
 , NULL, NULL, 'PR01 4PR'
 , '2099-11-24 10:00:00'
 ),
 (
-003, 'AA3 Junior Prosecutor', 'AA03'
+003333, 'AA3 Junior Prosecutor', 'AA03'
 , '99 Prosecutor Street', 'Prosecutorville', 'Prosecutorton'
 , NULL, NULL, 'PR01 9PR'
 , '2009-04-16 20:00:00'
