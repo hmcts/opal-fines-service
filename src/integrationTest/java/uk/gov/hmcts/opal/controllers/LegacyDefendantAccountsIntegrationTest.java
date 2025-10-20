66179ec1b3e8de4e18c928a12dadab3fff52134b
Aneesah33 <aneesah.khan@hmcts.net>
2025-10-20 17:03:10 +0100

    PO-1973: disable legacy defendant account party tests (stub required)

--- src/integrationTest/java/uk/gov/hmcts/opal/controllers/LegacyDefendantAccountsIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/opal/controllers/LegacyDefendantAccountsIntegrationTest.java
@@ -57,11 +57,13 @@ class LegacyDefendantAccountsIntegrationTest extends DefendantAccountsController
         super.testLegacyGetDefendantAtAGlance(log);
     }
 
+    @Disabled("A running instance of Legacy Stub App is required to execute this test")
     @Test
     void testGetDefendantAccountParty_Success() throws Exception {
         super.legacyGetDefendantAccountParty_Happy(log);
     }
 
+    @Disabled("A running instance of Legacy Stub App is required to execute this test")
     @Test
     void testGetDefendantAccountParty_Organisation() throws Exception {
         super.legacyGetDefendantAccountParty_Organisation(log);
