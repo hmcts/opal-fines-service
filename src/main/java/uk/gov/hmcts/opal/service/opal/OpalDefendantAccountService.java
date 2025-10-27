e071edecdf4378c50c0da31f5ecda8f2a9f2838e
marcmollins <Marc.Mollins@HMCTS.NET>
2025-10-27 10:22:16 +0000

    PO-2312
    removed imports

--- src/main/java/uk/gov/hmcts/opal/service/opal/OpalDefendantAccountService.java
+++ src/main/java/uk/gov/hmcts/opal/service/opal/OpalDefendantAccountService.java
@@ -10,7 +10,6 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
