daa0d719e28734c7fd00f0e035cf607f8af194be
Jonathan.Duffy <Jonathan.Duffy@cgi.com>
2026-01-20 11:18:34 +0000

    Implement integration tests for Add Payment Terms API

--- src/integrationTest/java/uk/gov/hmcts/opal/controllers/DefendantAccountsControllerIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/opal/controllers/DefendantAccountsControllerIntegrationTest.java
@@ -50,6 +50,7 @@ import uk.gov.hmcts.opal.SchemaPaths;
 import uk.gov.hmcts.opal.authorisation.model.FinesPermission;
 import uk.gov.hmcts.opal.common.user.authentication.service.AccessTokenService;
 import uk.gov.hmcts.opal.common.user.authorisation.model.UserState;
+import uk.gov.hmcts.opal.controllers.util.UserStateUtil;
 import uk.gov.hmcts.opal.dto.ToJsonString;
 import uk.gov.hmcts.opal.service.UserStateService;
 import uk.gov.hmcts.opal.service.opal.JsonSchemaValidationService;
@@ -4487,4 +4488,426 @@ abstract class DefendantAccountsControllerIntegrationTest extends AbstractIntegr
             .andExpect(jsonPath("$.payment_terms.days_in_default").value(30));
     }
 
+    @DisplayName("OPAL: Add Payment Terms - Unauthorized when missing auth header [@PO-1718]")
+    void test_Opal_AddPaymentTerms_Unauthorized(Logger log) throws Exception {
+        doThrow(new ResponseStatusException(UNAUTHORIZED, "Unauthorized"))
+            .when(userStateService).checkForAuthorisedUser(any());
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Business-Unit-Id", "78");
+        headers.add(HttpHeaders.IF_MATCH, "\"0\"");
+
+        String requestJson = """
+            {
+              "payment_terms": {
+                "days_in_default": 30,
+                "date_days_in_default_imposed": "2025-11-05",
+                "extension": true,
+                "reason_for_extension": "extn reason text",
+                "effective_date": "2025-11-01",
+                "payment_terms_type": {
+                  "payment_terms_type_code": "B",
+                  "payment_terms_type_display_name": "By date"
+                },
+                "instalment_period": {
+                  "instalment_period_code": "W",
+                  "instalment_period_display_name": "Weekly"
+                },
+                "lump_sum_amount": 120.00,
+                "instalment_amount": 10.00,
+                "posted_details": {
+                  "posted_by": "clerk1",
+                  "posted_date": "2025-02-02",
+                  "posted_by_name": "aa"
+                }
+              },
+              "request_payment_card": false,
+              "generate_payment_terms_change_letter": false
+            }
+            """;
+
+        mockMvc.perform(
+                post("/defendant-accounts/77/payment-terms")
+                    .headers(headers)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(requestJson)
+            )
+            .andExpect(status().isUnauthorized())
+            .andExpect(content().string(""));
+    }
+
+    @DisplayName("OPAL: Add Payment Terms - Forbidden when user lacks permission [@PO-1718]")
+    void test_Opal_AddPaymentTerms_Forbidden(Logger log) throws Exception {
+        when(userStateService.checkForAuthorisedUser(any())).thenReturn(
+            UserState.builder()
+                .userId(999L)
+                .userName("no-permission-user")
+                .businessUnitUser(java.util.Collections.emptySet())
+                .build()
+        );
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth("token_without_permission");
+        headers.add("Business-Unit-Id", "78");
+        headers.add(HttpHeaders.IF_MATCH, "\"0\"");
+
+        String requestJson = """
+            {
+              "payment_terms": {
+                "days_in_default": 30,
+                "date_days_in_default_imposed": "2025-11-05",
+                "extension": true,
+                "reason_for_extension": "extn reason text",
+                "effective_date": "2025-11-01",
+                "payment_terms_type": {
+                  "payment_terms_type_code": "B",
+                  "payment_terms_type_display_name": "By date"
+                },
+                "instalment_period": {
+                  "instalment_period_code": "W",
+                  "instalment_period_display_name": "Weekly"
+                },
+                "lump_sum_amount": 120.00,
+                "instalment_amount": 10.00,
+                "posted_details": {
+                  "posted_by": "clerk1",
+                  "posted_date": "2025-02-02",
+                  "posted_by_name": "aa"
+                }
+              },
+              "request_payment_card": false,
+              "generate_payment_terms_change_letter": false
+            }
+            """;
+
+        mockMvc.perform(
+                post("/defendant-accounts/77/payment-terms")
+                    .headers(headers)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(requestJson)
+            )
+            .andExpect(status().isForbidden())
+            .andExpect(content().contentType(MediaType.APPLICATION_PROBLEM_JSON))
+            .andExpect(jsonPath("$.type").value("https://hmcts.gov.uk/problems/forbidden"));
+    }
+
+    @DisplayName("OPAL: Add Payment Terms - Not Found when account not in header BU [@PO-1718]")
+    void test_Opal_AddPaymentTerms_NotFound_WrongBU(Logger log) throws Exception {
+        when(userStateService.checkForAuthorisedUser(any())).thenReturn(
+            UserStateUtil.permissionUser((short) 99, FinesPermission.AMEND_PAYMENT_TERMS)
+        );
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth("some_value");
+        headers.add("Business-Unit-Id", "99");
+        headers.add(HttpHeaders.IF_MATCH, "\"0\"");
+
+        String requestJson = """
+            {
+              "payment_terms": {
+                "days_in_default": 30,
+                "date_days_in_default_imposed": "2025-11-05",
+                "extension": true,
+                "reason_for_extension": "extn reason text",
+                "effective_date": "2025-11-01",
+                "payment_terms_type": {
+                  "payment_terms_type_code": "B",
+                  "payment_terms_type_display_name": "By date"
+                },
+                "instalment_period": {
+                  "instalment_period_code": "W",
+                  "instalment_period_display_name": "Weekly"
+                },
+                "lump_sum_amount": 120.00,
+                "instalment_amount": 10.00,
+                "posted_details": {
+                  "posted_by": "clerk1",
+                  "posted_date": "2025-02-02",
+                  "posted_by_name": "aa"
+                }
+              },
+              "request_payment_card": false,
+              "generate_payment_terms_change_letter": false
+            }
+            """;
+
+        mockMvc.perform(
+                post("/defendant-accounts/77/payment-terms")
+                    .headers(headers)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(requestJson)
+            )
+            .andExpect(status().isNotFound())
+            .andExpect(content().contentType(MediaType.APPLICATION_PROBLEM_JSON))
+            .andExpect(jsonPath("$.type").value("https://hmcts.gov.uk/problems/entity-not-found"));
+    }
+
+    @DisplayName("OPAL: Add Payment Terms - Conflict when If-Match does not match [@PO-1718]")
+    void test_Opal_AddPaymentTerms_IfMatchConflict(Logger log) throws Exception {
+        when(userStateService.checkForAuthorisedUser(any())).thenReturn(allPermissionsUser());
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth("good_token");
+        headers.add("Business-Unit-Id", "78");
+        headers.add(HttpHeaders.IF_MATCH, "\"9999\"");
+
+        String requestJson = """
+            {
+              "payment_terms": {
+                "days_in_default": 30,
+                "date_days_in_default_imposed": "2025-11-05",
+                "extension": true,
+                "reason_for_extension": "extn reason text",
+                "effective_date": "2025-11-01",
+                "payment_terms_type": {
+                  "payment_terms_type_code": "B",
+                  "payment_terms_type_display_name": "By date"
+                },
+                "instalment_period": {
+                  "instalment_period_code": "W",
+                  "instalment_period_display_name": "Weekly"
+                },
+                "lump_sum_amount": 120.00,
+                "instalment_amount": 10.00,
+                "posted_details": {
+                  "posted_by": "clerk1",
+                  "posted_date": "2025-02-02",
+                  "posted_by_name": "aa"
+                }
+              },
+              "request_payment_card": false,
+              "generate_payment_terms_change_letter": false
+            }
+            """;
+
+        mockMvc.perform(
+                post("/defendant-accounts/77/payment-terms")
+                    .headers(headers)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(requestJson)
+            )
+            .andExpect(status().isConflict())
+            .andExpect(content().contentType(MediaType.APPLICATION_PROBLEM_JSON))
+            .andExpect(jsonPath("$.type").value("https://hmcts.gov.uk/problems/optimistic-locking"));
+    }
+
+    @DisplayName("OPAL: Add Payment Terms - Conflict when If-Match missing [@PO-1718]")
+    void test_Opal_AddPaymentTerms_IfMatchMissing(Logger log) throws Exception {
+        when(userStateService.checkForAuthorisedUser(any())).thenReturn(allPermissionsUser());
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth("good_token");
+        headers.add("Business-Unit-Id", "78");
+
+        String requestJson = """
+            {
+              "payment_terms": {
+                "days_in_default": 30,
+                "date_days_in_default_imposed": "2025-11-05",
+                "extension": true,
+                "reason_for_extension": "extn reason text",
+                "effective_date": "2025-11-01",
+                "payment_terms_type": {
+                  "payment_terms_type_code": "B",
+                  "payment_terms_type_display_name": "By date"
+                },
+                "instalment_period": {
+                  "instalment_period_code": "W",
+                  "instalment_period_display_name": "Weekly"
+                },
+                "lump_sum_amount": 120.00,
+                "instalment_amount": 10.00,
+                "posted_details": {
+                  "posted_by": "clerk1",
+                  "posted_date": "2025-02-02",
+                  "posted_by_name": "aa"
+                }
+              },
+              "request_payment_card": false,
+              "generate_payment_terms_change_letter": false
+            }
+            """;
+
+        ResultActions a = mockMvc.perform(
+            post("/defendant-accounts/77/payment-terms")
+                .headers(headers)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestJson)
+        );
+
+        String body = a.andReturn().getResponse().getContentAsString();
+        log.info(":addPaymentTerms_IfMatchMissing body:\n{}", body);
+
+        a.andExpect(status().isConflict())
+            .andExpect(content().contentType(MediaType.APPLICATION_PROBLEM_JSON))
+            .andExpect(jsonPath("$.type",
+                org.hamcrest.Matchers.anyOf(is("https://hmcts.gov.uk/problems/resource-conflict"),
+                    is("https://hmcts.gov.uk/problems/optimistic-locking"))));
+    }
+
+    @DisplayName("OPAL: Add Payment Terms - Schema validation error [@PO-1718]")
+    void test_Opal_AddPaymentTerms_SchemaValidation(Logger log) throws Exception {
+        when(userStateService.checkForAuthorisedUser(any())).thenReturn(allPermissionsUser());
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth("good_token");
+        headers.add("Business-Unit-Id", "78");
+        headers.add(HttpHeaders.IF_MATCH, "\"0\"");
+
+        mockMvc.perform(
+                post("/defendant-accounts/77/payment-terms")
+                    .headers(headers)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content("{\"request_payment_card\": true}")
+            )
+            .andExpect(status().isBadRequest())
+            .andExpect(content().contentType(MediaType.APPLICATION_PROBLEM_JSON))
+            .andExpect(jsonPath("$.type").value("https://hmcts.gov.uk/problems/json-schema-validation"));
+    }
+
+    @DisplayName("OPAL: Add Payment Terms - Timeout [@PO-1718]")
+    void test_Opal_AddPaymentTerms_Timeout(Logger log) throws Exception {
+        when(userStateService.checkForAuthorisedUser(any()))
+            .thenThrow(new ResponseStatusException(org.springframework.http.HttpStatus.REQUEST_TIMEOUT, "Timeout"));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth("some_value");
+        headers.add("Business-Unit-Id", "78");
+        headers.add(HttpHeaders.IF_MATCH, "\"0\"");
+
+        String requestJson = """
+            {
+              "payment_terms": {
+                "days_in_default": 30,
+                "date_days_in_default_imposed": "2025-11-05",
+                "extension": true,
+                "reason_for_extension": "extn reason text",
+                "effective_date": "2025-11-01",
+                "payment_terms_type": {
+                  "payment_terms_type_code": "B",
+                  "payment_terms_type_display_name": "By date"
+                },
+                "instalment_period": {
+                  "instalment_period_code": "W",
+                  "instalment_period_display_name": "Weekly"
+                },
+                "lump_sum_amount": 120.00,
+                "instalment_amount": 10.00,
+                "posted_details": {
+                  "posted_by": "clerk1",
+                  "posted_date": "2025-02-02",
+                  "posted_by_name": "aa"
+                }
+              },
+              "request_payment_card": false,
+              "generate_payment_terms_change_letter": false
+            }
+            """;
+
+        mockMvc.perform(
+                post("/defendant-accounts/77/payment-terms")
+                    .headers(headers)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(requestJson)
+            )
+            .andExpect(status().isRequestTimeout());
+    }
+
+    @DisplayName("OPAL: Add Payment Terms - Service unavailable [@PO-1718]")
+    void test_Opal_AddPaymentTerms_ServiceUnavailable(Logger log) throws Exception {
+        when(userStateService.checkForAuthorisedUser(any()))
+            .thenThrow(new ResponseStatusException(
+                org.springframework.http.HttpStatus.SERVICE_UNAVAILABLE, "Gateway down"));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth("some_value");
+        headers.add("Business-Unit-Id", "78");
+        headers.add(HttpHeaders.IF_MATCH, "\"0\"");
+
+        String requestJson = """
+            {
+              "payment_terms": {
+                "days_in_default": 30,
+                "date_days_in_default_imposed": "2025-11-05",
+                "extension": true,
+                "reason_for_extension": "extn reason text",
+                "effective_date": "2025-11-01",
+                "payment_terms_type": {
+                  "payment_terms_type_code": "B",
+                  "payment_terms_type_display_name": "By date"
+                },
+                "instalment_period": {
+                  "instalment_period_code": "W",
+                  "instalment_period_display_name": "Weekly"
+                },
+                "lump_sum_amount": 120.00,
+                "instalment_amount": 10.00,
+                "posted_details": {
+                  "posted_by": "clerk1",
+                  "posted_date": "2025-02-02",
+                  "posted_by_name": "aa"
+                }
+              },
+              "request_payment_card": false,
+              "generate_payment_terms_change_letter": false
+            }
+            """;
+
+        mockMvc.perform(
+                post("/defendant-accounts/77/payment-terms")
+                    .headers(headers)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(requestJson)
+            )
+            .andExpect(status().isServiceUnavailable());
+    }
+
+    @DisplayName("OPAL: Add Payment Terms - Server error [@PO-1718]")
+    void test_Opal_AddPaymentTerms_ServerError(Logger log) throws Exception {
+        when(userStateService.checkForAuthorisedUser(any()))
+            .thenThrow(new ResponseStatusException(
+                org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR, "Boom"));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth("some_value");
+        headers.add("Business-Unit-Id", "78");
+        headers.add(HttpHeaders.IF_MATCH, "\"0\"");
+
+        String requestJson = """
+            {
+              "payment_terms": {
+                "days_in_default": 30,
+                "date_days_in_default_imposed": "2025-11-05",
+                "extension": true,
+                "reason_for_extension": "extn reason text",
+                "effective_date": "2025-11-01",
+                "payment_terms_type": {
+                  "payment_terms_type_code": "B",
+                  "payment_terms_type_display_name": "By date"
+                },
+                "instalment_period": {
+                  "instalment_period_code": "W",
+                  "instalment_period_display_name": "Weekly"
+                },
+                "lump_sum_amount": 120.00,
+                "instalment_amount": 10.00,
+                "posted_details": {
+                  "posted_by": "clerk1",
+                  "posted_date": "2025-02-02",
+                  "posted_by_name": "aa"
+                }
+              },
+              "request_payment_card": false,
+              "generate_payment_terms_change_letter": false
+            }
+            """;
+
+        mockMvc.perform(
+                post("/defendant-accounts/77/payment-terms")
+                    .headers(headers)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(requestJson)
+            )
+            .andExpect(status().isInternalServerError());
+    }
 }
