a648039cc6c5b62efd3c10078cfbc813a1c5cdb9
Ben Edwards <147524406+Ben-Edwards-cgi@users.noreply.github.com>
2026-02-05 13:25:41 +0000

    Revert "Update Read Me files."
    This reverts commit d727685e3575065d9fee34ca0523c9f3a55a4c70.

--- bin/secrets/secrets-stg-environment.sh
+++ bin/secrets/secrets-stg-environment.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # To set the secrets in your shell, source this file ie. source ./bin/secrets-stg.sh
-# Ensure you already have the Azure CLI installed - `brew install azure-cli`
 
 echo "Exporting secrets from Azure keyvault (opal-stg), please ensure you have \"az\" installed and you have logged in, using \"az login\"."
 
@@ -13,5 +12,5 @@ echo "LAUNCH_DARKLY_SDK_KEY=$(az keyvault secret show --vault-name opal-stg --na
 
 #Some local database variables to help gradle understand flyway
 echo "FLYWAY_URL=jdbc:postgresql://localhost:5432/opal-fines-db"
-echo "FLYWAY_USER=opal-fines"
-echo "FLYWAY_PASSWORD=opal-fines"
+echo "FLYWAY_USER=opal-db-user"
+echo "FLYWAY_PASSWORD=opal-db-password"
