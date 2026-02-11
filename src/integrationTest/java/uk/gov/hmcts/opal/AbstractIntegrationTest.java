43b08c63e4b0d124856f3f1bb09309658c30a203
benedwards <ben.edwards@cgi.com>
2026-02-11 17:15:00 +0000

    Added legacy gateway url toggle

--- src/integrationTest/java/uk/gov/hmcts/opal/AbstractIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/opal/AbstractIntegrationTest.java
@@ -3,6 +3,8 @@ package uk.gov.hmcts.opal;
 import static uk.gov.hmcts.opal.TestContainerConfig.POSTGRES_CONTAINER;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.net.ServerSocket;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,6 +19,7 @@ import org.springframework.test.web.servlet.MockMvc;
 @ContextConfiguration(classes = {TestContainerConfig.class})
 @AutoConfigureMockMvc
 public class AbstractIntegrationTest {
+
     @Autowired
     protected MockMvc mockMvc;
 
@@ -29,5 +32,19 @@ public class AbstractIntegrationTest {
         registry.add("spring.datasource.url", POSTGRES_CONTAINER::getJdbcUrl);
         registry.add("spring.datasource.username", POSTGRES_CONTAINER::getUsername);
         registry.add("spring.datasource.password", POSTGRES_CONTAINER::getPassword);
+
+        registry.add("legacy-gateway.url", () -> isPortAvailable(4553)
+            ? "https://opal-legacy-db-stub.staging.platform.hmcts.net/opal"
+            : "http://localhost:4553/opal"
+        );
+    }
+
+    public static boolean isPortAvailable(int port) {
+        try (ServerSocket socket = new ServerSocket(port)) {
+            socket.setReuseAddress(true);
+            return true;  // we successfully bound it
+        } catch (IOException e) {
+            return false; // something else is using it
+        }
     }
 }
