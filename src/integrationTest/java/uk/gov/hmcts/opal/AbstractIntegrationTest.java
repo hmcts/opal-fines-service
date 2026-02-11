a29f48fbcbb491a34864b41e1df49cd2932022da
benedwards <ben.edwards@cgi.com>
2026-02-11 17:20:13 +0000

    Added test container for legacy stub

--- src/integrationTest/java/uk/gov/hmcts/opal/AbstractIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/opal/AbstractIntegrationTest.java
@@ -3,8 +3,6 @@ package uk.gov.hmcts.opal;
 import static uk.gov.hmcts.opal.TestContainerConfig.POSTGRES_CONTAINER;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.IOException;
-import java.net.ServerSocket;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -32,19 +30,5 @@ public class AbstractIntegrationTest {
         registry.add("spring.datasource.url", POSTGRES_CONTAINER::getJdbcUrl);
         registry.add("spring.datasource.username", POSTGRES_CONTAINER::getUsername);
         registry.add("spring.datasource.password", POSTGRES_CONTAINER::getPassword);
-
-        registry.add("legacy-gateway.url", () -> isPortAvailable(4553)
-            ? "https://opal-legacy-db-stub.staging.platform.hmcts.net/opal"
-            : "http://localhost:4553/opal"
-        );
-    }
-
-    public static boolean isPortAvailable(int port) {
-        try (ServerSocket socket = new ServerSocket(port)) {
-            socket.setReuseAddress(true);
-            return true;  // we successfully bound it
-        } catch (IOException e) {
-            return false; // something else is using it
-        }
     }
 }
--- src/integrationTest/java/uk/gov/hmcts/opal/TestContainerConfig.java
+++ src/integrationTest/java/uk/gov/hmcts/opal/TestContainerConfig.java
@@ -1,6 +1,10 @@
 package uk.gov.hmcts.opal;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.List;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
@@ -20,5 +24,22 @@ public class TestContainerConfig {
         //POSTGRES_CONTAINER.setPortBindings(List.of("55432:5432"));
 
         POSTGRES_CONTAINER.start();
+
+        if (isPortAvailable(4553)) {
+            final GenericContainer<?> LEGACY_STUB_CONTAINER =
+                new GenericContainer<>(DockerImageName.parse("sdshmctspublic.azurecr.io/opal/legacy-db-stub:latest"))
+                    .withExposedPorts(4553);
+            LEGACY_STUB_CONTAINER.setPortBindings(List.of("4553:4553"));
+            LEGACY_STUB_CONTAINER.start();
+        }
+    }
+
+    public static boolean isPortAvailable(int port) {
+        try (ServerSocket socket = new ServerSocket(port)) {
+            socket.setReuseAddress(true);
+            return true;  // we successfully bound it
+        } catch (IOException e) {
+            return false; // something else is using it
+        }
     }
 }
