2ef3b2d26dde6b509899deb86a9c78d9a4acaf03
Dat Nguyen <thanh.datnguyen@hmcts.net>
2026-03-12 11:33:00 +0000

    PO-2890: Added null checks to BusinessUnitTypeConverter

--- src/main/java/uk/gov/hmcts/opal/entity/converter/BusinessUnitTypeConverter.java
+++ src/main/java/uk/gov/hmcts/opal/entity/converter/BusinessUnitTypeConverter.java
@@ -2,18 +2,21 @@ package uk.gov.hmcts.opal.entity.converter;
 
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
+import org.apache.logging.log4j.util.Strings;
 import uk.gov.hmcts.opal.entity.businessunit.BusinessUnitType;
+
+import java.util.Objects;
 
 @Converter(autoApply = true)
 public class BusinessUnitTypeConverter implements AttributeConverter<BusinessUnitType, String> {
 
     @Override
     public String convertToDatabaseColumn(BusinessUnitType type) {
-        return type.getLabel();
+        return !Objects.isNull(type) ? type.getLabel() : null;
     }
 
     @Override
     public BusinessUnitType convertToEntityAttribute(String label) {
-        return BusinessUnitType.getByLabel(label);
+        return Strings.isNotBlank(label) ? BusinessUnitType.getByLabel(label) : null;
     }
 }
