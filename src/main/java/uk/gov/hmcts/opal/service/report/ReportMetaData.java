d2d7e5c7e29b0dee4a5f18878fefc36e633f3fbf
Sarah Thompson <Sarah.thompson@HMCTS.NET>
2026-03-13 11:10:53 +0000

    PO-2257 - update reportMetaData list object

--- src/main/java/uk/gov/hmcts/opal/service/report/ReportMetaData.java
+++ src/main/java/uk/gov/hmcts/opal/service/report/ReportMetaData.java
@@ -3,10 +3,11 @@ package uk.gov.hmcts.opal.service.report;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import uk.gov.hmcts.opal.logging.integration.dto.ParticipantIdentifier;
 
 @Data
 @AllArgsConstructor
 public class ReportMetaData {
-    private List<Integer> pdpoPartyIds;
+    private List<ParticipantIdentifier> pdpoPartyIds;
 
 }
