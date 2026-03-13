9f15d3173aa9b2c6918f794b822fc6eb7be713a0
hennaparmar <henna.parmar@hmcts.net>
2026-03-13 09:22:11 +0000

    PO-1854: Implement PATCH /defendant-accounts/{id} in DefendantAccountApiController

--- src/main/java/uk/gov/hmcts/opal/controllers/DefendantAccountApiController.java
+++ src/main/java/uk/gov/hmcts/opal/controllers/DefendantAccountApiController.java
@@ -6,6 +6,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.NativeWebRequest;
+import uk.gov.hmcts.opal.dto.DefendantAccountResponse;
+import uk.gov.hmcts.opal.dto.UpdateDefendantAccountRequest;
 import uk.gov.hmcts.opal.generated.http.api.DefendantAccountApi;
 import uk.gov.hmcts.opal.generated.model.GetEnforcementStatusResponse;
 import uk.gov.hmcts.opal.service.DefendantAccountService;
@@ -23,5 +26,29 @@ public class DefendantAccountApiController implements DefendantAccountApi {
         log.debug(":GET:getDefendantAccountEnforcementStatus: for defendant id: {}", id);
 
         return buildResponse(defendantAccountService.getEnforcementStatus(id, authHeaderValue));
+    }
+
+    @Override
+    public ResponseEntity<DefendantAccountResponse> updateDefendantAccount(
+        Long id,
+        UpdateDefendantAccountRequest body,
+        NativeWebRequest nativeWebRequest
+    ) {
+        // read headers from the NativeWebRequest (OpenAPI generator sometimes gives NativeWebRequest)
+        String authHeader = nativeWebRequest.getHeader("Authorization");
+        String businessUnitId = nativeWebRequest.getHeader("Business-Unit-Id");
+        String ifMatch = nativeWebRequest.getHeader("If-Match");
+
+        log.debug(":PATCH:updateDefendantAccount (OpenAPI): id={}", id);
+
+        // delegate to existing service layer (same call used in manual controller)
+        // NOTE: this assumes the generated model 'UpdateDefendantAccountRequest' is compatible with your service method
+        return buildResponse(defendantAccountService.updateDefendantAccount(
+            id,
+            businessUnitId,
+            body,      // request object
+            ifMatch,
+            authHeader
+        ));
     }
 }
