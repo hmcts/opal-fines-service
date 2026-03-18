77a7bd0b444a0d105701c22587db605687c187e6
ShanCGI <223634236+ShanCGI@users.noreply.github.com>
2026-03-18 12:30:40 +0000

    PO-1940 - remove duplicate method

--- src/main/java/uk/gov/hmcts/opal/service/opal/OpalDefendantAccountPartyService.java
+++ src/main/java/uk/gov/hmcts/opal/service/opal/OpalDefendantAccountPartyService.java
@@ -162,18 +162,6 @@ public class OpalDefendantAccountPartyService implements DefendantAccountPartySe
 
     @Override
     @Transactional
-    public GetDefendantAccountPartyResponse addDefendantAccountParty(
-        Long accountId, Long dapId, String businessUnitId,
-        String postedBy, String businessUserId, String ifMatch, DefendantAccountParty request) {
-
-        // not implemented.
-        throw new UnsupportedOperationException("Adding a party to an account is not yet supported in Opal");
-    }
-
-
-
-    @Override
-    @Transactional
     public GetDefendantAccountPartyResponse replaceDefendantAccountParty(
         Long accountId, Long dapId, DefendantAccountParty request, String ifMatch, String businessUnitId,
         String postedBy, String businessUserId) {
