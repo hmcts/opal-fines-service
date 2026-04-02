7672547f950220b663ea8cd5848712357fbed9ce
Elizabeth Adeleke <66313212+Elizabeth1420@users.noreply.github.com>
2026-04-02 13:48:13 +0100

    PO-1940 add Defendant Account Party
    tested addDefendantAccountParty_Success

--- src/test/java/uk/gov/hmcts/opal/controllers/DefendantAccountControllerTest.java
+++ src/test/java/uk/gov/hmcts/opal/controllers/DefendantAccountControllerTest.java
@@ -14,10 +14,13 @@ import uk.gov.hmcts.opal.dto.AddEnforcementResponse;
 import uk.gov.hmcts.opal.dto.DefendantAccountHeaderSummary;
 import uk.gov.hmcts.opal.dto.RemoveDefendantAccountEnforcementHoldRequest;
 import uk.gov.hmcts.opal.dto.RemoveDefendantAccountEnforcementHoldResponse;
+import uk.gov.hmcts.opal.dto.GetDefendantAccountPartyResponse;
+import uk.gov.hmcts.opal.dto.common.DefendantAccountParty;
 import uk.gov.hmcts.opal.dto.search.AccountSearchDto;
 import uk.gov.hmcts.opal.dto.search.DefendantAccountSearchResultsDto;
 import uk.gov.hmcts.opal.exception.ResourceConflictException;
 import uk.gov.hmcts.opal.service.DefendantAccountEnforcementService;
+import uk.gov.hmcts.opal.service.DefendantAccountPartyService;
 import uk.gov.hmcts.opal.service.DefendantAccountService;
 import uk.gov.hmcts.opal.service.legacy.LegacyDefendantAccountService;
 
@@ -42,6 +45,9 @@ class DefendantAccountControllerTest {
 
     @Mock
     private DefendantAccountEnforcementService defendantAccountEnforcementService;
+
+    @Mock
+    private DefendantAccountPartyService defendantAccountPartyService;
 
     @InjectMocks
     private DefendantAccountController defendantAccountController;
@@ -155,6 +161,53 @@ class DefendantAccountControllerTest {
         assertNotNull(response.getBody());
         assertEquals(expectedResponse, response.getBody());
     }
+    @Test
+    void testAddDefendantAccountParty_Success() {
+        // Arrange
+        Long defendantAccountId = 1L;
+        Long defendantAccountPartyId = 2L;
+        String businessUnitId = "10";
+        String ifMatch = "1";
+
+        DefendantAccountParty request = new DefendantAccountParty();
+        GetDefendantAccountPartyResponse mockResponse = new GetDefendantAccountPartyResponse();
+
+        when(defendantAccountPartyService.addDefendantAccountParty(
+            defendantAccountId,
+            defendantAccountPartyId,
+            BEARER_TOKEN,
+            ifMatch,
+            businessUnitId,
+            request
+        )).thenReturn(mockResponse);
+
+        // Act
+        ResponseEntity<GetDefendantAccountPartyResponse> response =
+            defendantAccountController.addDefendantAccountParty(
+                defendantAccountId,
+                defendantAccountPartyId,
+                businessUnitId,
+                ifMatch,
+                BEARER_TOKEN,
+                request
+            );
+
+        // Assert
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(mockResponse, response.getBody());
+
+        verify(defendantAccountPartyService).addDefendantAccountParty(
+            defendantAccountId,
+            defendantAccountPartyId,
+            BEARER_TOKEN,
+            ifMatch,
+            businessUnitId,
+            request
+        );
+    }
+
+
+}
 
     @Test
     void testRemoveEnforcementHold_forbiddenWhenServiceThrowsPermissionNotAllowedException() {
