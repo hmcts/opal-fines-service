2379fc04a0ee98cdd1bb7cd9d8df627033424bf6
Aneesah33 <aneesah.khan@hmcts.net>
2026-04-09 16:30:09 +0100

    Po 1963 replace defendant account party change api (#1647)
    * PO-1963: defer parent/guardian removal pending PO-1897
    
    * PO-1963 add method to delete parent/guardian parties when converting individual to organisation
    
    * PO-1963 address review feedback for replace party conversion and tests
    
    * PO-1963 add non-conversion guard assertion in replace party unit test
    
    * PO-1963: move put party it parent/guardian setup into shared SQL fixture

--- src/main/java/uk/gov/hmcts/opal/service/opal/OpalDefendantAccountPartyService.java
+++ src/main/java/uk/gov/hmcts/opal/service/opal/OpalDefendantAccountPartyService.java
@@ -41,6 +41,7 @@ import uk.gov.hmcts.opal.entity.defendantaccount.AssociationType;
 import uk.gov.hmcts.opal.entity.defendantaccount.DefendantAccountEntity;
 import uk.gov.hmcts.opal.entity.defendantaccount.DefendantAccountPartiesEntity;
 import uk.gov.hmcts.opal.entity.amendment.RecordType;
+import uk.gov.hmcts.opal.repository.DefendantAccountPartiesRepository;
 import uk.gov.hmcts.opal.service.iface.DefendantAccountPartyServiceInterface;
 import uk.gov.hmcts.opal.service.persistence.AliasRepositoryService;
 import uk.gov.hmcts.opal.service.persistence.AmendmentRepositoryService;
@@ -63,6 +64,8 @@ public class OpalDefendantAccountPartyService implements DefendantAccountPartySe
     private final AliasRepositoryService aliasRepositoryService;
 
     private final PartyRepositoryService partyRepositoryService;
+
+    private final DefendantAccountPartiesRepository defendantAccountPartiesRepository;
 
 
     @Override
@@ -178,6 +181,11 @@ public class OpalDefendantAccountPartyService implements DefendantAccountPartySe
             throw new IllegalArgumentException("Request body is required");
         }
 
+        PartyDetails partyDetails = request.getPartyDetails();
+        if (isConvertingFromIndividualToOrganisation(party, partyDetails)) {
+            removeParentGuardianParties(account, dapId);
+        }
+
         Optional.ofNullable(request.getDefendantAccountPartyType())
             .map(AssociationType::getByLabel)
             .ifPresent(dap::setAssociationType);
@@ -198,7 +206,7 @@ public class OpalDefendantAccountPartyService implements DefendantAccountPartySe
             party.getOrganisationName(),
             party.getSurname());
 
-        OpalDefendantAccountBuilders.applyPartyCoreReplace(party, request.getPartyDetails());
+        OpalDefendantAccountBuilders.applyPartyCoreReplace(party, partyDetails);
         OpalDefendantAccountBuilders.applyPartyAddressReplace(party, request.getAddress());
         OpalDefendantAccountBuilders.applyPartyContactReplace(party, request.getContactDetails());
 
@@ -210,7 +218,7 @@ public class OpalDefendantAccountPartyService implements DefendantAccountPartySe
             request.getLanguagePreferences(), isDebtor
         );
 
-        replaceAliasesForParty(party.getPartyId(), request.getPartyDetails());
+        replaceAliasesForParty(party.getPartyId(), partyDetails);
 
         amendmentRepositoryService.auditFinaliseStoredProc(
             account.getDefendantAccountId(),
@@ -229,6 +237,25 @@ public class OpalDefendantAccountPartyService implements DefendantAccountPartySe
             .defendantAccountParty(mapDefendantAccountParty(dap, aliasEntity))
             .version(bumpVersion(accountId).getVersion())
             .build();
+    }
+
+    private boolean isConvertingFromIndividualToOrganisation(PartyEntity party, PartyDetails partyDetails) {
+        return !party.isOrganisation()
+            && Boolean.TRUE.equals(Optional.ofNullable(partyDetails)
+                .map(PartyDetails::getOrganisationFlag)
+                .orElse(null));
+    }
+
+    private void removeParentGuardianParties(DefendantAccountEntity account, Long dapId) {
+        int deletedRows = defendantAccountPartiesRepository.deleteByAccountIdAndAssociationTypeExcludingDapId(
+            account.getDefendantAccountId(),
+            AssociationType.PARENT_GUARDIAN,
+            dapId
+        );
+        if (deletedRows > 0) {
+            log.info("replaceDefendantAccountParty: removed {} parent/guardian parties for accountId={}",
+                deletedRows, account.getDefendantAccountId());
+        }
     }
 
     private void replaceAliasesForParty(Long partyId, PartyDetails pd) {
