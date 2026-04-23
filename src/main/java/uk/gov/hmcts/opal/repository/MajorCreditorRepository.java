0490851a2a660484be8a8824e18aea96a104a0dd
Dat Nguyen <thanh.datnguyen@hmcts.net>
2026-04-23 16:25:19 +0100

    PO-2887: Updated MajorCreditor to use entity graphs

--- src/main/java/uk/gov/hmcts/opal/repository/MajorCreditorRepository.java
+++ src/main/java/uk/gov/hmcts/opal/repository/MajorCreditorRepository.java
@@ -1,11 +1,27 @@
 package uk.gov.hmcts.opal.repository;
 
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
-import uk.gov.hmcts.opal.entity.majorcreditor.MajorCreditorFullEntity;
+import uk.gov.hmcts.opal.entity.majorcreditor.MajorCreditorEntity;
+
+import java.util.Optional;
+import java.util.function.Function;
 
 @Repository
-public interface MajorCreditorRepository extends JpaRepository<MajorCreditorFullEntity, Long>,
-    JpaSpecificationExecutor<MajorCreditorFullEntity> {
+public interface MajorCreditorRepository extends JpaRepository<MajorCreditorEntity, Long>,
+    JpaSpecificationExecutor<MajorCreditorEntity> {
+
+    @Override
+    @EntityGraph(value = MajorCreditorEntity.ENTITY_GRAPH_FULL, type = EntityGraph.EntityGraphType.FETCH)
+    Optional<MajorCreditorEntity> findById(Long majorCreditorId);
+
+    @Override
+    @EntityGraph(value = MajorCreditorEntity.ENTITY_GRAPH_FULL, type = EntityGraph.EntityGraphType.FETCH)
+    <S extends MajorCreditorEntity, R> R findBy(
+        Specification<MajorCreditorEntity> spec,
+        Function<? super JpaSpecificationExecutor.SpecificationFluentQuery<S>, R> queryFunction
+    );
 }
