7158adf62c046875c161b5763a237d3b8d91dc34
Sean Reed <Sean.Reed@HMCTS.NET>
2026-04-30 15:05:43 +0100

    PO-2833 - auth using cached user state

--- src/integrationTest/java/uk/gov/hmcts/opal/controllers/JwtControllerIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/opal/controllers/JwtControllerIntegrationTest.java
@@ -2,14 +2,11 @@ package uk.gov.hmcts.opal.controllers;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.nimbusds.jose.JOSEException;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import uk.gov.hmcts.opal.AbstractIntegrationWithSecurityTest;
-import uk.gov.hmcts.opal.SecurityItProperties;
 
 @Slf4j(topic = "opal.JwtControllerIntegrationTest")
 @DisplayName("JWT Controller Integration Tests")
@@ -18,16 +15,11 @@ class JwtControllerIntegrationTest extends AbstractIntegrationWithSecurityTest {
     private static final String URL = "/business-units/5";
     public static final String AUTHORIZATION = "authorization";
 
-    @Autowired
-    public JwtControllerIntegrationTest(SecurityItProperties securityItProperties) throws JOSEException {
-        super(securityItProperties);
-    }
-
     @Test
     @DisplayName("Testing Valid Token")
     void testValidJwtTokenGivesOk() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get(URL)
-                .header(AUTHORIZATION, "Bearer " + validToken()))
+                .header(AUTHORIZATION, "Bearer " + validToken))
             .andExpect(status().isOk());
     }
 
@@ -35,7 +27,7 @@ class JwtControllerIntegrationTest extends AbstractIntegrationWithSecurityTest {
     @DisplayName("Testing Expired JWT Token")
     void testExpiredTokenGivesUnauthorized() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get(URL)
-                .header(AUTHORIZATION, "Bearer " + expiredToken()))
+                .header(AUTHORIZATION, "Bearer " + expiredToken))
             .andExpect(status().isUnauthorized());
     }
 
