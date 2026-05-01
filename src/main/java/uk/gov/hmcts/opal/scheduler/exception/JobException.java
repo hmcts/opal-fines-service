90862206685f4ecc7f5afab940fc5e0366f28d84
Dat Nguyen <thanh.datnguyen@hmcts.net>
2026-05-01 16:16:38 +0100

    PO-2947: Remove Quartz schedular jobs

--- src/main/java/uk/gov/hmcts/opal/scheduler/exception/JobException.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.hmcts.opal.scheduler.exception;
-
-public class JobException extends RuntimeException {
-
-    public JobException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
--- src/main/java/uk/gov/hmcts/opal/scheduler/exception/JobNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.hmcts.opal.scheduler.exception;
-
-public class JobNotFoundException extends RuntimeException {
-
-    public JobNotFoundException(String message) {
-        super(message);
-    }
-}
