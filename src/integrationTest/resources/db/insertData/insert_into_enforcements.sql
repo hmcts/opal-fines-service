7c403a5550732117849065e52d9b61bd595d55e9
Sarah Wall <259313819+sarahtw-cgi@users.noreply.github.com>
2026-05-01 10:22:42 +0100

    PO-2255 - fix test data conflict

--- src/integrationTest/resources/db/insertData/insert_into_enforcements.sql
+++ src/integrationTest/resources/db/insertData/insert_into_enforcements.sql
@@ -13,40 +13,49 @@
 *
 **/
 
-INSERT INTO public.enforcements(
-    enforcement_id, defendant_account_id, posted_date, posted_by, result_id, reason, enforcer_id, jail_days, result_responses, warrant_reference, case_reference, hearing_date, hearing_court_id, posted_by_name, earliest_release_date, enforcement_account_type)
-VALUES (
-           91000000007001,
-           99000000000001,
-           now(),
-           'L080JG',
-           'REGF',
-           'Test enforcement',
-           null,
-           null,
-           null,
-           '001/25/00001',
-           null,
-           null,
-           null,
-           'opal-test',
-           null,
-           null),
-       (
-           91000000007002,
-           99000000000002,
-           TIMESTAMP '2000-01-01',
-           'L080JG',
-           'REGF',
-           'Test enforcement',
-           null,
-           null,
-           null,
-           '001/25/00001',
-           null,
-           null,
-           null,
-           'opal-test',
-           null,
-           null
-    );
+INSERT INTO public.enforcements(enforcement_id, defendant_account_id, posted_date, posted_by,
+                                result_id, reason, enforcer_id, jail_days, result_responses,
+                                warrant_reference, case_reference, hearing_date, hearing_court_id,
+                                posted_by_name, earliest_release_date, enforcement_account_type)
+VALUES (91000000007001,
+        99000000000001,
+        now(),
+        'L080JG',
+        'REGF',
+        'Test enforcement',
+        null,
+        null,
+        null,
+        '001/25/00001',
+        null,
+        null,
+        null,
+        'opal-test',
+        null,
+        null),
+       (91000000007002,
+        99000000000002,
+        TIMESTAMP '2000-01-01',
+        'L080JG',
+        'REGF',
+        'Test enforcement',
+        null,
+        null,
+        null,
+        '001/25/00001',
+        null,
+        null,
+        null,
+        'opal-test',
+        null,
+        null)
+    ON CONFLICT (enforcement_id)
+    DO UPDATE
+    SET
+        posted_date = EXCLUDED.posted_date,
+    result_id = EXCLUDED.result_id
+WHERE
+    public.enforcements.posted_date IS DISTINCT
+FROM EXCLUDED.posted_date
+    OR public.enforcements.result_id IS DISTINCT
+FROM EXCLUDED.result_id;
