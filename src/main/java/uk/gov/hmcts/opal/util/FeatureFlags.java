ace0b35a7881ec77a836e77ad09f8f1ff9c81453
hennaparmar <henna.parmar@hmcts.net>
2026-05-05 14:52:07 +0100

    PO-1992 feature flag

--- src/main/java/uk/gov/hmcts/opal/util/FeatureFlags.java
+++ src/main/java/uk/gov/hmcts/opal/util/FeatureFlags.java
@@ -2,10 +2,14 @@ package uk.gov.hmcts.opal.util;
 
 public final class FeatureFlags {
 
+    public static final String DEFAULT_VALUE_PROPERTY_PREFIX = "launchdarkly.default-flag-values.";
     public static final String RELEASE_1B = "release-1b";
-    public static final String RELEASE_1B_DEFAULT_VALUE_PROPERTY =
-        "launchdarkly.default-flag-values.release-1b";
+    public static final String RELEASE_1B_DEFAULT_VALUE_PROPERTY = DEFAULT_VALUE_PROPERTY_PREFIX + RELEASE_1B;
 
     private FeatureFlags() {
+    }
+
+    public static String defaultValueProperty(String featureFlag) {
+        return DEFAULT_VALUE_PROPERTY_PREFIX + featureFlag;
     }
 }
