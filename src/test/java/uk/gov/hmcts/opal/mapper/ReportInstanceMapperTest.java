97807012372783ade86f396c378715a674e7890e
alex-reeves-cgi <275961728+alex-reeves-cgi@users.noreply.github.com>
2026-05-06 16:23:28 +0100

    PO-2252 IT and unit tests for create report instance functionality

---
+++ src/test/java/uk/gov/hmcts/opal/mapper/ReportInstanceMapperTest.java
@@ -0,0 +1,27 @@
+package uk.gov.hmcts.opal.mapper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+import uk.gov.hmcts.opal.entity.ReportEntity;
+import uk.gov.hmcts.opal.entity.ReportInstanceEntity;
+import uk.gov.hmcts.opal.generated.model.CreateReportInstanceResponseReports;
+
+public class ReportInstanceMapperTest {
+    private final ReportInstanceMapper reportInstanceMapper = Mappers.getMapper(ReportInstanceMapper.class);
+
+    @Test
+    public void toResponseDto_shouldMapFromEntity() {
+        ReportInstanceEntity reportInstanceEntity = ReportInstanceEntity.builder()
+            .report(ReportEntity.builder().reportId("REPORT-ID-123").build())
+            .reportInstanceId(456L)
+            .build();
+        CreateReportInstanceResponseReports  responseDto = reportInstanceMapper.toResponseDto(reportInstanceEntity);
+
+        assertNotNull(responseDto);
+        //assertEquals("REPORT-ID-123", responseDto.getReportId());
+        //assertEquals(456L, responseDto.getReportInstanceId());
+    }
+}
