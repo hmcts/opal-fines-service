25a8df017c993535ac1c9deaa184d8813ebfce32
hennaparmar <henna.parmar@hmcts.net>
2026-05-26 03:09:24 +0100

    add organisation and individual aliases to LegacyUpdateMinorCreditorAccountRequest test for JSON serialization verification

--- src/test/java/uk/gov/hmcts/opal/dto/legacy/LegacyUpdateMinorCreditorAccountRequestTest.java
+++ src/test/java/uk/gov/hmcts/opal/dto/legacy/LegacyUpdateMinorCreditorAccountRequestTest.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.opal.dto.legacy;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.junit.jupiter.api.Test;
 import uk.gov.hmcts.opal.dto.ToJsonString;
+import uk.gov.hmcts.opal.dto.legacy.common.IndividualDetails;
 import uk.gov.hmcts.opal.dto.legacy.common.LegacyCreditorAccountPaymentDetails;
 import uk.gov.hmcts.opal.dto.legacy.common.LegacyPartyDetails;
 import uk.gov.hmcts.opal.dto.legacy.common.OrganisationDetails;
@@ -25,6 +26,26 @@ class LegacyUpdateMinorCreditorAccountRequestTest {
                 .organisationFlag(true)
                 .organisationDetails(OrganisationDetails.builder()
                     .organisationName("Updated Ltd")
+                    .organisationAliases(new OrganisationDetails.OrganisationAlias[] {
+                        OrganisationDetails.OrganisationAlias.builder()
+                            .aliasId("ORG-1")
+                            .sequenceNumber((short) 7)
+                            .organisationName("Updated Alias")
+                            .build()
+                    })
+                    .build())
+                .individualDetails(IndividualDetails.builder()
+                    .title("Ms")
+                    .firstNames("Creditor")
+                    .surname("Updated")
+                    .individualAliases(new IndividualDetails.IndividualAlias[] {
+                        IndividualDetails.IndividualAlias.builder()
+                            .aliasId("IND-1")
+                            .sequenceNumber((short) 9)
+                            .surname("AliasSurname")
+                            .forenames("AliasForenames")
+                            .build()
+                    })
                     .build())
                 .build())
             .address(AddressDetailsLegacy.builder()
@@ -48,11 +69,41 @@ class LegacyUpdateMinorCreditorAccountRequestTest {
         assertTrue(json.get("party_details").get("organisation_flag").asBoolean());
         assertEquals("Updated Ltd",
             json.get("party_details").get("organisation_details").get("organisation_name").asText());
+        assertEquals("ORG-1",
+            json.get("party_details").get("organisation_details").get("organisation_aliases").get(0)
+                .get("alias_id").asText());
+        assertEquals(7,
+            json.get("party_details").get("organisation_details").get("organisation_aliases").get(0)
+                .get("sequence_number").asInt());
+        assertEquals("Updated Alias",
+            json.get("party_details").get("organisation_details").get("organisation_aliases").get(0)
+                .get("organisation_name").asText());
+        assertEquals("Creditor",
+            json.get("party_details").get("individual_details").get("forenames").asText());
+        assertEquals("IND-1",
+            json.get("party_details").get("individual_details").get("individual_aliases").get(0)
+                .get("alias_id").asText());
+        assertEquals(9,
+            json.get("party_details").get("individual_details").get("individual_aliases").get(0)
+                .get("sequence_number").asInt());
+        assertEquals("AliasForenames",
+            json.get("party_details").get("individual_details").get("individual_aliases").get(0)
+                .get("forenames").asText());
         assertEquals("99 Updated Road", json.get("address").get("address_line_1").asText());
         assertEquals("Ref-01", json.get("payment").get("account_reference").asText());
 
         assertFalse(json.get("party_details").has("partyId"));
         assertFalse(json.get("party_details").has("organisationFlag"));
+        assertFalse(json.get("party_details").get("organisation_details").has("organisationAliases"));
+        assertFalse(json.get("party_details").get("organisation_details").get("organisation_aliases").get(0)
+            .has("aliasId"));
+        assertFalse(json.get("party_details").get("organisation_details").get("organisation_aliases").get(0)
+            .has("sequenceNumber"));
+        assertFalse(json.get("party_details").get("individual_details").has("firstNames"));
+        assertFalse(json.get("party_details").get("individual_details").get("individual_aliases").get(0)
+            .has("aliasId"));
+        assertFalse(json.get("party_details").get("individual_details").get("individual_aliases").get(0)
+            .has("sequenceNumber"));
         assertFalse(json.get("address").has("addressLine1"));
     }
 }
