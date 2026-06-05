d045244330d72c77868118bcb78aedf382c92728
hennaparmar <henna.parmar@hmcts.net>
2026-06-05 11:59:09 +0100

    Remove unused import in DefendantAccountApiControllerTest for cleaner code

--- src/test/java/uk/gov/hmcts/opal/controllers/DefendantAccountApiControllerTest.java
+++ src/test/java/uk/gov/hmcts/opal/controllers/DefendantAccountApiControllerTest.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.opal.controllers;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
