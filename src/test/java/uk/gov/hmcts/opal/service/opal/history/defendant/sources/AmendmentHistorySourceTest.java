b1a2fcf3046b4e3b381fe86a3a722e216484af87
hennaparmar <henna.parmar@hmcts.net>
2026-06-08 10:37:52 +0100

    Remove unused import in AmendmentHistorySourceTest to clean up code

--- src/test/java/uk/gov/hmcts/opal/service/opal/history/defendant/sources/AmendmentHistorySourceTest.java
+++ src/test/java/uk/gov/hmcts/opal/service/opal/history/defendant/sources/AmendmentHistorySourceTest.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.opal.service.opal.history.defendant.sources;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDateTime;
