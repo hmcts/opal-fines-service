6c3dd8e04c433e7498d30f7d5ba0bb35beedaa27
benedwards <ben.edwards@cgi.com>
2026-06-08 17:11:48 +0100

    Updated cache logic

--- src/main/java/uk/gov/hmcts/opal/dto/reference/OffenceSearchData.java
+++ src/main/java/uk/gov/hmcts/opal/dto/reference/OffenceSearchData.java
@@ -4,15 +4,21 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.time.OffsetDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public record OffenceSearchData(
-    @JsonProperty("offence_id") Long offenceId,
-    @JsonProperty("cjs_code") String cjsCode,
-    @JsonProperty("offence_title") String offenceTitle,
-    @JsonProperty("offence_title_cy") String offenceTitleCy,
-    @JsonProperty("date_used_from") OffsetDateTime dateUsedFrom,
-    @JsonProperty("date_used_to") OffsetDateTime dateUsedTo,
-    @JsonProperty("offence_oas") String offenceOas,
-    @JsonProperty("offence_oas_cy") String offenceOasCy) {
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class OffenceSearchData {
+    @JsonProperty("offence_id") Long offenceId;
+    @JsonProperty("cjs_code") String cjsCode;
+    @JsonProperty("offence_title") String offenceTitle;
+    @JsonProperty("offence_title_cy") String offenceTitleCy;
+    @JsonProperty("date_used_from") OffsetDateTime dateUsedFrom;
+    @JsonProperty("date_used_to") OffsetDateTime dateUsedTo;
+    @JsonProperty("offence_oas") String offenceOas;
+    @JsonProperty("offence_oas_cy") String offenceOasC;
 }
