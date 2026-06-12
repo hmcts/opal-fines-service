6c48b2bccf1e88da5f617e8e73e840c8d81bf46a
hennaparmar <henna.parmar@hmcts.net>
2026-06-12 15:53:22 +0100

    Add unit tests for business unit warning threshold in operational reports

--- src/test/java/uk/gov/hmcts/opal/service/ReportServiceTest.java
+++ src/test/java/uk/gov/hmcts/opal/service/ReportServiceTest.java
@@ -13,6 +13,8 @@ import static uk.gov.hmcts.opal.testdata.ReportTestData.createReportEntityWithNu
 import static uk.gov.hmcts.opal.testdata.ReportTestData.defaultReportEntityBuilder;
 
 import jakarta.persistence.EntityNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Nested;
@@ -27,8 +29,10 @@ import uk.gov.hmcts.opal.authorisation.model.FinesPermission;
 import uk.gov.hmcts.opal.common.user.authorisation.exception.PermissionNotAllowedException;
 import uk.gov.hmcts.opal.common.user.authorisation.model.UserState;
 import uk.gov.hmcts.opal.entity.ReportEntity;
+import uk.gov.hmcts.opal.entity.configurationitem.ConfigurationItemEntity;
 import uk.gov.hmcts.opal.generated.model.ReportReports;
 import uk.gov.hmcts.opal.mapper.ReportEntityMapper;
+import uk.gov.hmcts.opal.repository.ConfigurationItemRepository;
 import uk.gov.hmcts.opal.repository.ReportRepository;
 
 @ExtendWith(MockitoExtension.class)
@@ -39,6 +43,9 @@ class ReportServiceTest {
 
     @Mock
     private ReportEntityMapper reportMapper;
+
+    @Mock
+    private ConfigurationItemRepository configurationItemRepository;
 
     @Mock
     private UserStateService userStateService;
@@ -73,9 +80,28 @@ class ReportServiceTest {
             when(userState.anyBusinessUnitUserHasPermission(null)).thenReturn(true);
             when(userState.anyBusinessUnitUserHasPermission(any(FinesPermission.class))).thenReturn(true);
 
+            if ("operational_report_enforcement".equals(entity.getReportId())
+                || "operational_report_payment".equals(entity.getReportId())) {
+                when(configurationItemRepository.findByItemNameAndBusinessUnitIdIsNull(
+                    "OPERATIONAL_REPORT_BU_WARNING_THRESHOLD"
+                )).thenReturn(Optional.of(
+                    ConfigurationItemEntity.builder().itemValue("10").build()
+                ));
+            }
+
             ReportReports result = reportService.getReport(entity.getReportId());
 
-            assertEquals(reportDto, result);
+            if ("operational_report_enforcement".equals(entity.getReportId())
+                || "operational_report_payment".equals(entity.getReportId())) {
+                Map<String, Object> expectedParameters = new HashMap<>();
+                if (reportDto.getReportParameters() != null) {
+                    expectedParameters.putAll(reportDto.getReportParameters());
+                }
+                expectedParameters.put("business_unit_warning_threshold", 10);
+                assertEquals(expectedParameters, result.getReportParameters());
+            } else {
+                assertEquals(reportDto, result);
+            }
             verify(reportRepository).findById(entity.getReportId());
             verify(reportMapper).toDto(entity);
         }
