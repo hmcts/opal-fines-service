d1855c3f355b022644621dbd2cc7ef4a75a960cc
Jordan Muscott <288038593+jordan-muscott-cgi@users.noreply.github.com>
2026-06-22 15:25:01 +0100

    removed unused import

--- src/main/java/uk/gov/hmcts/opal/repository/EnforcementAccountTypeRepository.java
+++ src/main/java/uk/gov/hmcts/opal/repository/EnforcementAccountTypeRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import uk.gov.hmcts.opal.entity.enforcement.EnforcementAccountTypeEntity;
 
-import java.util.List;
-
 @Repository
 public interface EnforcementAccountTypeRepository extends JpaRepository<EnforcementAccountTypeEntity, Long> {
 }
