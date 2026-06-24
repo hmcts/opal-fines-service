8f27a1081c217b14913fa0b46b5db695dbe1991b
marcmollins <Marc.Mollins@HMCTS.NET>
2026-06-24 14:23:19 +0100

    NO-NOT-MERGE--FMEA-Testing-Branch
    updating minorcreditor to call user-service

--- src/main/java/uk/gov/hmcts/opal/config/FeignConfiguration.java
+++ src/main/java/uk/gov/hmcts/opal/config/FeignConfiguration.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.opal.config;
 
-import feign.RequestInterceptor;
 import feign.codec.Decoder;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.cloud.openfeign.support.FeignHttpMessageConverters;
@@ -15,19 +14,5 @@ public class FeignConfiguration {
     @Bean
     public Decoder feignDecoder(ObjectProvider<FeignHttpMessageConverters> messageConverters) {
         return new ResponseEntityDecoder(new SpringDecoder(messageConverters));
-    }
-
-    @Bean
-    public RequestInterceptor userServiceFmeaHeaderInterceptor(UserServiceFmeaProperties properties) {
-        return requestTemplate -> {
-            if (!properties.enabled()) {
-                return;
-            }
-
-            String path = requestTemplate.path();
-            if (path != null && path.startsWith("/v2/users/0/state")) {
-                requestTemplate.header(properties.headerName(), properties.headerValue());
-            }
-        };
     }
 }
