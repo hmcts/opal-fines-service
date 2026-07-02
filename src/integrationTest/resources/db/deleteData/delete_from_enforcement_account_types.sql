bf9b7bfd383b979746e458eeed714aea2db598bd
Thomas Croasdale <288008923+thomas-croasdale-cgi@users.noreply.github.com>
2026-07-02 09:35:35 +0100

    PO-2435 Reset test data more safely

--- src/integrationTest/resources/db/deleteData/delete_from_enforcement_account_types.sql
+++ src/integrationTest/resources/db/deleteData/delete_from_enforcement_account_types.sql
@@ -1,13 +1,15 @@
 -- Reset the test data
-DELETE FROM enforcement_account_types
-WHERE enforcement_account_type_id IN (1, 2, 3);
+UPDATE enforcement_account_types
+SET account_type='COL', minimum_balance=NULL, enforcement_account_type='COLH',
+    account_type_path='L', version_number=1
+WHERE enforcement_account_type_id=1;
 
-INSERT INTO enforcement_account_types (enforcement_account_type_id, account_type, minimum_balance,
-                                       enforcement_account_type, account_type_path, version_number)
-VALUES
-  (1, 'COL', NULL, 'COLH',
-        'L', 1),
-  (2, 'COL', NULL, 'COLL',
-   'L', 1),
-(3, 'A', NULL, 'AH',
-  'H', 1);
+UPDATE enforcement_account_types
+SET account_type='COL', minimum_balance=NULL, enforcement_account_type='COLL',
+    account_type_path='L', version_number=1
+WHERE enforcement_account_type_id=2;
+
+UPDATE enforcement_account_types
+SET account_type='A', minimum_balance=NULL, enforcement_account_type='AH',
+    account_type_path='H', version_number=1
+WHERE enforcement_account_type_id=3;
