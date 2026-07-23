6c714f4cc1806b6a1d785a7a9a3028e570ab0d14
Chi Cho <chi.cho@hmcts.net>
2026-07-23 09:48:01 +0100

    added version history

--- src/main/resources/db/migration/ddl/V1_270__alter_v_defendant_accounts_header_originator_fields.sql
+++ src/main/resources/db/migration/ddl/V1_270__alter_v_defendant_accounts_header_originator_fields.sql
@@ -1,5 +1,5 @@
 /**
-* OPAL Program
+* CGI OPAL Program
 *
 * MODULE      : alter_v_defendant_accounts_header_originator_fields.sql
 *
@@ -7,62 +7,73 @@
 *
 * VERSION HISTORY:
 *
-* Date          Author         Version     Nature of Change
-* ----------    -----------    --------    ----------------------------------------------------------------------------
-* 17/07/2026    C Cho          1.0         PO-2976 - Add originator_type and originator_name to defendant account header view.
+* Date          Author      Version     Nature of Change
+* ----------    --------    --------    ------------------------------------------------------------------------------------
+* 29/07/2025    C Cho       1.0         PO-1641 Create view to retrieve defendant header summary information
+* 01/12/2025    C Cho       1.1         PO-2338 Add has_consolidated_accounts flag
+* 06/03/2026    T McCallion 1.2         Need to recreate view as part of new ENUMs work
+*                                       Taken from: V20251204_434__amend_view_v_defendant_accounts_header.sql
+*                                                   Only change: Added DROP statement
+* 10/03/2026    T McCallion 1.3         Need to recreate view as part of new ENUMs work (PO-2931)
+*                                       Taken from: V20260310_460__recreate_view_v_defendant_accounts_header.sql. No changes
+* 17/03/2026    T McCallion 1.4         Need to recreate view as part of new ENUMs work (PO-2868)
+*                                       Taken from: V20260316_465__recreate_view_v_defendant_accounts_header.sql. No changes
+* 17/07/2026    C Cho       1.5         PO-2976 - Add originator_type and originator_name to defendant account header view.
 *
 **/
+DROP VIEW IF EXISTS v_defendant_accounts_header;
 
-CREATE OR REPLACE VIEW public.v_defendant_accounts_header AS
- SELECT DISTINCT da.defendant_account_id,
-    da.version_number,
-    da.account_number,
-    da.prosecutor_case_reference,
-    da.account_status,
-    da.account_type,
-    da.amount_paid AS paid_written_off,
-    da.account_balance,
-    da.amount_imposed,
-    public.f_arrears(da.defendant_account_id) AS arrears,
-    dap_def.defendant_account_party_id,
-        CASE
-            WHEN (dap_def.debtor IS TRUE) THEN dap_def.association_type
-            ELSE NULL::public.t_association_type_enum
-        END AS debtor_type,
-    p_def.party_id,
-    p_def.title,
-    p_def.forenames,
-    p_def.surname,
-    p_def.birth_date,
-    p_def.organisation,
-    p_def.organisation_name,
-    bu.business_unit_id,
-    bu.business_unit_name,
-    bu.business_unit_code,
-    fpo.ticket_number,
-    dap_pg.defendant_account_party_id AS parent_guardian_account_party_id,
-        CASE
-            WHEN (dap_pg.defendant_account_id IS NOT NULL) THEN true
-            ELSE false
-        END AS has_parent_guardian,
-        CASE
-            WHEN (dap_pg.debtor IS TRUE) THEN dap_pg.association_type
-            ELSE NULL::public.t_association_type_enum
-        END AS parent_guardian_debtor_type,
-        CASE
-            WHEN (dt.defendant_account_id IS NOT NULL) THEN true
-            ELSE false
-        END AS has_consolidated_accounts,
-    da.originator_type,
-    da.originator_name
-   FROM ((((((public.defendant_accounts da
-     JOIN public.business_units bu ON ((da.business_unit_id = bu.business_unit_id)))
-     JOIN public.defendant_account_parties dap_def ON (((da.defendant_account_id = dap_def.defendant_account_id) AND (dap_def.association_type = 'Defendant'::public.t_association_type_enum))))
-     JOIN public.parties p_def ON ((dap_def.party_id = p_def.party_id)))
-     LEFT JOIN public.fixed_penalty_offences fpo ON ((da.defendant_account_id = fpo.defendant_account_id)))
-     LEFT JOIN public.defendant_account_parties dap_pg ON (((da.defendant_account_id = dap_pg.defendant_account_id) AND (dap_pg.association_type = 'Parent/Guardian'::public.t_association_type_enum))))
-     LEFT JOIN ( SELECT DISTINCT defendant_transactions.defendant_account_id
-           FROM public.defendant_transactions
-          WHERE ((defendant_transactions.transaction_type = 'CONSOL'::public.t_defendant_transaction_type_enum) AND (defendant_transactions.associated_record_type = 'defendant_accounts'::public.t_associated_record_type_enum))) dt ON ((da.defendant_account_id = dt.defendant_account_id)));
+CREATE OR REPLACE VIEW v_defendant_accounts_header
+AS 
+    SELECT DISTINCT
+           da.defendant_account_id
+         , da.version_number
+         , da.account_number
+         , da.prosecutor_case_reference
+         , da.account_status
+         , da.account_type
+         , da.amount_paid AS paid_written_off
+         , da.account_balance
+         , da.amount_imposed
+         , f_arrears(da.defendant_account_id) AS arrears
+         , dap_def.defendant_account_party_id
+         , CASE WHEN dap_def.debtor IS TRUE THEN dap_def.association_type ELSE NULL END AS debtor_type
+         , p_def.party_id
+         , p_def.title
+         , p_def.forenames
+         , p_def.surname
+         , p_def.birth_date
+         , p_def.organisation
+         , p_def.organisation_name
+         , bu.business_unit_id
+         , bu.business_unit_name
+         , bu.business_unit_code
+         , fpo.ticket_number
+         , dap_pg.defendant_account_party_id AS parent_guardian_account_party_id
+         , CASE WHEN dap_pg.defendant_account_id IS NOT NULL THEN TRUE ELSE FALSE END AS has_parent_guardian
+         , CASE WHEN dap_pg.debtor IS TRUE THEN dap_pg.association_type ELSE NULL END AS parent_guardian_debtor_type
+         , CASE WHEN dt.defendant_account_id IS NOT NULL THEN TRUE ELSE FALSE END AS has_consolidated_accounts
+         , da.originator_type
+           da.originator_name
+      FROM defendant_accounts da
+      JOIN business_units bu
+        ON da.business_unit_id = bu.business_unit_id                
+      JOIN defendant_account_parties dap_def
+        ON da.defendant_account_id = dap_def.defendant_account_id
+       AND dap_def.association_type = 'Defendant'
+      JOIN parties p_def
+        ON dap_def.party_id = p_def.party_id
+ LEFT JOIN fixed_penalty_offences fpo
+        ON da.defendant_account_id = fpo.defendant_account_id
+ LEFT JOIN defendant_account_parties dap_pg
+        ON da.defendant_account_id = dap_pg.defendant_account_id
+       AND dap_pg.association_type = 'Parent/Guardian'
+ LEFT JOIN ( SELECT DISTINCT defendant_account_id
+           FROM defendant_transactions
+              WHERE transaction_type = 'CONSOL'
+                AND associated_record_type = 'defendant_accounts'
+           ) dt
+        ON da.defendant_account_id = dt.defendant_account_id
+;
 
-COMMENT ON VIEW public.v_defendant_accounts_header IS 'Retrieves defendant account header information for the Defendant Header Summary';
+COMMENT ON VIEW v_defendant_accounts_header IS 'Retrieves defendant account header information for the Defendant Header Summary';
