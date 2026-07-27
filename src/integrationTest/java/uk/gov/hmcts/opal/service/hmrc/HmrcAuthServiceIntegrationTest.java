e9157f46371e6521559b5534d4c7c22bb9dde0a9
Jordan Muscott <288038593+jordan-muscott-cgi@users.noreply.github.com>
2026-07-27 13:20:58 +0100

    fix Value annotations. Correclty start WireMock server. Fix 1st integration test. Add integration test for cache

--- src/integrationTest/java/uk/gov/hmcts/opal/service/hmrc/HmrcAuthServiceIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/opal/service/hmrc/HmrcAuthServiceIntegrationTest.java
@@ -1,13 +1,16 @@
 package uk.gov.hmcts.opal.service.hmrc;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -20,10 +23,15 @@ import uk.gov.hmcts.opal.AbstractIntegrationTest;
 import uk.gov.hmcts.opal.config.cache.CacheKeys;
 import uk.gov.hmcts.opal.service.hmrc.response.HMRCAuthToken;
 
-//@ActiveProfiles({"hmrc"})
 @Slf4j(topic = "opal.HmrcAuthServiceIntegrationTest")
 @DisplayName("HMRC Auth Service Integration Test")
-public class HmrcAuthServiceIntegrationTest extends AbstractIntegrationTest {
+@WireMockTest(httpPort = 8080)
+//@EnableWireMock({
+//    @ConfigureWireMock(
+//        port = 8888)
+//})
+public class HmrcAuthServiceIntegrationTest
+    extends AbstractIntegrationTest { // TODO do i want to be inheriting from this?
 
     @Autowired
     private StringRedisTemplate redisTemplate;
@@ -31,35 +39,55 @@ public class HmrcAuthServiceIntegrationTest extends AbstractIntegrationTest {
     @Autowired
     private RestClient restClient;
 
+    @Autowired
     private HmrcAuthService hmrcAuthService;
 
     private HMRCAuthToken hmrcAuthToken = new HMRCAuthToken(
         "xxxx-test-token-xxxx", "bearer", 14400, "test-scope1+test-scope2"
     );
 
+    @Override
     @BeforeEach
-    void setup() {
-        hmrcAuthService = new HmrcAuthService( //TODO why is @Value not working
-            restClient, "test-hmrc-client-id",
-            "test-hmrc-client-secret",
-            "test-scope1+test-scope2",
-            "http://localhost:4553/oauth/token"
-        );
+    public void beforeEach() {
         redisTemplate.delete(CacheKeys.HMRC_AUTH_TOKEN);
+
+        stubFor(get(urlPathEqualTo("/oauth/token"))
+            .withQueryParam("client_id", equalTo("test-hmrc-client-id"))
+            .withQueryParam("client_secret", equalTo("test-hmrc-client-secret"))
+            .withQueryParam("grant_type", equalTo("client_credentials"))
+            .withQueryParam("scope", equalTo("test-scope1 test-scope2"))
+            .willReturn(aResponse()
+                .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                .withBody(objectMapper.writeValueAsString(hmrcAuthToken))));
     }
 
     @Test
     void correctlyCallsHmrcEndpointAndReturnsDto() {
-        WireMock.configureFor("localhost", 4553);
-        stubFor(get("/oauth/token")
-            .willReturn(aResponse()
-                .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
-                .withBody(objectMapper.writeValueAsString(hmrcAuthToken))));
 
         HMRCAuthToken returnedAuthToken = hmrcAuthService.getAuthToken();
 
-        assertEquals(hmrcAuthToken, returnedAuthToken);
-        WireMock.verify(1, getRequestedFor(urlEqualTo("/oauth/token")));
+        assertAll(
+            () -> assertEquals(hmrcAuthToken.getAccessToken(), returnedAuthToken.getAccessToken()),
+            () -> assertEquals(hmrcAuthToken.getTokenType(), returnedAuthToken.getTokenType()),
+            () -> assertEquals(hmrcAuthToken.getExpiresIn(), returnedAuthToken.getExpiresIn()),
+            () -> assertEquals(hmrcAuthToken.getScope(), returnedAuthToken.getScope())
+        );
+        WireMock.verify(1, getRequestedFor(urlPathEqualTo("/oauth/token")));
     }
 
+
+    @Test
+    void multipleCallsToHmrcEndpoint_UtiliseCache() {
+        HMRCAuthToken returnedAuthToken1 = hmrcAuthService.getAuthToken();
+
+        HMRCAuthToken returnedAuthToken2 = hmrcAuthService.getAuthToken();
+
+        WireMock.verify(1, getRequestedFor(urlPathEqualTo("/oauth/token")));
+        assertAll(
+            () -> assertEquals(returnedAuthToken1.getAccessToken(), returnedAuthToken2.getAccessToken()),
+            () -> assertEquals(returnedAuthToken1.getTokenType(), returnedAuthToken2.getTokenType()),
+            () -> assertEquals(returnedAuthToken1.getExpiresIn(), returnedAuthToken2.getExpiresIn()),
+            () -> assertEquals(returnedAuthToken1.getScope(), returnedAuthToken2.getScope())
+        );
+    }
 }
