53d49a855e6e643da1ec674e1e72e0c8878fe581
alex-reeves-cgi <275961728+alex-reeves-cgi@users.noreply.github.com>
2026-07-27 09:17:06 +0100

    PO-3470 added cash list report csv mapper

---
+++ src/main/java/uk/gov/hmcts/opal/service/report/mapper/csv/CashListReportCSVMapper.java
@@ -0,0 +1,52 @@
+package uk.gov.hmcts.opal.service.report.mapper.csv;
+
+import static uk.gov.hmcts.opal.service.report.CommonReportStringConstants.NEW_LINE;
+
+import java.util.List;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.opal.service.report.CashListReportData;
+import uk.gov.hmcts.opal.service.report.CashListReportData.CashListEntry;
+
+@Component
+public class CashListReportCSVMapper implements ReportCSVMapper<CashListReportData> {
+
+    @Override
+    public Class<CashListReportData> getReportDataType() {
+        return CashListReportData.class;
+    }
+
+    @Override
+    public String reportToCSVString(CashListReportData cashListReportData) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(dataListToFullCSVRow(headerRow()));
+        int entryNumber = 1;
+        for (CashListEntry cashListEntry : cashListReportData.getEntries()) {
+            sb.append(dataListToFullCSVRow(convertDataRow(entryNumber++, cashListEntry)));
+        }
+        sb.append("Total").append(NEW_LINE);
+        sb.append(cashListReportData.getTotal().toString());
+        return sb.toString();
+    }
+
+    private List<String> headerRow() {
+        return List.of("Entry",
+            "Type",
+            "Susp.",
+            "Account no",
+            "Name",
+            "Name (Additional Information)",
+            "Payment",
+            "Amount");
+    }
+
+    private List<String> convertDataRow(int entryNumber, CashListEntry cashListEntry) {
+        return List.of(""+entryNumber,
+            cashListEntry.getType(),
+            cashListEntry.getSuspense(),
+            cashListEntry.getAccountNumber(),
+            cashListEntry.getName(),
+            cashListEntry.getNameAdditionalInformation(),
+            cashListEntry.getPaymentMethod(),
+            cashListEntry.getAmount().toString());
+    }
+}
