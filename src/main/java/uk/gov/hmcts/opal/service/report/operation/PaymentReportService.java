fcb8a1ef73f6d8d763d8b6d076b6bebf12f3955f
Russell Dodd <russell.dodd@hmcts.net>
2026-07-28 09:58:54 +0100

    Refactor of Opal 'Specification' classes to make code more re-usable and composable

--- src/main/java/uk/gov/hmcts/opal/service/report/operation/PaymentReportService.java
+++ src/main/java/uk/gov/hmcts/opal/service/report/operation/PaymentReportService.java
@@ -5,14 +5,11 @@ import static uk.gov.hmcts.opal.service.report.ReportId.OP_PAYMENT;
 import static uk.gov.hmcts.opal.service.report.ReportType.SUMMARY;
 
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import tools.jackson.databind.ObjectMapper;
 import uk.gov.hmcts.opal.dto.report.operation.OperationReportByPaymentFiltersDto;
-import uk.gov.hmcts.opal.dto.report.operation.PaymentReportMode;
 import uk.gov.hmcts.opal.entity.ReportInstanceEntity;
 import uk.gov.hmcts.opal.entity.defendantaccount.DefendantAccountEntity;
 import uk.gov.hmcts.opal.repository.DefendantAccountRepository;
@@ -42,37 +39,10 @@ public class PaymentReportService implements ReportInterface<OperationReportData
     public OperationReportDataInterface generateReportData(ReportInstanceEntity reportInstance) {
         OperationReportByPaymentFiltersDto filters = readFilters(reportInstance);
         validator.validate(filters);
-        List<DefendantAccountEntity> baseAccounts = defendantAccountRepository.findAll(
+        List<DefendantAccountEntity> accounts = defendantAccountRepository.findAll(
             OperationReportSpecs.accountFiltersSpec(filters),
-            Sort.by(ACCOUNT_NUMBER)
-        );
-        Set<String> baseAccountNumbers = baseAccounts.stream()
-            .map(DefendantAccountEntity::getAccountNumber)
-            .collect(Collectors.toSet());
-
-        PaymentReportMode reportMode = filters.getReportMode();
-        return switch (reportMode) {
-            case SINCE_LAST_ENFORCEMENT -> mapReportData(
-                filters,
-                applyBaseFilter(
-                    defendantAccountRepository.findAccountsWithPaymentMadeAfterLastEnforcementAction(
-                        filters.getSinceLastEnforcementAction().name(),
-                        Boolean.TRUE.equals(filters.getIsPaymentMade())
-                    ),
-                    baseAccountNumbers
-                )
-            );
-            case WITH_REGF -> mapReportData(
-                filters,
-                applyBaseFilter(
-                    defendantAccountRepository.findAccountsWithPaymentMadeAfterFirstRegfEnforcement(
-                        Boolean.TRUE.equals(filters.getIsPaymentMade())
-                    ),
-                    baseAccountNumbers
-                )
-            );
-            default -> mapReportData(filters, baseAccounts);
-        };
+            Sort.by(ACCOUNT_NUMBER));
+        return mapReportData(filters, accounts);
     }
 
     @Override
@@ -92,21 +62,11 @@ public class PaymentReportService implements ReportInterface<OperationReportData
             : detailedResultMapper.map(accounts);
     }
 
-    private List<DefendantAccountEntity> applyBaseFilter(
-        List<DefendantAccountEntity> accounts,
-        Set<String> baseAccountNumbers
-    ) {
-        return accounts.stream()
-            .filter(account -> baseAccountNumbers.contains(account.getAccountNumber()))
-            .toList();
-    }
-
     private OperationReportByPaymentFiltersDto readFilters(ReportInstanceEntity reportInstance) {
         try {
             return objectMapper.readValue(
                 reportInstance.getReportParameters(),
-                OperationReportByPaymentFiltersDto.class
-            );
+                OperationReportByPaymentFiltersDto.class);
         } catch (Exception e) {
             throw new RuntimeException("Failed to parse report filters", e);
         }
