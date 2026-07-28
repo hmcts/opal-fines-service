f58e20a438ef2eac9ddad8ace15a80c8e17ba81c
Thomas Croasdale <288008923+thomas-croasdale-cgi@users.noreply.github.com>
2026-07-28 15:09:53 +0100

    PO-2395 Integration test fixes

--- src/main/java/uk/gov/hmcts/opal/interceptor/FeignRequestInterceptor.java
+++ src/main/java/uk/gov/hmcts/opal/interceptor/FeignRequestInterceptor.java
@@ -2,15 +2,15 @@ package uk.gov.hmcts.opal.interceptor;
 
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.opal.service.hmrc.HMRCAuthentication;
 
 @Component
-@AllArgsConstructor
 public class FeignRequestInterceptor implements RequestInterceptor {
 
-    private final HMRCAuthentication auth;
+    @Autowired(required=false) //TODO: remove required=false once this interface has an implementation.
+    private HMRCAuthentication auth;
 
     @Override
     public void apply(RequestTemplate template) {
--- src/main/resources/openapi/hmrc/README.md
+++ src/main/resources/openapi/hmrc/README.md
@@ -1,3 +1,3 @@
 # TODO: Delete this file.
 This file is only here to ensure the /hmrc directory it lives in stays in git, without it the builds will fail.
-Once this /hmrc directory has the HMRC openapi specs in this file can be safely dfeleted
+Once this /hmrc directory contains the HMRC openapi specs, this file can be safely deleted.
