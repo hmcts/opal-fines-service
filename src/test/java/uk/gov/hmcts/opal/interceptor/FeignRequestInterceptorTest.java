7570192862700bf5bea975f913803e7d7bf71c7e
Thomas Croasdale <288008923+thomas-croasdale-cgi@users.noreply.github.com>
2026-07-29 09:39:31 +0100

    PO-2395 Fix style

--- src/test/java/uk/gov/hmcts/opal/interceptor/FeignRequestInterceptorTest.java
+++ src/test/java/uk/gov/hmcts/opal/interceptor/FeignRequestInterceptorTest.java
@@ -1,25 +1,16 @@
 package uk.gov.hmcts.opal.interceptor;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 
 import feign.RequestTemplate;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.web.method.HandlerMethod;
-import uk.gov.hmcts.opal.annotation.CheckAcceptHeader;
 import uk.gov.hmcts.opal.service.hmrc.HmrcAuthentication;
 
 @ExtendWith(MockitoExtension.class)
@@ -38,7 +29,7 @@ class FeignRequestInterceptorTest {
         private FeignRequestInterceptor interceptor;
 
         @Test
-        void AppliesAuthenticationTokenWhenAuthIsAvailable() {
+        void appliesAuthenticationTokenWhenAuthIsNotNull() {
             when(auth.getToken()).thenReturn("some-token");
 
             interceptor.apply(request);
@@ -57,7 +48,7 @@ class FeignRequestInterceptorTest {
         private FeignRequestInterceptor interceptor;
 
         @Test
-        void AppliesAuthenticationTokenWhenAuthIsAvailable() {
+        void doesNotApplyAuthTokenWhenNull() {
             interceptor.apply(request);
 
             verifyNoInteractions(request);
