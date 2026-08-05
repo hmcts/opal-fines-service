bbb53629eae16290cf958022b741e6a91d425340
alex-reeves-cgi <275961728+alex-reeves-cgi@users.noreply.github.com>
2026-08-05 14:38:16 +0100

    PO-6409 checkstyle

--- src/test/java/uk/gov/hmcts/opal/controllers/MinorCreditorControllerTest.java
+++ src/test/java/uk/gov/hmcts/opal/controllers/MinorCreditorControllerTest.java
@@ -12,12 +12,7 @@ import uk.gov.hmcts.opal.dto.Creditor;
 import uk.gov.hmcts.opal.dto.MinorCreditorSearch;
 import uk.gov.hmcts.opal.service.MinorCreditorService;
 
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
