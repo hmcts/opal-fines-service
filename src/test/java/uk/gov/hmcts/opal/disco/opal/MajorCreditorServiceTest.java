eb6beb735e47f299dd4d783be2aeb8b198ed10ac
joesheencgi <292120363+joesheencgi@users.noreply.github.com>
2026-08-10 16:53:02 +0100

    Fixed code imports

--- src/test/java/uk/gov/hmcts/opal/disco/opal/MajorCreditorServiceTest.java
+++ src/test/java/uk/gov/hmcts/opal/disco/opal/MajorCreditorServiceTest.java
@@ -15,6 +15,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
