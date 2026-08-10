e6d1837d4b20a97646a89e1a275931915ae74a43
Justin <ju.morgan@cgi.com>
2026-08-10 10:20:14 +0100

    Test case coverage for missing Int test aim

--- src/integrationTest/java/uk/gov/hmcts/opal/controllers/TestingSupportControllerDeleteInterfaceJobsIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/opal/controllers/TestingSupportControllerDeleteInterfaceJobsIntegrationTest.java
@@ -1,14 +1,24 @@
 package uk.gov.hmcts.opal.controllers;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.doAnswer;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.net.ConnectException;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.postgresql.util.PSQLException;
+import org.postgresql.util.PSQLState;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.ResultActions;
 import uk.gov.hmcts.opal.AbstractIntegrationTest;
@@ -17,12 +27,20 @@ import uk.gov.hmcts.opal.repository.InterfaceJobRepository;
 import uk.gov.hmcts.opal.repository.InterfaceMessageRepository;
 import uk.gov.hmcts.opal.repository.PaymentInRepository;
 import uk.gov.hmcts.opal.repository.TillRepository;
+import uk.gov.hmcts.opal.service.opal.InterfaceJobService;
 import uk.hmcts.zephyr.automation.junit5.annotations.JiraEpic;
 import uk.hmcts.zephyr.automation.junit5.annotations.JiraStory;
 
 @ActiveProfiles({"integration"})
 @Slf4j(topic = "opal.TestingSupportControllerDeleteInterfaceJobsTest")
+@DisplayName("Testing Support Interface Jobs Delete Controller Integration Tests")
 public class TestingSupportControllerDeleteInterfaceJobsIntegrationTest extends AbstractIntegrationTest {
+
+    private static final String URL = "/testing-support/interface-jobs";
+
+    @MockitoSpyBean
+    private InterfaceJobService interfaceJobService;
+
     @Autowired
     private InterfaceFileRepository interfaceFileRepository;
 
@@ -62,7 +80,7 @@ public class TestingSupportControllerDeleteInterfaceJobsIntegrationTest extends 
         assertThat(tillRepository.countByInterfaceFile_InterfaceFileIdIn(interfaceFileIds)).isEqualTo(2);
         assertThat(paymentInRepository.count()).isEqualTo(2);
 
-        ResultActions actions = mockMvc.perform(delete("/testing-support/interface-jobs")
+        ResultActions actions = mockMvc.perform(delete(URL)
             .queryParam("ids", "" + firstInterfaceJobId, "" + secondInterfaceJobId));
 
         actions.andExpect(status().isOk());
@@ -78,8 +96,27 @@ public class TestingSupportControllerDeleteInterfaceJobsIntegrationTest extends 
     @JiraStory("PO-2578")
     @JiraEpic("PO-2468")
     void wouldDeleteInterfaceJobsAndAssociatedDataNoneSupplied() throws Exception {
-        ResultActions actions = mockMvc.perform(delete("/testing-support/interface-jobs"));
+        ResultActions actions = mockMvc.perform(delete(URL));
 
         actions.andExpect(status().isOk());
+    }
+
+    @Test
+    @JiraStory("PO-2578")
+    @JiraEpic("PO-2468")
+    void deleteInterfaceJobs_whenDatabaseIsUnavailable_returnsStandardisedServiceUnavailable() throws Exception {
+        doAnswer(invocation -> {
+            throw new PSQLException("Connection refused", PSQLState.CONNECTION_FAILURE, new ConnectException());
+        }).when(interfaceJobService).deleteInterfaceJobs(anyList());
+
+        mockMvc.perform(delete(URL)
+                .queryParam("ids", "987651")
+                .accept(MediaType.APPLICATION_PROBLEM_JSON))
+            .andExpect(status().isServiceUnavailable())
+            .andExpect(content().contentType(MediaType.APPLICATION_PROBLEM_JSON))
+            .andExpect(jsonPath("$.title").value("Service Unavailable"))
+            .andExpect(jsonPath("$.detail").value("Opal database is currently unavailable"))
+            .andExpect(jsonPath("$.status").value(503))
+            .andExpect(jsonPath("$.type").value("https://hmcts.gov.uk/problems/database-unavailable"));
     }
 }
