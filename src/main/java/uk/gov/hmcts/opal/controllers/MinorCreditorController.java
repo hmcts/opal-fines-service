95cc9661641331ca8dca6cbd78471a3ad27df0a4
Jacob Gardner <jacob.gardner@hmcts.net>
2026-08-12 15:06:31 +0100

    checkstyle

--- src/main/java/uk/gov/hmcts/opal/controllers/MinorCreditorController.java
+++ src/main/java/uk/gov/hmcts/opal/controllers/MinorCreditorController.java
@@ -7,7 +7,6 @@ import static uk.gov.hmcts.opal.util.HttpUtil.buildResponse;
 import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.time.Duration;
 import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
