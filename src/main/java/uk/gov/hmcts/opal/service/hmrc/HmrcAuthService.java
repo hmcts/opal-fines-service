2d8b67b76f9b1dee7bda0afea98592048ae3ed5f
Jordan Muscott <288038593+jordan-muscott-cgi@users.noreply.github.com>
2026-08-13 15:31:00 +0100

    switched to using lombok for constructor

--- src/main/java/uk/gov/hmcts/opal/service/hmrc/HmrcAuthService.java
+++ src/main/java/uk/gov/hmcts/opal/service/hmrc/HmrcAuthService.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.opal.service.hmrc;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
@@ -10,15 +11,11 @@ import uk.gov.hmcts.opal.service.hmrc.clients.response.HmrcAuthToken;
 
 @Service
 @Slf4j(topic = "opal.HmrcAuthService")
+@RequiredArgsConstructor
 public class HmrcAuthService implements HmrcAuthentication {
 
     private final HmrcAuthClient client;
     private final HmrcAuthCreds credentials;
-
-    public HmrcAuthService(HmrcAuthClient client, HmrcAuthCreds credentials) {
-        this.client = client;
-        this.credentials = credentials;
-    }
 
     @Override
     @Cacheable(CacheNames.HMRC_AUTH_SERVICE)
