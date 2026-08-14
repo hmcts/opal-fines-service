ddb24e19dcffd1b397767cebc826a60a1a0d6a36
Justin <ju.morgan@cgi.com>
2026-08-14 15:17:19 +0100

    Bracket fix

--- src/main/java/uk/gov/hmcts/opal/controllers/TestingSupportController.java
+++ src/main/java/uk/gov/hmcts/opal/controllers/TestingSupportController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.opal.common.launchdarkly.FeatureToggle;
@@ -132,6 +131,7 @@ public class TestingSupportController {
         interfaceJobService.deleteInterfaceJobs(interfaceJobIds);
 
         return ResponseEntity.ok().build();
+    }
     /**
      * From {@link MajorCreditorController}.
      * @param criteria search criteria
