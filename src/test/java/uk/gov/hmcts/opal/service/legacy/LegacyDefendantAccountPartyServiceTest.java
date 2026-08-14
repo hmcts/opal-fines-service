00a46eef2a6cc1e8ece24866a4231ac9ca9ea052
hennaparmar <henna.parmar@hmcts.net>
2026-08-14 14:14:26 +0100

    Refactor DefendantAccount.yaml and LegacyDefendantAccountPartyServiceTest for improved readability

--- src/test/java/uk/gov/hmcts/opal/service/legacy/LegacyDefendantAccountPartyServiceTest.java
+++ src/test/java/uk/gov/hmcts/opal/service/legacy/LegacyDefendantAccountPartyServiceTest.java
@@ -1790,9 +1790,10 @@ class LegacyDefendantAccountPartyServiceTest extends LegacyTestsBase {
         );
 
         // Act
-        RemoveDefendantAccountPartyResponseDefendantAccount response = legacyDefendantAccountPartyService.removeDefendantAccountParty(
-            77L, 20010L, (short) 78, "dev_user", "poster", "Poster Name", "\"3\"", null
-        );
+        RemoveDefendantAccountPartyResponseDefendantAccount response =
+            legacyDefendantAccountPartyService.removeDefendantAccountParty(
+                77L, 20010L, (short) 78, "dev_user", "poster", "Poster Name", "\"3\"", null
+            );
 
         // Assert - response contains expected values from legacy response
         assertEquals("20010", response.getDefendantAccountPartyId());
@@ -1829,9 +1830,10 @@ class LegacyDefendantAccountPartyServiceTest extends LegacyTestsBase {
             Mockito.nullable(String.class)
         );
 
-        RemoveDefendantAccountPartyResponseDefendantAccount result = legacyDefendantAccountPartyService.removeDefendantAccountParty(
-            77L, 20010L, (short) 78, "dev_user", "poster", "Poster Name", "\"3\"", null
-        );
+        RemoveDefendantAccountPartyResponseDefendantAccount result =
+            legacyDefendantAccountPartyService.removeDefendantAccountParty(
+                77L, 20010L, (short) 78, "dev_user", "poster", "Poster Name", "\"3\"", null
+            );
 
         assertNull(result.getDefendantAccountPartyId());
         assertNull(result.getVersion());
@@ -1857,9 +1859,10 @@ class LegacyDefendantAccountPartyServiceTest extends LegacyTestsBase {
             Mockito.nullable(String.class)
         );
 
-        RemoveDefendantAccountPartyResponseDefendantAccount response = legacyDefendantAccountPartyService.removeDefendantAccountParty(
-            77L, 20010L, (short) 78, "dev_user", "poster", "Poster Name", "\"3\"", null
-        );
+        RemoveDefendantAccountPartyResponseDefendantAccount response =
+            legacyDefendantAccountPartyService.removeDefendantAccountParty(
+                77L, 20010L, (short) 78, "dev_user", "poster", "Poster Name", "\"3\"", null
+            );
 
         assertNull(response.getDefendantAccountPartyId());
         assertNull(response.getVersion());
