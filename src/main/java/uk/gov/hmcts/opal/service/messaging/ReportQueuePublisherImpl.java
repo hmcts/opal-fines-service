e82f1972682ee410b6d1187b98e9f39c9dfc5dbc
benedwards <ben.edwards@cgi.com>
2026-08-17 14:07:50 +0100

    Added large pdf

--- src/main/java/uk/gov/hmcts/opal/service/messaging/ReportQueuePublisherImpl.java
+++ src/main/java/uk/gov/hmcts/opal/service/messaging/ReportQueuePublisherImpl.java
@@ -6,9 +6,6 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j(topic = "opal.ReportPublishService")
 public class ReportQueuePublisherImpl implements ReportQueuePublisher {
-//    private final JmsTemplate jmsTemplate;
-//    private final ObjectMapper objectMapper;
-//    private final ReportServiceBusProperties properties;
 
     @Override
     public void publish(Long reportInstanceId) {
