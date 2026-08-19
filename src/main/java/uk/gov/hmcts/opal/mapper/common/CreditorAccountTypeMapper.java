2e6f4f52aafd640eb842cb7b943a9ced2660b9b8
alex-reeves-cgi <275961728+alex-reeves-cgi@users.noreply.github.com>
2026-08-19 12:59:49 +0100

    PO-8975 cleaned up old code

--- src/main/java/uk/gov/hmcts/opal/mapper/common/CreditorAccountTypeMapper.java
+++ src/main/java/uk/gov/hmcts/opal/mapper/common/CreditorAccountTypeMapper.java
@@ -3,15 +3,10 @@ package uk.gov.hmcts.opal.mapper.common;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
-import uk.gov.hmcts.opal.dto.common.CreditorAccountTypeReference;
 import uk.gov.hmcts.opal.entity.creditoraccount.CreditorAccountType;
 
 @Mapper(componentModel = "spring")
 public interface CreditorAccountTypeMapper {
-
-    @Mapping(target = "type", source = "enumValue", qualifiedByName = "mapType")
-    @Mapping(target = "displayName", source = "enumValue", qualifiedByName = "mapDisplayName")
-    CreditorAccountTypeReference toDto(CreditorAccountType enumValue);
 
     @Mapping(target = "type", source = "enumValue", qualifiedByName = "mapType")
     @Mapping(target = "displayName", expression = "java(toGeneratedDisplayName(enumValue))")
--- src/main/java/uk/gov/hmcts/opal/mapper/legacy/BusinessUnitSummaryLegacyMapper.java
+++ src/main/java/uk/gov/hmcts/opal/mapper/legacy/BusinessUnitSummaryLegacyMapper.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.opal.mapper.legacy;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
-import uk.gov.hmcts.opal.dto.common.BusinessUnitSummary;
 import uk.gov.hmcts.opal.generated.model.BusinessUnitSummaryCommon;
 
 @Mapper(
@@ -10,8 +9,6 @@ import uk.gov.hmcts.opal.generated.model.BusinessUnitSummaryCommon;
     unmappedTargetPolicy = ReportingPolicy.IGNORE
 )
 public interface BusinessUnitSummaryLegacyMapper {
-
-    BusinessUnitSummary toOpal(uk.gov.hmcts.opal.dto.legacy.common.BusinessUnitSummary businessUnitSummary);
 
     BusinessUnitSummaryCommon toBusinessUnitSummaryCommon(
         uk.gov.hmcts.opal.dto.legacy.common.BusinessUnitSummary businessUnitSummary);
