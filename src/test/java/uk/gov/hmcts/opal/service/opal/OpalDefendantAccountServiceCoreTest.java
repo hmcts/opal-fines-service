012735468825ee80a2a1204bc743b59594027e4a
Krishna Sapkota <krishna.sapkota@hmcts.net>
2026-08-19 12:46:43 +0100

    PO-8985: Improve mapper tests

--- src/test/java/uk/gov/hmcts/opal/service/opal/OpalDefendantAccountServiceCoreTest.java
+++ src/test/java/uk/gov/hmcts/opal/service/opal/OpalDefendantAccountServiceCoreTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.lenient;
@@ -13,16 +12,13 @@ import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.mapstruct.factory.Mappers;
 import uk.gov.hmcts.opal.dto.EnforcementStatus;
 import uk.gov.hmcts.opal.dto.GetDefendantAccountConsolidatedAccountsResult;
 import uk.gov.hmcts.opal.entity.defendantaccount.ConsolidatedAccountEntity;
@@ -30,15 +26,12 @@ import uk.gov.hmcts.opal.entity.defendantaccount.DefendantAccountEntity;
 import uk.gov.hmcts.opal.entity.defendantaccount.AssociationType;
 import uk.gov.hmcts.opal.entity.defendantaccount.DefendantAccountPartiesEntity;
 import uk.gov.hmcts.opal.entity.defendantaccount.DefendantAccountStatus;
-import uk.gov.hmcts.opal.entity.FixedPenaltyOffenceEntity;
 import uk.gov.hmcts.opal.entity.PartyEntity;
 import uk.gov.hmcts.opal.entity.enforcement.EnforcementEntity;
 import uk.gov.hmcts.opal.exception.DefendantAccountNotFoundException;
 import uk.gov.hmcts.opal.generated.model.ConsolidatedAccountDefendantAccount;
-import uk.gov.hmcts.opal.generated.model.GetDefendantAccountFixedPenaltyResponse;
 import uk.gov.hmcts.opal.generated.model.GetEnforcementStatusResponse.DefendantAccountTypeEnum;
 import uk.gov.hmcts.opal.mapper.ConsolidatedAccountMapper;
-import uk.gov.hmcts.opal.mapper.DefendantAccountFixedPenaltyMapper;
 import uk.gov.hmcts.opal.repository.ConsolidatedAccountRepository;
 import uk.gov.hmcts.opal.repository.DefendantAccountRepository;
 import uk.gov.hmcts.opal.repository.DefendantAccountSummaryViewRepository;
@@ -46,7 +39,6 @@ import uk.gov.hmcts.opal.service.persistence.DebtorDetailRepositoryService;
 import uk.gov.hmcts.opal.service.persistence.DefendantAccountRepositoryService;
 import uk.gov.hmcts.opal.service.persistence.EnforcementRepositoryService;
 import uk.gov.hmcts.opal.service.persistence.EnforcerRepositoryService;
-import uk.gov.hmcts.opal.service.persistence.FixedPenaltyOffenceRepositoryService;
 
 @ExtendWith(MockitoExtension.class)
 class OpalDefendantAccountServiceCoreTest {
@@ -67,9 +59,6 @@ class OpalDefendantAccountServiceCoreTest {
     private DefendantAccountSummaryViewRepository dasvRepository;
 
     @Mock
-    private FixedPenaltyOffenceRepositoryService fixedPenaltyOffenceRepositoryService;
-
-    @Mock
     private EnforcerRepositoryService enforcerRepoService;
 
     @Mock
@@ -78,15 +67,9 @@ class OpalDefendantAccountServiceCoreTest {
     @Mock
     private EnforcementRepositoryService enforcementRepositoryService;
 
-    @Spy
-    private DefendantAccountFixedPenaltyMapper defendantAccountFixedPenaltyMapper =
-        Mappers.getMapper(DefendantAccountFixedPenaltyMapper.class);
-
     // Services under test
     @InjectMocks
     private OpalDefendantAccountService service;
-    @InjectMocks
-    private OpalDefendantAccountFixedPenaltyService fpService;
     @InjectMocks
     private OpalDefendantAccountEnforcementService enforcementService;
 
@@ -128,87 +111,6 @@ class OpalDefendantAccountServiceCoreTest {
 
         assertEquals(defendantAccountId, exception.getDefendantAccountId());
         verifyNoInteractions(consolidatedAccountRepository, consolidatedAccountMapper);
-    }
-
-    @Test
-    void vehicleFixedPenaltyFlag_shouldBeFalse_whenVehicleRegistrationIsNullAndFlagFalse() {
-        Long defendantAccountId = 201L;
-        DefendantAccountEntity account = buildMockAccount(defendantAccountId);
-
-        FixedPenaltyOffenceEntity offence = buildMockOffence();
-        offence.setVehicleRegistration(null);
-        offence.setVehicleFixedPenalty(false);
-
-        when(defendantAccountRepositoryService.findById(defendantAccountId)).thenReturn(account);
-        when(fixedPenaltyOffenceRepositoryService.findByDefendantAccountId(defendantAccountId))
-            .thenReturn(offence);
-
-        GetDefendantAccountFixedPenaltyResponse response =
-            fpService.getDefendantAccountFixedPenalty(defendantAccountId);
-
-        assertFalse(response.getVehicleFixedPenaltyFlag(),
-            "Expected flag to be false when vehicleFixedPenalty=false and registration is null");
-    }
-
-    @Test
-    void vehicleFixedPenaltyFlag_shouldBeFalse_whenVehicleRegistrationIsNVAndFlagFalse() {
-        Long defendantAccountId = 202L;
-        DefendantAccountEntity account = buildMockAccount(defendantAccountId);
-
-        FixedPenaltyOffenceEntity offence = buildMockOffence();
-        offence.setVehicleRegistration("NV");
-        offence.setVehicleFixedPenalty(false);
-
-        when(defendantAccountRepositoryService.findById(defendantAccountId)).thenReturn(account);
-        when(fixedPenaltyOffenceRepositoryService.findByDefendantAccountId(defendantAccountId))
-            .thenReturn(offence);
-
-        GetDefendantAccountFixedPenaltyResponse response =
-            fpService.getDefendantAccountFixedPenalty(defendantAccountId);
-
-        assertFalse(response.getVehicleFixedPenaltyFlag(),
-            "Expected flag to be false when vehicleFixedPenalty=false and registration='NV'");
-    }
-
-    @Test
-    void vehicleFixedPenaltyFlag_shouldBeTrue_whenVehicleRegistrationIsNotNV() {
-        Long defendantAccountId = 203L;
-        DefendantAccountEntity account = buildMockAccount(defendantAccountId);
-
-        FixedPenaltyOffenceEntity offence = buildMockOffence();
-        offence.setVehicleRegistration("AB12CDE");
-        offence.setVehicleFixedPenalty(false);
-
-        when(defendantAccountRepositoryService.findById(defendantAccountId)).thenReturn(account);
-        when(fixedPenaltyOffenceRepositoryService.findByDefendantAccountId(defendantAccountId))
-            .thenReturn(offence);
-
-        GetDefendantAccountFixedPenaltyResponse response =
-            fpService.getDefendantAccountFixedPenalty(defendantAccountId);
-
-        assertTrue(response.getVehicleFixedPenaltyFlag(),
-            "Expected flag to be true when vehicleRegistration='AB12CDE' even if vehicleFixedPenalty=false");
-    }
-
-    private DefendantAccountEntity buildMockAccount(Long accountId) {
-        return DefendantAccountEntity.builder()
-            .defendantAccountId(accountId)
-            .originatorName("Kingston-upon-Thames Mags Court")
-            .versionNumber(1L)
-            .build();
-    }
-
-    private FixedPenaltyOffenceEntity buildMockOffence() {
-        return FixedPenaltyOffenceEntity.builder()
-            .ticketNumber("888")
-            .vehicleRegistration(null)
-            .offenceLocation("London")
-            .noticeNumber("PN98765")
-            .issuedDate(LocalDate.of(2024, 1, 1))
-            .licenceNumber("DOE1234567")
-            .vehicleFixedPenalty(false)
-            .timeOfOffence(LocalTime.parse("12:34"))
-            .build();
     }
 
     @Test
