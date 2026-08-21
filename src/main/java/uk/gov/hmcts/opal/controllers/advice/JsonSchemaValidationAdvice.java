1e8664c4c0545567be3dfef8b8cb3870b6d04f1f
hennaparmar <henna.parmar@hmcts.net>
2026-08-21 16:43:01 +0100

    Remove token-derived fields from draft account requests and update related tests

--- src/main/java/uk/gov/hmcts/opal/controllers/advice/JsonSchemaValidationAdvice.java
+++ src/main/java/uk/gov/hmcts/opal/controllers/advice/JsonSchemaValidationAdvice.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Type;
 import java.nio.charset.StandardCharsets;
-import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.HttpHeaders;
@@ -13,23 +12,13 @@ import org.springframework.http.HttpInputMessage;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.RequestBodyAdviceAdapter;
-import tools.jackson.core.JacksonException;
-import tools.jackson.databind.JsonNode;
 import uk.gov.hmcts.opal.annotation.JsonSchemaValidated;
-import uk.gov.hmcts.opal.dto.ToJsonString;
 import uk.gov.hmcts.opal.exception.JsonSchemaValidationException;
 import uk.gov.hmcts.opal.service.opal.JsonSchemaValidationService;
 
 @ControllerAdvice
 @AllArgsConstructor
 public class JsonSchemaValidationAdvice extends RequestBodyAdviceAdapter {
-
-    private static final List<String> DRAFT_ACCOUNT_FORBIDDEN_REQUEST_FIELDS = List.of(
-        "submitted_by",
-        "submitted_by_name",
-        "validated_by",
-        "validated_by_name"
-    );
 
     private final JsonSchemaValidationService jsonSchemaValidationService;
 
@@ -51,7 +40,6 @@ public class JsonSchemaValidationAdvice extends RequestBodyAdviceAdapter {
         if (annotation != null) {
             String schemaPath = annotation.schemaPath();
             rejectTimelineDataForDraftAccountRequests(body, schemaPath);
-            rejectTokenDerivedFieldsForDraftAccountRequests(body, schemaPath);
             jsonSchemaValidationService.validateOrError(body, schemaPath);
         }
 
@@ -76,34 +64,5 @@ public class JsonSchemaValidationAdvice extends RequestBodyAdviceAdapter {
             && body.contains("\"timeline_data\"")) {
             throw new JsonSchemaValidationException("timeline_data is not allowed in draft account requests");
         }
-    }
-
-    private static void rejectTokenDerivedFieldsForDraftAccountRequests(String body, String schemaPath) {
-        if (!isDraftAccountRequestSchema(schemaPath)) {
-            return;
-        }
-
-        JsonNode rootNode;
-        try {
-            rootNode = ToJsonString.getObjectMapper().readTree(body);
-        } catch (JacksonException e) {
-            return;
-        }
-
-        List<String> suppliedFields = DRAFT_ACCOUNT_FORBIDDEN_REQUEST_FIELDS.stream()
-            .filter(field -> rootNode.get(field) != null)
-            .toList();
-
-        if (!suppliedFields.isEmpty()) {
-            throw new JsonSchemaValidationException("Fields are not allowed in draft account requests: "
-                                                        + String.join(", ", suppliedFields));
-        }
-    }
-
-    private static boolean isDraftAccountRequestSchema(String schemaPath) {
-        return schemaPath.contains("draft-account")
-            && (schemaPath.contains("addDraftAccountRequest")
-            || schemaPath.contains("replaceDraftAccountRequest")
-            || schemaPath.contains("updateDraftAccountRequest"));
     }
 }
