2667c0aed39d985b311cf301e0e00c285d0eef5a
joesheencgi <292120363+joesheencgi@users.noreply.github.com>
2026-08-24 12:01:59 +0100

    Removed unused imports and fixed mapping and mapper imports

--- src/main/java/uk/gov/hmcts/opal/mapper/request/PaymentTermsMapper.java
+++ src/main/java/uk/gov/hmcts/opal/mapper/request/PaymentTermsMapper.java
@@ -3,14 +3,11 @@ package uk.gov.hmcts.opal.mapper.request;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
-import org.openapitools.jackson.nullable.JsonNullable;
 import uk.gov.hmcts.opal.dto.PaymentTerms;
 import uk.gov.hmcts.opal.dto.common.InstalmentPeriod;
 import uk.gov.hmcts.opal.dto.common.PaymentTermsType;
-import uk.gov.hmcts.opal.dto.legacy.LegacyGetDefendantAccountPaymentTermsResponse;
 import uk.gov.hmcts.opal.entity.paymentterms.PaymentTermsEntity;
 import uk.gov.hmcts.opal.entity.paymentterms.TermsTypeCode;
-import uk.gov.hmcts.opal.generated.model.PaymentTermsSummaryCommonStrict;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface PaymentTermsMapper {
@@ -26,10 +23,6 @@ public interface PaymentTermsMapper {
     @Mapping(source = "termsTypeCode", target = "paymentTermsType.paymentTermsTypeCode")
     @Mapping(source = "instalmentPeriod", target = "instalmentPeriod.instalmentPeriodCode")
     PaymentTerms toDto(PaymentTermsEntity savedPaymentTerms);
-
-    PaymentTermsSummaryCommonStrict toPaymentTermsSummary(PaymentTermsEntity entity);
-
-    PaymentTermsSummaryCommonStrict toPaymentTermsSummary(LegacyGetDefendantAccountPaymentTermsResponse legacy);
 
     default uk.gov.hmcts.opal.entity.paymentterms.InstalmentPeriod map(
         InstalmentPeriod.InstalmentPeriodCode code
@@ -47,9 +40,5 @@ public interface PaymentTermsMapper {
 
     default PaymentTermsType.PaymentTermsTypeCode map(TermsTypeCode code) {
         return code == null ? null : PaymentTermsType.PaymentTermsTypeCode.fromValue(code.getCode());
-    }
-
-    default <T> JsonNullable<T> toJsonNullable(T value) {
-        return value == null ? JsonNullable.undefined() : JsonNullable.of(value);
     }
 }
