e4e3e3c83f091d8e9202ecdc3e1bc840af016633
alex-reeves-cgi <275961728+alex-reeves-cgi@users.noreply.github.com>
2026-09-07 15:40:14 +0100

    PO-9718 used DateUtils for age check, adds a null check

--- src/main/java/uk/gov/hmcts/opal/service/opal/OpalDefendantAccountBuilders.java
+++ src/main/java/uk/gov/hmcts/opal/service/opal/OpalDefendantAccountBuilders.java
@@ -109,6 +109,7 @@ import uk.gov.hmcts.opal.generated.model.PaymentTermsSummaryCommonStrict;
 import uk.gov.hmcts.opal.generated.model.PaymentTermsTypeCommonStrict;
 import uk.gov.hmcts.opal.generated.model.ResultReferenceCommon;
 import uk.gov.hmcts.opal.generated.model.ResultResponsesCommon;
+import uk.gov.hmcts.opal.util.DateTimeUtils;
 
 public class OpalDefendantAccountBuilders {
 
@@ -1279,7 +1280,7 @@ public class OpalDefendantAccountBuilders {
     }
 
     public static boolean isNotYouth(PartyEntity party) {
-        return !isYouth(party.getBirthDate().atStartOfDay(), party.getAge());
+        return !isYouth(DateTimeUtils.startOf(party.getBirthDate()), party.getAge());
     }
 
     public static DefendantAccountPartiesEntity filterDefendantParty(DefendantAccountEntity account) {
