69e598e4a114c69cf8d1d190d6cc33f3bed36820
AlanRogers-user <Alan.Rogers@HMCTS.net>
2026-09-09 08:37:29 +0100

    Last change for review comments.

--- src/main/java/uk/gov/hmcts/opal/dto/legacy/utils/ValidationUtils.java
+++ src/main/java/uk/gov/hmcts/opal/dto/legacy/utils/ValidationUtils.java
@@ -18,19 +18,4 @@ public class ValidationUtils {
         return count == 1;
     }
 
-    /* This looks like a candidate for communalization for several legacy services...
-    public static <T> void checkResponseForError(Response<T> response, String method) {
-        if (response.isError()) {
-            log.error(":{}: legacy error HTTP {}", method, response.code);
-            if (response.isException()) {
-                log.error(":{}: exception:", method, response.exception);
-            } else if (response.isLegacyFailure()) {
-                log.error(":{}: legacy failure body:\n{}", method, response.body);
-            } else if (response.code.equals(HttpStatusCode.valueOf(HttpStatus.NOT_FOUND.value()))) {
-                log.error(":{}: legacy not found", method);
-            }
-        } else if (response.isSuccessful()) {
-            log.info(":{}: legacy success.", method);
-        }
-    }*/
 }
