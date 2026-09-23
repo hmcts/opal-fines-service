13152237a4636ecea9f7f079be831550c826ef25
alex-reeves-cgi <275961728+alex-reeves-cgi@users.noreply.github.com>
2026-09-23 14:57:14 +0100

    PO-3701 get court fees endpoint

--- src/main/java/uk/gov/hmcts/opal/repository/CourtFeeRepository.java
+++ src/main/java/uk/gov/hmcts/opal/repository/CourtFeeRepository.java
@@ -4,9 +4,10 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import uk.gov.hmcts.opal.entity.CourtFeeEntity;
+import uk.gov.hmcts.opal.entity.businessunit.BusinessUnitEntity;
 
 @Repository
 public interface CourtFeeRepository extends JpaRepository<CourtFeeEntity, Long> {
 
-    List<CourtFeeEntity> findAllByBusinessUnit_businessUnitId(Short businessUnitId);
+    List<CourtFeeEntity> findAllByBusinessUnit(BusinessUnitEntity businessUnit);
 }
--- src/main/java/uk/gov/hmcts/opal/service/CourtFeesService.java
+++ src/main/java/uk/gov/hmcts/opal/service/CourtFeesService.java
@@ -4,22 +4,32 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import uk.gov.hmcts.opal.common.user.authorisation.exception.PermissionNotAllowedException;
+import uk.gov.hmcts.opal.common.user.authorisation.model.Domain;
 import uk.gov.hmcts.opal.entity.CourtFeeEntity;
+import uk.gov.hmcts.opal.entity.businessunit.BusinessUnitEntity;
 import uk.gov.hmcts.opal.generated.model.CourtFeesResponse;
 import uk.gov.hmcts.opal.mapper.CourtFeeMapper;
 import uk.gov.hmcts.opal.repository.CourtFeeRepository;
+import uk.gov.hmcts.opal.service.opal.BusinessUnitService;
 
 @Service
 @Slf4j(topic = "opal.CourtFeesService")
 @RequiredArgsConstructor
 public class CourtFeesService {
+    private final UserStateService userStateService;
+    private final BusinessUnitService businessUnitService;
     private final CourtFeeRepository courtFeeRepository;
     private final CourtFeeMapper courtFeeMapper;
 
     public CourtFeesResponse getCourtFeesForBusinessUnit(Short businessUnitId) {
-        //todo use id or get BU 1st?
-        //todo test security? prob don't need to use UserService tho...
-        List<CourtFeeEntity> courtFees = courtFeeRepository.findAllByBusinessUnit_businessUnitId(businessUnitId);
+        BusinessUnitEntity businessUnit = businessUnitService.getBusinessUnit(businessUnitId);
+        userStateService.getUserStateFromSecurityContext()
+            .getDomainBusinessUnitUsers(Domain.FINES)
+            .getBusinessUnitUserForBusinessUnit(businessUnitId)
+            .orElseThrow(() -> new PermissionNotAllowedException(businessUnitId));
+
+        List<CourtFeeEntity> courtFees = courtFeeRepository.findAllByBusinessUnit(businessUnit);
         return courtFeeMapper.toCourtFeesResponse(courtFees);
     }
 }
